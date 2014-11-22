7d8f3fb40e6c876ab1edfab2eb13e1e7eba0e3ad
TimEvWw <tim.evers@gmail.com>
2014-11-22 19:49:39 -0100

    refactor command execution

--- lib/controller_command_proc.rb
+++ lib/controller_command_proc.rb
@@ -7,57 +7,17 @@ class ControllerCommandProc
     @bot_dbaccess        = $bot_dbaccess
   end
 
+  def whitelist
+    ['MOVE ABSOLUTE','MOVE RELATIVE','HOME X','HOME Y','HOME Z','CALIBRATE X','CALIBRATE Y','CALIBRATE Z','DOSE WATER','SET SPEED','PIN WRITE','PIN READ','PIN MODE','PIN PULSE','SERVO MOVE']
+  end
+
   def process_command( cmd )
 
     if cmd != nil
       cmd.command_lines.each do |command_line|
-        $status.info_movement = "#{command_line.action.downcase} xyz=#{command_line.coord_x} #{command_line.coord_y} #{command_line.coord_z} amt=#{command_line.amount} spd=#{command_line.speed}"
+        #$status.info_movement = "#{command_line.action.downcase} xyz=#{command_line.coord_x} #{command_line.coord_y} #{command_line.coord_z} amt=#{command_line.amount} spd=#{command_line.speed}"
         @bot_dbaccess.write_to_log(1,@info_movement)
-
-        if $hardware_sim == 0
-          case command_line.action.upcase
-            when "MOVE ABSOLUTE"
-              $bot_hardware.move_absolute(command_line.coord_x, command_line.coord_y, command_line.coord_z)
-            when "MOVE RELATIVE"
-              $bot_hardware.move_relative(command_line.coord_x, command_line.coord_y, command_line.coord_z)
-            when "HOME X"
-              $bot_hardware.move_home_x
-            when "HOME Y"
-              $bot_hardware.move_home_y
-            when "HOME Z"
-              $bot_hardware.move_home_z
-
-            when "CALIBRATE X"
-              $bot_hardware.calibrate_x
-            when "CALIBRATE Y"
-              $bot_hardware.calibrate_y
-            when "CALIBRATE Z"
-              $bot_hardware.calibrate_z
-
-            when "DOSE WATER"
-              #$bot_hardware.dose_water(command_line.amount)
-            when "SET SPEED"
-              $bot_hardware.set_speed(command_line.speed)
-            when "PIN WRITE"
-              $bot_hardware.pin_std_set_value(command_line.pin_nr, command_line.pin_value_1, command_line.pin_mode)
-            when "PIN READ"
-              $bot_hardware.pin_std_read_value(command_line.pin_nr, command_line.pin_mode, command_line.external_info)
-            when "PIN MODE"
-              $bot_hardware.pin_std_set_mode(command_line.pin_nr, command_line.pin_mode)
-            when "PIN PULSE"
-              $bot_hardware.pin_std_pulse(command_line.pin_nr, command_line.pin_value_1, 
-                command_line.pin_value_2, command_line.pin_time, command_line.pin_mode)
-
-            when "SERVO MOVE"
-              $bot_hardware.servo_std_move(command_line.pin_nr, command_line.pin_value_1)
-
-          end
-
-        else
-          @bot_dbaccess.write_to_log(1,'>simulating hardware<')
-
-          sleep 2
-        end
+        send_command(command_line)
       end
     else
       sleep 0.1
@@ -66,4 +26,79 @@ class ControllerCommandProc
     $status.info_movement = 'idle'
 
   end
+
+  def send_command(command_line)
+    if $hardware_sim == 0
+      function = command_line.action.downcase.sub(' ','_')
+      puts function
+      send(function, command_line))
+    else
+      @bot_dbaccess.write_to_log(1,'>simulating hardware<')
+      sleep 2
+    end
+  end
+
+  def move_absolute(command_line)
+    $bot_hardware.move_absolute(command_line.coord_x, command_line.coord_y, command_line.coord_z)
+  end
+
+  def move_relative(command_line)
+    $bot_hardware.move_relative(command_line.coord_x, command_line.coord_y, command_line.coord_z)
+  end
+
+  def home_x(command_line)
+    $bot_hardware.move_home_x
+  end
+
+  def home_y(command_line)
+    $bot_hardware.move_home_y
+  end
+
+  def home_z(command_line)
+    $bot_hardware.move_home_z
+  end
+
+  def calibration_x(command_line)
+    $bot_hardware.calibrate_x
+  end
+
+  def calibration_y(command_line)
+    $bot_hardware.calibrate_y
+  end
+
+  def calibration_z(command_line)
+    $bot_hardware.calibrate_z
+  end
+
+  def dose_water(command_line)
+    #$bot_hardware.dose_water(command_line.amount)
+  end
+
+  def set_speed(command_line)
+    $bot_hardware.set_speed(command_line.speed)
+  end
+
+  def pin_write(command_line)
+    $bot_hardware.pin_std_set_value(command_line.pin_nr, command_line.pin_value_1, command_line.pin_mode)
+  end
+
+  def pin_read(command_line)
+    $bot_hardware.pin_std_read_value(command_line.pin_nr, command_line.pin_mode, command_line.external_info)
+  end
+
+  def pin_mode(command_line)
+    $bot_hardware.pin_std_set_mode(command_line.pin_nr, command_line.pin_mode)
+  end
+
+  def pin_pulse(command_line)
+    $bot_hardware.pin_std_pulse(command_line.pin_nr, command_line.pin_value_1, 
+                command_line.pin_value_2, command_line.pin_time, command_line.pin_mode)
+  end
+
+  def servo_move(command_line)
+    $bot_hardware.servo_std_move(command_line.pin_nr, command_line.pin_value_1)
+  end
+
+
+
 end
