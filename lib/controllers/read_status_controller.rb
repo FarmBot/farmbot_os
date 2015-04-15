ee17c817e0bb518ff11d5d9101f9220b271d8e6d
Rick Carlino <rick.carlino@gmail.com>
2015-04-15 08:25:34 -0500

    Add pin values to read_status response object

--- lib/controllers/read_status_controller.rb
+++ lib/controllers/read_status_controller.rb
@@ -2,8 +2,28 @@ require_relative 'abstract_controller'
 
 class ReadStatusController < AbstractController
   def call
-    reply "read_status", x: bot.status[:x], y: bot.status[:y],
-                         z: bot.status[:z], busy: bot.status[:busy],
-                         current_command:  bot.status[:last]
+    reply "read_status", info
+  end
+
+  private
+
+  def info
+    info = {busy: bot.status[:busy], current_command: bot.status[:last]}
+    info.merge(pin_info).merge(axis_info)
+  end
+
+  def pin_info
+    # Performs lazy evaluation of pin status. If the pin status is 'unknown',
+    # performs a lookup for next status poll. Otherwise, uses cached value.
+    [*0..13].inject({}) do |hsh, pin|
+      status = bot.status.pin(pin)
+      hsh["pin#{pin}".to_sym] = status
+      bot.commands.read_parameter(pin) if status == :unknown
+      hsh
+    end
+  end
+
+  def axis_info
+    {x: bot.status[:x], y: bot.status[:y], z: bot.status[:z]}
   end
 end
