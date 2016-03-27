56face147965a481dbdde872c762120e0d5f25ec
Rick Carlino <rick.carlino@gmail.com>
2016-03-27 16:09:45 -0500

    Remove settings.yml from git

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,12 +1,15 @@
 require 'settingslogic'
 module FBPi
   class Settings < Settingslogic
+    DEFAULTS = "db/settings.defaults.yml"
     source "db/settings.yml"
     namespace ENV['FBENV'] || 'production'
 
     def self.save
-      original_file = YAML.load_file(source)
+      defaults = YAML.load_file(DEFAULTS)
+      original_file = File.file?(source) ? YAML.load_file(source) : {}
       original_file[namespace] = Hash[self]
+      defaults.merge(original_file)
       yml_string = original_file.to_yaml
       File.open(source, 'w') { |f| f.write(original_file.to_yaml) }
       self
