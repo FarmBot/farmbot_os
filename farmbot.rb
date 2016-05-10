3cd1eb87e58b490603908afb66c534437415f3b7
Rick Carlino <rick.carlino@gmail.com>
2016-05-10 15:55:03 -0500

    Lower reconnect wait time to 1000 ms

--- farmbot.rb
+++ farmbot.rb
@@ -1,9 +1,10 @@
 require_relative 'lib/farmbot-pi'
+
 begin
   puts "Connecting..."
   FarmBotPi.new.start
 rescue MQTT::NotConnectedException => e
-  puts "OH NOES! Farmbot was disconnected from MQTT. Retrying in 3 seconds."
-  sleep 3
+  puts "OH NOES! Farmbot was disconnected from MQTT. Retrying..."
+  sleep 1
   retry
 end
