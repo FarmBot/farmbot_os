fb15e5cf3f195364020b66ea99c64d6353d2814f
Rick Carlino <rick.carlino@gmail.com>
2016-05-13 22:15:08 -0500

    dfasf

--- lib/command_objects/if_statement.rb
+++ lib/command_objects/if_statement.rb
@@ -25,7 +25,11 @@ private
       expression = lhs+operator+rhs
       test = "{% if #{expression} %}true{% else %}false{% endif %}"
       outcome = Liquid::Template.parse(test).render({}) == true
-      puts "\n\nsequence #{sequence.name} WILL execute\n" if outcome
+      if outcome
+        puts "\n\nsequence #{sequence.name} WILL execute\n"
+      else
+        puts "\n\nsequence #{sequence.name} WILL NOT execute\n"
+      end
       return outcome
     end
   end
