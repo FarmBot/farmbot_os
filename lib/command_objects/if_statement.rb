46db79414ee565625e9aea017a67091902d657da
Rick Carlino <rick.carlino@gmail.com>
2016-05-13 21:59:14 -0500

    sfasda

--- lib/command_objects/if_statement.rb
+++ lib/command_objects/if_statement.rb
@@ -24,8 +24,9 @@ private
       # String eval via liquid templates for safety / sandboxing.
       expression = lhs+operator+rhs
       test = "{% if #{expression} %}true{% else %}false{% endif %}"
-      outcome = Liquid::Template.parse(test).render({})
-      return (outcome == "true")
+      outcome = Liquid::Template.parse(test).render({}) == true
+      puts "sequence #{sequence.name} WILL execute" if outcome
+      return outcome
     end
   end
 end
