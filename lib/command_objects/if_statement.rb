ffbef4c9c6d6ca331570b508a728a423d6bf9772
Rick Carlino <rick.carlino@gmail.com>
2016-05-16 18:49:55 -0500

    Remove Friday hotfix

--- lib/command_objects/if_statement.rb
+++ lib/command_objects/if_statement.rb
@@ -22,17 +22,11 @@ private
 
     def evaluates_to_true
       # String eval via liquid templates for safety / sandboxing.
-      expression = lhs+operator+rhs
-      # test = "{% if #{expression} %}true{% else %}false{% endif %}"
-      # outcome = Liquid::Template.parse(test).render({}).strip == "true"
-      # puts "\n\n #{ test } \n"
-      # if outcome
-      #   puts "\n\nsequence #{sequence.name} WILL execute\n"
-      # else
-      #   puts "\n\nsequence #{sequence.name} WILL NOT execute\n"
-      # end
-
-      return eval(expression) # outcome
+      expression = "#{lhs} #{operator} #{rhs}"
+      test = "{% if #{expression} %}true{% else %}false{% endif %}"
+      outcome = Liquid::Template.parse(test).render({}).strip == "true"
+      puts outcome ? "WILL EXECUTE THIS SEQUENCE" : "WONT EXECUTE THIS SEQUENCE"
+      return outcome
     end
   end
 end
