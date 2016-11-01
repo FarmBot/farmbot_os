153d8fb839e1fd3fec23c87e45f4d41fe231298c
connor rigby <connor@farmbot.io>
2016-11-01 12:04:14 -0700

    Small overhaul to gcode parser

---
+++ test/serial/gcode_test.exs
@@ -0,0 +1,12 @@
+ExUnit.start
+
+defmodule GcodeTest do
+  use ExUnit.Case, async: true
+
+  test "Parses report paramater" do
+    a = Gcode.parse_code("R21 R0 V0")
+    b = Gcode.parse_code("R21 R0 V0 Q5")
+    assert(a == {:report_parameter_value, :param_version, 0, "0"})
+    assert(b == {:report_parameter_value, :param_version, 0, "5"})
+  end
+end
