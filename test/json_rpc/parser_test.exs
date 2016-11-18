c76d20c7d3662f2cc23444f5834a0c6e4a674638
connor rigby <connor@farmbot.io>
2016-11-18 15:45:35 -0800

    how2test

---
+++ test/json_rpc/parser_test.exs
@@ -0,0 +1,5 @@
+defmodule RPC.ParserTest do
+  use ExUnit.Case, async: true
+
+
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,17 +1,2 @@
 ExUnit.start
-IO.puts "starting test genserver"
-Code.load_file("test_genserver.ex", "./test")
-Code.ensure_loaded(TestGenServer)
-Code.load_file("fake_mqtt.ex", "./test")
-Code.ensure_loaded(FakeMqtt)
-Code.load_file("fake_net_man.ex", "./test")
-Code.ensure_loaded(NetMan)
-# Start safe storage (in dev mode)
-{:ok, _} = TestGenServer.start_link(SafeStorage)
-
-# Start farmbot auth
-{:ok, _fb_auth} = Farmbot.Auth.start(:normal, [])
-
-#FIXME
-{:ok, _botState} = Farmbot.BotState.start_link(
-      %{target: "test", compat_version: 0, env: :test, version: "2.1.4"})
+StateDebugger.start
