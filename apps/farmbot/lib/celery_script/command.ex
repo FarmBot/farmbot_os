b23b2462fec4a97fc608668d27fbf36509c3c0a2
connor rigby <connor@farmbot.io>
2017-01-04 11:31:06 -0800

    i probably forgot something

--- apps/farmbot/lib/celery_script/command.ex
+++ apps/farmbot/lib/celery_script/command.ex
@@ -106,7 +106,7 @@ defmodule Farmbot.CeleryScript.Command do
 
   # is this one a good idea?
   # there might be too expectations here: it could return the current position,
-  # or 0 
+  # or 0
   def ast_to_coord(%Ast{kind: "nothing", args: _, body: _}) do
     coordinate(%{x: 0, y: 0, z: 0}, [])
   end
@@ -288,7 +288,9 @@ defmodule Farmbot.CeleryScript.Command do
     # TODO: maybe don't do this as magic numbers. I dont even remember where
     # these numbers came from
     magic_numbers =
-      [0,11,12,13,21,22,23,31,32,33,41,42,43,51,52,53,61,62,63,71,72,73,101,102,103]
+      [0,11,12,13,21,22,23,31,
+       32,33,41,42,43,51,52,53,
+       61,62,63,71,72,73,101,102,103]
    for param <- magic_numbers do
      GHan.block_send("F21 P#{param}")
    end
