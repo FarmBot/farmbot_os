c6fe0cd418bcf3b0e7e9073cd0b7bc20b62a36ff
connor rigby <connor@farmbot.io>
2017-01-16 11:01:43 -0800

    backup. i want to try something

--- mix.exs
+++ mix.exs
@@ -7,14 +7,16 @@ defmodule FarmbotOs.Mixfile do
     [apps_path: "apps",
      version: @version,
      target: target(Mix.env),
+     config_path: "farmbot_config.exs",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     default_task: "hello",
+     deps: deps()]
   end
 
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
-  defp deps, do: []
+  defp deps(), do: []
 
   def target(:prod) do
     blah = System.get_env("NERVES_TARGET") || "rpi3"
@@ -23,4 +25,14 @@ defmodule FarmbotOs.Mixfile do
   end
 
   def target(_), do: "development"
+end
+
+defmodule Mix.Tasks.Hello do
+  use Mix.Task
+
+  def run(_) do
+    File.cd "apps/farmbot"
+    t = File.ls!
+    Mix.shell.info "hello #{inspect t}"
+  end
 end
