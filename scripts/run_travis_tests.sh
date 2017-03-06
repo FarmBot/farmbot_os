6c86f5c943fb24f0fbdd0443cfb69f45e55e8619
connor rigby <connor@farmbot.io>
2017-03-06 08:18:13 -0800

    nooop

--- scripts/run_travis_tests.sh
+++ scripts/run_travis_tests.sh
@@ -2,6 +2,6 @@
 export MIX_ENV=test
 mix deps.get
 mix travis_test || { echo 'Tests failed!' ; exit 1; }
-if [[ "$`TRAVIS_BRANCH`" == "staging" ]]; then
+if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
   make release
 fi
