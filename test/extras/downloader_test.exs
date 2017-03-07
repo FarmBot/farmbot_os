bb77127634301b0f5f9a7602a2aad0d3087c4558
connor rigby <connor@farmbot.io>
2017-03-07 09:08:55 -0800

    increase coverage so i dont get shamed

---
+++ test/extras/downloader_test.exs
@@ -0,0 +1,14 @@
+defmodule DownloaderTest do
+  use ExUnit.Case
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
+  test "downloads a file" do
+    use_cassette "good_corpus_request" do
+      path = "/tmp/download.thing"
+      url = "http://localhost:3000/api/corpuses"
+      ret_path = Downloader.run(url, path)
+      assert ret_path == path
+      assert File.exists?(path)
+    end
+  end
+end
