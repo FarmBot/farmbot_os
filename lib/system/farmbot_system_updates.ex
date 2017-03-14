18028d9a5f47b1c957d397e71dd935c91508c14a
connor rigby <connor@farmbot.io>
2017-03-14 10:39:19 -0700

    disable updates for rc versions

--- lib/system/farmbot_system_updates.ex
+++ lib/system/farmbot_system_updates.ex
@@ -48,8 +48,19 @@ defmodule Farmbot.System.Updates do
   @doc """
     Checks for updates
   """
-  @spec check_updates :: {:update, String.t} | :no_updates | {:error, term}
+  @spec check_updates :: {:update, binary} | :no_updates | {:error, term}
   def check_updates do
+    current = Farmbot.BotState.get_os_version
+    if String.contains?(current, "rc") do
+      Logger.error "Release Candidate Releases don't currently support updates!"
+      :no_updates
+    else
+      do_http_req()
+    end
+  end
+
+  @spec check_updates :: {:update, binary} | :no_updates | {:error, term}
+  defp do_http_req do
     case HTTPoison.get(@releases_url <> "/latest", @headers, @ssl_hack) do
        {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
          json = Poison.decode!(body)
@@ -58,7 +69,6 @@ defmodule Farmbot.System.Updates do
          url = "https://github.com/FarmBot/farmbot_os/releases/download/#{version}/farmbot-#{@target}-#{version_without_v}.fw"
          {:update, url}
        {:error, %HTTPoison.Error{reason: reason}} -> {:error, reason}
-       error -> {:error, error}
     end
   end
 
