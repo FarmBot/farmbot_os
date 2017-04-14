76ceba65da3cd72fc383bfe73cf0797d24c21073
connor rigby <connor@farmbot.io>
2017-04-14 09:55:53 -0700

    Fix styling/syntax from Rick.

--- lib/farmbot/celery_script/commands/zero.ex
+++ lib/farmbot/celery_script/commands/zero.ex
@@ -16,7 +16,7 @@ defmodule Farmbot.CeleryScript.Command.Zero do
   def run(%{axis: axis}, []), do: do_write(axis)
 
   @spec do_write(binary) :: no_return
-  defp do_write("x"), do:"F84 X1 Y0 Z0" |>  UartHan.write
-  defp do_write("y"), do:"F84 X0 Y1 Z0" |>  UartHan.write
-  defp do_write("z"), do:"F84 X0 Y0 Z1" |>  UartHan.write
+  defp do_write("x"), do: "F84 X1 Y0 Z0" |> UartHan.write
+  defp do_write("y"), do: "F84 X0 Y1 Z0" |> UartHan.write
+  defp do_write("z"), do: "F84 X0 Y0 Z1" |> UartHan.write
 end
