021b50d2703770970ac6c236edcc07016d678232
connor rigby <connor@farmbot.io>
2017-05-04 07:43:40 -0700

    dont use master for buildroot lol

--- scripts/clone_system.sh
+++ scripts/clone_system.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 SYSTEM=$1
 NERVES_SYSTEM_BR_GIT="https://github.com/nerves-project/nerves_system_br"
+NERVES_SYSTEM_BR_COMMIT="049d8e19b69b0f84084182d9bdd915e4eb431ed5"
 
 if [ -d "nerves/NERVES_SYSTEM_$SYSTEM" ]; then
   # Control will enter here if $DIRECTORY exists.
   echo "NERVES_SYSTEM_$SYSTEM dir found."
 else
   echo "NERVES_SYSTEM_$SYSTEM dir not found."
+  CWD=$(pwd)
   git clone $NERVES_SYSTEM_BR_GIT nerves/nerves_system_br
+  cd nerves/nerves_system_br
+  git checkout $NERVES_SYSTEM_BR_COMMIT
+  cd $CWD
 fi
 
 nerves/nerves_system_br/create-build.sh nerves/nerves_system_$SYSTEM/nerves_defconfig nerves/NERVES_SYSTEM_$SYSTEM
