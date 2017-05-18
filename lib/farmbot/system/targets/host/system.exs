45383ee49893a0788fc471d1478309926a9629c0
connor rigby <connor@farmbot.io>
2017-05-18 13:33:27 -0700

    try this rick

--- lib/farmbot/system/targets/host/system.exs
+++ lib/farmbot/system/targets/host/system.exs
@@ -1,6 +1,8 @@
-defmodule Module.concat([Farmbot,System,"host"]) do
+defmodule Module.concat([Farmbot,System,"host"]) do 
   @moduledoc false
   @behaviour Farmbot.System
+
+  require IEx
 
   def reboot, do: :ok
   def power_off, do: :ok
@@ -11,6 +13,7 @@ defmodule Module.concat([Farmbot,System,"host"]) do
       File.rm_rf files
       File.write("#{path()}/factory_reset_reason", reason)
     end, true
+    IEx.pry
     System.halt(0)
   end
 
