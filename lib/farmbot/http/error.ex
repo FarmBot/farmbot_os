b737234faf4252ed93ffc95bc05b779be6e3f5d4
connor rigby <connor@farmbot.io>
2017-07-06 14:39:24 -0700

    /shrug

--- lib/farmbot/http/error.ex
+++ lib/farmbot/http/error.ex
@@ -1,3 +1,21 @@
 defmodule Farmbot.HTTP.Error do
-  
+  @moduledoc "Some Farmbot HTTP Adapter error."
+
+  alias Farmbot.HTTP.{Response}
+
+  defexception [:message, :response]
+
+  def exception(desc) when is_atom(desc) or is_binary(desc) do
+    %__MODULE__{message: String.trim("#{inspect desc}")}
+  end
+
+  def exception(%Response{status_code: code} = resp) do
+    %__MODULE__{message: "HTTP Request failed (#{code})", response: resp}
+  end
+
+  def exception(other) do
+    %__MODULE__{message: "Unknown error: #{inspect other}"}
+  end
+
+  def message(%{message: message}), do: message
 end
