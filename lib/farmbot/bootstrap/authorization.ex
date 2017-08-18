a853c4903e5962170879790425db2f94b107059b
connor rigby <connor@farmbot.io>
2017-08-18 11:48:44 -0700

    Refactor default auth task and test it.

--- lib/farmbot/bootstrap/authorization.ex
+++ lib/farmbot/bootstrap/authorization.ex
@@ -25,12 +25,53 @@ defmodule Farmbot.Bootstrap.Authorization do
   # It gets overwrote in the Test Environment.
   @doc "Authorizes with the farmbot api."
   def authorize(email, password, server) do
-    {:ok, {_http_ver, _head, body}} = :httpc.request('#{server}/api/public_key')
-    key = RSA.decode_key("#{body}")
-    secret = %{email: email, password: password, id: UUID.uuid1, version: 1} |> Poison.encode! |> RSA.encrypt({:public, key}) |> Base.encode64
-    payload = %{user:  %{credentials: secret}} |> Poison.encode!
-    request = {'#{server}/api/tokens', ['UserAgent', 'FarmbotOSBootstrap'], 'application/json', payload}
-    {:ok, {_, _, resp}} = :httpc.request(:post, request, [], [])
-    {:ok, Poison.decode!(resp)["token"]["encoded"]}
+    with {:ok, rsa_key } <- fetch_rsa_key(server),
+         {:ok, payload } <- build_payload(email, password, rsa_key),
+         {:ok, resp    } <- request_token(server, payload),
+         {:ok, body    } <- Poison.decode(resp),
+         {:ok, map     } <- Map.fetch(body, "token") do
+           Map.fetch(map,  "encoded")
+         else
+           :error -> {:error, "unknown error."}
+           err -> handle_error(err)
+         end
   end
+
+  defp fetch_rsa_key(server) do
+    case :httpc.request('#{server}/api/public_key') do
+      {:ok, {_, _, body}} ->
+        r = body |> to_string() |> RSA.decode_key()
+        {:ok, r}
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  defp build_payload(email, password, rsa_key) do
+    secret = %{email: email, password: password, id: UUID.uuid1, version: 1}
+    |> Poison.encode!
+    |> RSA.encrypt({:public, rsa_key})
+    |> Base.encode64
+    payload = %{user:  %{credentials: secret}} |> Poison.encode
+  end
+
+  defp request_token(server, payload) do
+    request = {'#{server}/api/tokens',
+               ['UserAgent', 'FarmbotOSBootstrap'],
+               'application/json', payload}
+
+    case :httpc.request(:post, request, [], []) do
+      {:ok, {{_, 200, _}, _, resp  }} -> {:ok, resp}
+      {:ok, {{_, 422, _}, _, _resp }} ->
+        {:error, "Failed to authorize with the Farmbot web application at: #{server}"}
+      {:error, error} -> {:error, error}
+    end
+
+  end
+
+  defp handle_error({:error, {:failed_connect, [{:to_address, {domain, 443}}, {:inet, [:inet], :nxdomain}]}}) do
+    msg = "Failed to connect to #{domain}"
+    {:error, msg}
+  end
+
+  defp handle_error({:error, reason} = error), do: error
 end
---
+++ test/farmbot/bootstrap/authorization_test.exs
@@ -0,0 +1,34 @@
+defmodule Farmbot.Bootstrap.AuthorizationTest do
+  @moduledoc "Tests the default authorization implementation"
+  alias Farmbot.Bootstrap.Authorization, as: Auth
+  use ExUnit.Case
+
+  @moduletag :farmbot_api
+
+  setup do
+    email  = Application.get_env(:farmbot, :authorization)[:email   ] || raise Auth.Error, "No email provided."
+    pass   = Application.get_env(:farmbot, :authorization)[:password] || raise Auth.Error, "No password provided."
+    server = Application.get_env(:farmbot, :authorization)[:server  ] || raise Auth.Error, "No server provided."
+    [email: email, password: pass, server: server]
+  end
+
+  test "Authorizes with the farmbot web api.", ctx do
+    res = Auth.authorize(ctx.email, ctx.password, ctx.server)
+    assert match?({:ok, _}, res)
+    {:ok, bin_tkn} = res
+    empty_token = struct(Farmbot.Jwt)
+    assert match?(empty_token, Farmbot.Jwt.decode!(bin_tkn))
+  end
+
+  test "gives a nice error on bad credentials.", ctx do
+    res = Auth.authorize(ctx.email, ctx.password, "https://your.farmbot.io/")
+    assert match?({:error, _}, res)
+    {:error, message} = res
+    assert message == "Failed to connect to your.farmbot.io"
+
+    res = Auth.authorize("yolo@mtv.org", "123password", ctx.server)
+    assert match?({:error, _}, res)
+    {:error, message} = res
+    assert message == "Failed to authorize with the Farmbot web application at: #{ctx.server}"
+  end
+end
