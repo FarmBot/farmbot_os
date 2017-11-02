1d04c3ed71e4048b01e5a4dca607e9687ff876f2
connor rigby <connor@farmbot.io>
2017-11-02 12:17:19 -0700

    Nope

--- lib/farmbot/celery_script/virtual_machine/virtual_machine.ex
+++ lib/farmbot/celery_script/virtual_machine/virtual_machine.ex
@@ -5,15 +5,15 @@ defmodule Farmbot.CeleryScript.VirtualMachine do
   alias AST.Compiler
   alias Farmbot.CeleryScript.VirtualMachine.{InstructionSet, RuntimeError}
 
+  use GenServer
   defmodule State do
     @moduledoc false
-
     defstruct instruction_set: struct(InstructionSet)
   end
 
-  def execute(ast, state \\ %State{})
+  # alias Farmbot.CeleryScript.VirtualMachine, as: VM
+  # Farmbot.HTTP.get!("/api/sequences/2").body |> Poison.decode! |> Farmbot.CeleryScript.AST.parse
+  def execute(%AST{} = ast, state \\ struct(State)) do
 
-  def execute(%AST{} = ast, state) do
-    ast |> Compiler.compile(state.instruction_set)
   end
 end
