a8c115917020ce7ea6e809c9fa14cb18eca4f9fe
Connor Rigby <connor@farmbot.io>
2017-11-03 16:37:03 -0700

    bump versions

--- config/config.exs
+++ config/config.exs
@@ -19,7 +19,7 @@ config :elixir, ansi_enabled: true
 config :iex, :colors, enabled: true
 
 # bot <-> firmware transports.
-config :farmbot, expected_fw_version: ["5.0.4.F", "5.0.4.R"]
+config :farmbot, expected_fw_version: ["5.0.7.F", "5.0.7.R"]
 
 # Rollbar
 config :farmbot, rollbar_access_token: "dcd79b191ab84aa3b28259cbb80e2060"
