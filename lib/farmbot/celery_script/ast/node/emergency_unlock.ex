910b7c6d775188d290f6f184765ddd80c288d50c
connor rigby <connor@farmbot.io>
2017-11-21 12:22:53 -0800

    Add log for unlocked bot.

--- lib/farmbot/celery_script/ast/node/emergency_unlock.ex
+++ lib/farmbot/celery_script/ast/node/emergency_unlock.ex
@@ -2,11 +2,14 @@ defmodule Farmbot.CeleryScript.AST.Node.EmergencyUnlock do
   @moduledoc false
   use Farmbot.CeleryScript.AST.Node
   allow_args []
+  use Farmbot.Logger
 
   def execute(_, _, env) do
     env = mutate_env(env)
     case Farmbot.Firmware.emergency_unlock do
-      :ok -> {:ok, env}
+      :ok ->
+        Logger.success 1, "Bot is Successfully unlocked."
+        {:ok, env}
       {:error, reason} -> {:error, reason, env}
     end
   end
