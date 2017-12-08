a3e7c0b5d5a93dea82a0f2c130264c5f571c9f49
Connor Rigby <connor@farmbot.io>
2017-12-08 12:49:24 -0800

    Set default to true.

--- priv/config_storage/migrations/20171208041623_arduino_debug_param.exs
+++ priv/config_storage/migrations/20171208041623_arduino_debug_param.exs
@@ -4,6 +4,6 @@ defmodule Farmbot.System.ConfigStorage.Migrations.ArduinoDebugParam do
   import Farmbot.System.ConfigStorage.MigrationHelpers
 
   def change do
-    create_settings_config("arduino_debug_messages", :bool, false)
+    create_settings_config("arduino_debug_messages", :bool, true)
   end
 end
