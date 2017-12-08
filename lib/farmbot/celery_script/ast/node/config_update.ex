d1b2ce7f89f91b55182304469a0225f898de2e1c
Connor Rigby <connor@farmbot.io>
2017-12-07 20:19:59 -0800

    Add paramater for arduino debug logs

--- lib/farmbot/celery_script/ast/node/config_update.ex
+++ lib/farmbot/celery_script/ast/node/config_update.ex
@@ -62,6 +62,7 @@ defmodule Farmbot.CeleryScript.AST.Node.ConfigUpdate do
   defp lookup_os_config("sequence_init_log",         val), do: {:ok, {:bool,   "settings", format_bool_for_os(val)}}
   defp lookup_os_config("sequence_body_log",         val), do: {:ok, {:bool,   "settings", format_bool_for_os(val)}}
   defp lookup_os_config("sequence_complete_log",     val), do: {:ok, {:bool,   "settings", format_bool_for_os(val)}}
+  defp lookup_os_config("arduino_debug_messages",    val), do: {:ok, {:bool,   "settings", format_bool_for_os(val)}}
 
   defp lookup_os_config("network_not_found_timer",   val) when val > 0, do: {:ok, {:float,  "settings", to_float(val)}}
   defp lookup_os_config("network_not_found_timer",  _val), do: {:error, "network_not_found_timer must be greater than zero"}
@@ -69,6 +70,7 @@ defmodule Farmbot.CeleryScript.AST.Node.ConfigUpdate do
   defp lookup_os_config("firmware_hardware", "farmduino"), do: {:ok, {:string, "settings", "farmduino"}}
   defp lookup_os_config("firmware_hardware",   "arduino"), do: {:ok, {:string, "settings", "arduino"}}
   defp lookup_os_config("firmware_hardware",     unknown), do: {:error, "unknown hardware: #{unknown}"}
+
   defp lookup_os_config(unknown_config,                _), do: {:error, "unknown config: #{unknown_config}"}
 
   defp format_bool_for_os(1), do: true
