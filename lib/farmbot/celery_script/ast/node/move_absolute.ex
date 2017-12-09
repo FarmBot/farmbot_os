8c43489363a605e2a19c52bc0fd20d574204b6da
Connor Rigby <connor@farmbot.io>
2017-12-09 13:56:12 -0800

    Only log movement messages when arduino config is disabled

--- lib/farmbot/celery_script/ast/node/move_absolute.ex
+++ lib/farmbot/celery_script/ast/node/move_absolute.ex
@@ -12,18 +12,30 @@ defmodule Farmbot.CeleryScript.AST.Node.MoveAbsolute do
          {:ok, pos_b} <- ast_to_vec3(offset)
     do
       pos = vec3_math(pos_a, :+, pos_b)
-      Logger.busy 2, "Moving to (#{pos.x}, #{pos.y}, #{pos.z})"
+      maybe_log_busy(pos)
       speed_x = (speed / 100) * (Farmbot.BotState.get_param(:movement_max_spd_x) || 1)
       speed_y = (speed / 100) * (Farmbot.BotState.get_param(:movement_max_spd_y) || 1)
       speed_z = (speed / 100) * (Farmbot.BotState.get_param(:movement_max_spd_z) || 1)
       case Farmbot.Firmware.move_absolute(pos, speed_x |> round(), speed_y |> round(), speed_z |> round()) do
         :ok ->
-          Logger.success 2, "Movement to (#{pos.x}, #{pos.y}, #{pos.z}) complete."
+          maybe_log_complete(pos)
           {:ok, env}
         {:error, reason} -> {:error, reason, env}
       end
     else
       {:error, reason} -> {:error, reason, env}
+    end
+  end
+
+  defp maybe_log_busy do
+    unless Farmbot.System.ConfigStorage.get_config_value(:bool, "settings", "firmware_input_log") do
+      Logger.busy 2, "Moving to (#{pos.x}, #{pos.y}, #{pos.z})"
+    end
+  end
+
+  defp maybe_log_complete do
+    unless Farmbot.System.ConfigStorage.get_config_value(:bool, "settings", "firmware_input_log") do
+      Logger.success 2, "Movement to (#{pos.x}, #{pos.y}, #{pos.z}) complete."
     end
   end
 end
