e44bc514d7e9558d9f88aed583a63fe6024156e3
Connor Rigby <connor@farmbot.io>
2017-12-09 12:51:58 -0800

    Fix move absolute speed multiplication

--- lib/farmbot/celery_script/ast/node/move_absolute.ex
+++ lib/farmbot/celery_script/ast/node/move_absolute.ex
@@ -11,10 +11,10 @@ defmodule Farmbot.CeleryScript.AST.Node.MoveAbsolute do
          {:ok, pos_b} <- ast_to_vec3(offset)
     do
       pos = vec3_math(pos_a, :+, pos_b)
-      speed_x = speed * (Farmbot.BotState.get_param(:movement_max_spd_x) || 1)
-      speed_y = speed * (Farmbot.BotState.get_param(:movement_max_spd_y) || 1)
-      speed_z = speed * (Farmbot.BotState.get_param(:movement_max_spd_z) || 1)
-      case Farmbot.Firmware.move_absolute(pos, speed_x, speed_y, speed_z) do
+      speed_x = (speed / 100) * (Farmbot.BotState.get_param(:movement_max_spd_x) || 1)
+      speed_y = (speed / 100) * (Farmbot.BotState.get_param(:movement_max_spd_y) || 1)
+      speed_z = (speed / 100) * (Farmbot.BotState.get_param(:movement_max_spd_z) || 1)
+      case Farmbot.Firmware.move_absolute(pos, speed_x |> round(), speed_y |> round(), speed_z |> round()) do
         :ok -> {:ok, env}
         {:error, reason} -> {:error, reason, env}
       end
