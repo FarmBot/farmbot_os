066f401732a833f941e61bd52ddedd6f32b6afac
Connor Rigby <connor@farmbot.io>
2017-12-11 12:31:33 -0800

    add more logs for  reset and poweroff

--- lib/farmbot/celery_script/ast/node/power_off.ex
+++ lib/farmbot/celery_script/ast/node/power_off.ex
@@ -5,6 +5,8 @@ defmodule Farmbot.CeleryScript.AST.Node.PowerOff do
 
   def execute(_, _, env) do
     env = mutate_env(env)
+    Farmbot.BotState.set_sync_status(:maintenance)
+    Farmbot.BotState.force_state_push()
     Farmbot.System.shutdown("CeleryScript request")
     {:ok, env}
   end
--- lib/farmbot/celery_script/ast/node/reboot.ex
+++ lib/farmbot/celery_script/ast/node/reboot.ex
@@ -7,6 +7,8 @@ defmodule Farmbot.CeleryScript.AST.Node.Reboot do
   def execute(_, _, env) do
     env = mutate_env(env)
     Logger.warn 1, "Going down for a reboot!"
+    Farmbot.BotState.set_sync_status(:maintenance)
+    Farmbot.BotState.force_state_push()
     Farmbot.System.reboot("CeleryScript request.")
     {:ok, env}
   end
