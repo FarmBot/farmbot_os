1c183d47fe1dc6e5d1baad9e8e8c410235159af4
connor rigby <connor@farmbot.io>
2017-12-19 12:40:48 -0800

    Cleanup farmware supervisor.

--- lib/farmbot/farmware/supervisor.ex
+++ lib/farmbot/farmware/supervisor.ex
@@ -1,13 +1,15 @@
 defmodule Farmbot.Farmware.Supervisor do
   @moduledoc false
   use Supervisor
+  alias Farmbot.Farmware.Installer.Repository.SyncTask
 
   @doc false
   def start_link do
     Supervisor.start_link(__MODULE__, [], [name: __MODULE__])
   end
 
+  @doc false
   def init([]) do
-    Supervisor.init([Farmbot.Farmware.Installer.Repository.SyncTask], strategy: :one_for_one)
+    Supervisor.init([SyncTask], strategy: :one_for_one)
   end
 end
