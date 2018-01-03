970f6bce15d79b4f140ce63a7675e24259809e39
connor rigby <connor@farmbot.io>
2018-01-03 15:04:45 -0800

    Make sure to reboot if called from fe

--- lib/farmbot/celery_script/ast/node/check_updates.ex
+++ lib/farmbot/celery_script/ast/node/check_updates.ex
@@ -5,7 +5,7 @@ defmodule Farmbot.CeleryScript.AST.Node.CheckUpdates do
 
   def execute(%{package: :farmbot_os}, _, env) do
     env = mutate_env(env)
-    case Farmbot.System.Updates.check_updates() do
+    case Farmbot.System.Updates.check_updates(true) do
       :ok -> {:ok, env}
       :no_update -> {:ok, env}
       _ -> {:error, "Failed to check updates", env}
