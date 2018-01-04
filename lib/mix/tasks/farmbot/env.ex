c26743612de8e6e758e04ce9267dd41165b91fd6
connor rigby <connor@farmbot.io>
2018-01-04 11:12:11 -0800

    Add task for build a production image

--- lib/mix/tasks/farmbot/env.ex
+++ lib/mix/tasks/farmbot/env.ex
@@ -38,6 +38,43 @@ defmodule Mix.Tasks.Farmbot.Env do
   end
 
   @doc false
+  def format_date_time(%{ctime: {{yr, m, day}, {hr, min, sec}}}) do
+    dt =
+      %DateTime{
+        hour: hr,
+        year: yr,
+        month: m,
+        day: day,
+        minute: min,
+        second: sec,
+        time_zone: "Etc/UTC",
+        zone_abbr: "UTC",
+        std_offset: 0,
+        utc_offset: 0
+      }
+      |> Timex.local()
+
+    "#{dt.year}-#{pad(dt.month)}-#{pad(dt.day)}_#{pad(dt.hour)}#{pad(dt.minute)}"
+  end
+
+  defp pad(int) do
+    if int < 10, do: "0#{int}", else: "#{int}"
+  end
+
+  @doc false
+  def build_comment(time, comment) do
+    """
+    *New Farmbot Firmware!*
+    > *_Env_*:       `#{env()}`
+    > *_Target_*:    `#{target()}`
+    > *_Version_*:   `#{mix_config(:version)}`
+    > *_Commit_*:    `#{mix_config(:commit)}`
+    > *_Time_*:      `#{time}`
+    #{String.trim(comment)}
+    """
+  end
+
+  @doc false
   def slack_token do
     System.get_env("SLACK_TOKEN") || Mix.raise "No $SLACK_TOKEN environment variable."
   end
