76fa849a2e077eea3d5544aa924aae79512222d9
connor rigby <connor@farmbot.io>
2018-01-05 11:11:12 -0800

    Try caching arduino

--- .circleci/setup-arduino.sh
+++ .circleci/setup-arduino.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 ARDUINO_VERSION=1.8.5
-wget https://downloads.arduino.cc/arduino-$ARDUINO_VERSION-linux64.tar.xz
-tar xf arduino-$ARDUINO_VERSION-linux64.tar.xz
-mv arduino-$ARDUINO_VERSION $HOME
+[ "$(ls -A $HOME/arduino-$ARDUINO_VERSION)" ]; then
+  wget https://downloads.arduino.cc/arduino-$ARDUINO_VERSION-linux64.tar.xz
+  tar xf arduino-$ARDUINO_VERSION-linux64.tar.xz
+  mv arduino-$ARDUINO_VERSION $HOME
+else
+  echo "arduino $ARDUINO_VERSION already installed"
+fi
