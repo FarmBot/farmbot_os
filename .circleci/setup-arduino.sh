729e88bfa0a707dadf9ccfec2984bc83945097a4
connor rigby <connor@farmbot.io>
2018-01-05 11:17:13 -0800

    Fix installer script

--- .circleci/setup-arduino.sh
+++ .circleci/setup-arduino.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ARDUINO_VERSION=1.8.5
-[ "$(ls -A $HOME/arduino-$ARDUINO_VERSION)" ]; then
+if [ ! -d "$HOME/arduino-$ARDUINO_VERSION" ]; then
   wget https://downloads.arduino.cc/arduino-$ARDUINO_VERSION-linux64.tar.xz
   tar xf arduino-$ARDUINO_VERSION-linux64.tar.xz
   mv arduino-$ARDUINO_VERSION $HOME
