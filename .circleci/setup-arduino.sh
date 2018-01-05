ee55346c92d2225405b97d876f8719ceac8a24e2
connor rigby <connor@farmbot.io>
2018-01-05 10:56:03 -0800

    fix naming

---
+++ .circleci/setup-arduino.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ARDUINO_VERSION=1.8.5
+wget https://downloads.arduino.cc/arduino-$ARDUINO_VERSION-linux64.tar.xz
+tar xf arduino-$ARDUINO_VERSION-linux64.tar.xz
+mv arduino-$ARDUINO_VERSION $HOME
