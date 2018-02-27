2c7336f42e8927c637cbe8bf6719842c8fb4f763
connor rigby <connor@farmbot.io>
2018-02-27 10:17:54 -0800

    Bump versions

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Farmbot.Mixfile do
       target: @target,
       commit: commit(),
       arduino_commit: arduino_commit(),
-      archives: [nerves_bootstrap: "~> 0.8.0"],
+      archives: [nerves_bootstrap: "~> 0.8.2"],
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps_path: "deps/#{@target}",
