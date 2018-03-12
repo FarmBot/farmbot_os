c563d2ff3c0f4c931dad4ea04333359b701e69a3
connor rigby <connor@farmbot.io>
2018-03-12 13:29:45 -0700

    Fix compiler warning

--- lib/farmbot/celery_script/ast/node/toggle_pin.ex
+++ lib/farmbot/celery_script/ast/node/toggle_pin.ex
@@ -18,7 +18,7 @@ defmodule Farmbot.CeleryScript.AST.Node.TogglePin do
         Logger.warn 2, "Unknown pin value. Setting to zero."
         args = %{pin_mode: :digital, pin_number: num, pin_value: 0}
         Node.WritePin.execute(args, [], env)
-      {:ok, val} ->
+      {:ok, _val} ->
         {:error, "Can't toggle analog pin", env}
       {:error, reason} -> {:error, reason, env}
     end
