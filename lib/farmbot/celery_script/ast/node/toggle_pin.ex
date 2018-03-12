8c94ccb7b5a5e3f7f55f3f8809bf9b7dd5b39b55
connor rigby <connor@farmbot.io>
2018-03-12 15:32:18 -0700

    Clean that up a bit.

--- lib/farmbot/celery_script/ast/node/toggle_pin.ex
+++ lib/farmbot/celery_script/ast/node/toggle_pin.ex
@@ -8,17 +8,28 @@ defmodule Farmbot.CeleryScript.AST.Node.TogglePin do
   def execute(%{pin_number: num}, _, env) do
     env = mutate_env(env)
     case Farmbot.BotState.get_pin_value(num) do
-      {:ok, 0}  ->
-        args = %{pin_mode: :digital, pin_number: num, pin_value: 1}
-        Node.WritePin.execute(args, [], env)
-      {:ok, 1}  ->
-        args = %{pin_mode: :digital, pin_number: num, pin_value: 0}
-        Node.WritePin.execute(args, [], env)
-      res when res == {:ok, _} or res == {:error, :unknown_pin} ->
-        Logger.warn 2, "Unknown pin value. Setting to zero."
-        args = %{pin_mode: :digital, pin_number: num, pin_value: 0}
-        Node.WritePin.execute(args, [], env)
+      {:ok, 0}  -> high(env, num)
+      {:ok, 1}  -> low(env, num)
+      {:ok, _} -> unknown(env, num)
+      {:error, :unknown_pin} -> unknown(env, num)
       {:error, reason} -> {:error, reason, env}
     end
   end
+
+  defp unknown(env, num) do
+    Logger.warn 2, "Unknown pin value. Setting to zero."
+    low(mutate_env(env), num)
+  end
+
+  defp high(env, num) do
+    args = %{pin_mode: :digital, pin_number: num, pin_value: 1}
+    jump(mutate_env(env), args)
+  end
+
+  defp low(env, num) do
+    args = %{pin_mode: :digital, pin_number: num, pin_value: 0}
+    jump(mutate_env(env), args)
+  end
+
+  defp jump(env, args), do: Node.WritePin.execute(args, [], mutate_env(env))
 end
