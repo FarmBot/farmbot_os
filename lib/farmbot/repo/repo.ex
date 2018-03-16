e8454e9bf7842f2169520552cdb08ecdcfab71eb
connor rigby <connor@farmbot.io>
2018-03-16 13:48:40 -0700

    Check repo is up before responding to GPIO.

--- lib/farmbot/repo/repo.ex
+++ lib/farmbot/repo/repo.ex
@@ -237,6 +237,7 @@ defmodule Farmbot.Repo do
        end)
 
     Farmbot.FarmEvent.Manager.register_events repo.all(Farmbot.Asset.FarmEvent)
+    Farmbot.System.GPIO.confirm_asset_storage_up()
     :ok
   end
 
