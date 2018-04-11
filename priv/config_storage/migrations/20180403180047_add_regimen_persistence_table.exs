1d6bd90ef865865db07c0e6c5b9ea3518b911844
connor rigby <connor@farmbot.io>
2018-04-11 13:12:35 -0700

    Whoops forgot i put out a beta with that migration.

--- priv/config_storage/migrations/20180403180047_add_regimen_persistence_table.exs
+++ priv/config_storage/migrations/20180403180047_add_regimen_persistence_table.exs
@@ -4,9 +4,8 @@ defmodule Farmbot.System.ConfigStorage.Migrations.AddRegimenPersistenceTable do
   def change do
     create table("persistent_regimens") do
       add :regimen_id, :integer
-      add :farm_event_id, :integer
       add :time, :utc_datetime
     end
-    create unique_index("persistent_regimens", [:regimen_id, :time, :farm_event_id], name: :regimen_start_time)
+    unique_index("persistent_regimens", :regimen_id)
   end
 end
