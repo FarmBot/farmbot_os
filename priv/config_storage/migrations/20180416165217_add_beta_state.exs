b4f0b0767dcc13b4bb117d4176b48da2dcc283e1
connor rigby <connor@farmbot.io>
2018-04-16 11:24:43 -0700

    Fix missing import

--- priv/config_storage/migrations/20180416165217_add_beta_state.exs
+++ priv/config_storage/migrations/20180416165217_add_beta_state.exs
@@ -1,5 +1,6 @@
 defmodule Farmbot.System.ConfigStorage.Migrations.AddBetaState do
   use Ecto.Migration
+  import Farmbot.System.ConfigStorage.MigrationHelpers
 
   def change do
     create_settings_config("currently_on_beta", :bool, false)
