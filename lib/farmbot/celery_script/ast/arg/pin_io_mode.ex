db5c2829f6cbe40b9b0fe6dfe5292b8d2abe3a79
connor rigby <connor@farmbot.io>
2018-05-08 15:33:15 -0700

    Fix pin io mode type.

--- lib/farmbot/celery_script/ast/arg/pin_io_mode.ex
+++ lib/farmbot/celery_script/ast/arg/pin_io_mode.ex
@@ -2,11 +2,11 @@ defmodule Farmbot.CeleryScript.AST.Arg.PinIoMode do
   @moduledoc false
   @behaviour Farmbot.CeleryScript.AST.Arg
 
-  def decode(0x0), do: :input
-  def decode(0x2), do: :input_pullup
-  def decode(0x1), do: :output
+  def decode(0x0), do: {:ok, :input}
+  def decode(0x2), do: {:ok, :input_pullup}
+  def decode(0x1), do: {:ok, :output}
 
-  def encode(:input), do: 0x0
-  def encode(:input_pullup), do: 0x2
-  def encode(:output), do: 0x1
+  def encode(:input), do: {:ok, 0x0}
+  def encode(:input_pullup), do: {:ok, 0x2}
+  def encode(:output), do: {:ok, 0x1}
 end
