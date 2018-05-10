52cd5b026b6b74412174ffe487a39c5a95b16a8b
connor rigby <connor@farmbot.io>
2018-05-10 16:37:17 -0700

    false != nil

--- priv/config_storage/migrations/20180416165217_add_beta_state.exs
+++ priv/config_storage/migrations/20180416165217_add_beta_state.exs
@@ -1,8 +1,9 @@
 defmodule Farmbot.System.ConfigStorage.Migrations.AddBetaState do
   use Ecto.Migration
   import Farmbot.System.ConfigStorage.MigrationHelpers
+
   @default_currently_on_beta Application.get_env(:farmbot, :default_currently_on_beta)
-  @default_currently_on_beta || raise("Missing application env config: `:default_currently_on_beta`")
+  if is_nil(@default_currently_on_beta), do: raise("Missing application env config: `:default_currently_on_beta`")
 
   def change do
     create_settings_config("currently_on_beta", :bool, @default_currently_on_beta)
