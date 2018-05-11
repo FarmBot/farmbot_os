2d981ab513ad6bb0fd371c75ec1900cad70cef87
connor rigby <connor@farmbot.io>
2018-05-11 16:15:47 -0700

    Save logs if farmbot applicaiton stops

--- platform/target/system_tasks.ex
+++ platform/target/system_tasks.ex
@@ -2,6 +2,8 @@ defmodule Farmbot.Target.SystemTasks do
   @moduledoc "Target implementation for System Tasks."
 
   @behaviour Farmbot.System
+  @block_device "/dev/sda1"
+  @mount_point "/tmp/log_dev"
 
   def factory_reset(reason) do
     reboot(reason)
@@ -15,7 +17,19 @@ defmodule Farmbot.Target.SystemTasks do
     Nerves.Runtime.poweroff()
   end
 
-  def stop(_data) do
-    :ok
+  def stop(data) do
+    System.cmd("umount", [@mount_point])
+    if File.exists?(@block_device) do
+      IO.puts "Found Block device #{@block_device}"
+      File.mkdir_p!(@mount_point)
+      case System.cmd("mount", [@block_device, @mount_point]) do
+        {_, 0} ->
+          File.write(Path.join([@mount_point, "logs", DateTime.utc_now() |> to_string(), "logs.txt"]), data, [:sync])
+          System.cmd("umount", [@mount_point])
+          :ok
+        {reason, code} ->
+          IO.puts "Could not mount device #{code}: #{inspect reason}"
+      end
+    end
   end
 end
