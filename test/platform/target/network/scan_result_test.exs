43be09f00f2282c66fe5c269e17315695bcafe85
Paulo Gonzalez <pdgonzalez872@gmail.com>
2018-05-14 23:46:16 -0500

    Update test and allow it to run correctly. We have issues with compiling in the test directory.
    Can successfully run:
    `mix test test/platform/target/network/scan_result_test.exs --trace`

--- test/platform/target/network/scan_result_test.exs
+++ test/platform/target/network/scan_result_test.exs
@@ -3,11 +3,36 @@ defmodule Farmbot.Target.Network.ScanResultTest do
 
   alias Farmbot.Target.Network.ScanResult
 
-  describe "hi" do
-    test "nice" do
-      require IEx; IEx.pry
+  describe "decode/1" do
+    test "returns a an empty struct when we pass in an empty map" do
       result = ScanResult.decode(%{})
-      assert result == nil
+      expected = %Farmbot.Target.Network.ScanResult{
+                                                     bssid: nil,
+                                                     capabilities: nil,
+                                                     flags: nil,
+                                                     level: nil,
+                                                     noise: nil,
+                                                     security: nil,
+                                                     ssid: nil
+                                                   }
+      assert result == expected
+      assert result.__struct__ == Farmbot.Target.Network.ScanResult
+    end
+
+    test "returns a list of structs when we pass in empty maps" do
+      list_of_maps = [%{}, %{}]
+      result = ScanResult.decode(list_of_maps)
+      first_struct = Enum.at(result, 0)
+
+      assert is_list(result) == true
+      assert first_struct.__struct__ == Farmbot.Target.Network.ScanResult
+      assert Enum.count(result) == Enum.count(list_of_maps)
+    end
+  end
+
+  describe "sort_results/1" do
+    test "returns a sorted list of ssids by their level" do
+      "..."
     end
   end
 end
