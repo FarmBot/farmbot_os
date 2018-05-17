f6265564e40c58b5675544b734e67466f1e19c92
connor rigby <connor@farmbot.io>
2018-05-17 10:23:36 -0700

    Format things.

--- .formatter.exs
+++ .formatter.exs
@@ -1,8 +1,9 @@
 # Used by "mix format"
-known_formatted_files = File.read!("formatted_files")
+known_formatted_files =
+  File.read!("formatted_files")
   |> String.split("\n")
-  |> Enum.map(&String.trim())
-
+  |> Enum.map(&String.trim(&1))
+  |> List.delete("")
 
 [
   inputs: ["mix.exs", ".formatter.exs"] ++ known_formatted_files,
