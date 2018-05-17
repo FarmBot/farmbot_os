f7c1edb01d965d0772eb09f83cf622b9515aa465
Connor Rigby <connor@farmbot.io>
2018-05-17 09:11:42 -0700

    Add formatter rules

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,10 @@
 # Used by "mix format"
+known_formatted_files = File.read!("formatted_files")
+  |> String.split("\n")
+  |> Enum.map(&String.trim())
+
+
 [
-  inputs: ["mix.exs", "{config,lib,test,platform}/**/*.{ex,exs}"],
+  inputs: ["mix.exs", ".formatter.exs"] ++ known_formatted_files,
   line_length: 80
 ]
