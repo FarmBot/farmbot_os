f191a3c86344eff60993097b4308092d675ad175
connor rigby <connor@farmbot.io>
2018-07-13 10:04:46 -0700

    Change read/write pin to print names of peripherals and sensors

--- lib/farmbot/celery_script/ast/node/write_pin.ex
+++ lib/farmbot/celery_script/ast/node/write_pin.ex
@@ -9,49 +9,56 @@ defmodule Farmbot.CeleryScript.AST.Node.WritePin do
 
   allow_args [:pin_number, :pin_value, :pin_mode]
 
-  def execute(%{pin_number: %AST{kind: NamedPin} = named_pin, pin_mode: mode, pin_value: val}, body, env) do
+  def execute(%{pin_number: %AST{kind: NamedPin} = named_pin, pin_mode: mode, pin_value: val}, _body, env) do
     env = mutate_env(env)
     id = named_pin.args.pin_id
     type = named_pin.args.pin_type
     case fetch_resource(type, id) do
-      {:ok, number} ->
-        execute(%{pin_number: number, pin_mode: mode, pin_value: val}, body, env)
+      {:ok, %Peripheral{pin: num, label: name}} ->
+        do_write(env, num, mode, val, "Peripheral #{name}")
+      {:ok, %Sensor{pin: num, label: name}} ->
+        do_write(env, num, mode, val, "Sensor #{name}")
       {:error, reason} -> {:error, reason, env}
     end
   end
 
   def execute(%{pin_mode: mode, pin_value: value, pin_number: num}, [], env) do
     env = mutate_env(env)
+    do_write(env, num, mode, value, "pin")
+  end
+
+  defp do_write(env, num, mode, value, msg) do
+    env = mutate_env(env)
     case Farmbot.Firmware.write_pin(num, mode, value) do
       :ok ->
-        log_success(num, mode, value)
+        log_success(msg, num, mode, value)
         {:ok, env}
       {:error, reason} -> {:error, reason, env}
     end
   end
 
-  defp log_success(num, :digital, 1) do
-    Logger.success 1, "Pin #{num} turned on"
+  defp log_success(msg, num, :digital, 1) do
+    Logger.success 1, "Write #{msg} #{num} ON (1)"
   end
 
-  defp log_success(num, :digital, 0) do
-    Logger.success 1, "Pin #{num} turned off"
+  defp log_success(msg, num, :digital, 0) do
+    Logger.success 1, "Write #{msg} #{num} OFF (0)"
   end
 
-  defp log_success(num, _, val) do
-    Logger.success 1, "Pin #{num}: #{val} (analog)"
+  defp log_success(msg, num, _, val) do
+    Logger.success 1, "Write #{msg} #{num}: #{val} (analog)"
   end
 
   defp fetch_resource(Peripheral, id) do
     case Asset.get_peripheral_by_id(id) do
-      %Peripheral{pin: number} -> {:ok, number}
+      %Peripheral{} = per -> {:ok, per}
       nil -> {:error, "Could not find pin by id: #{id}"}
     end
   end
 
   defp fetch_resource(Sensor, id) do
     case Asset.get_sensor_by_id(id) do
-      %Sensor{pin: number} -> {:ok, number}
+      %Sensor{} = sen -> {:ok, sen}
       nil -> {:error, "Could not find pin by id: #{id}"}
     end
   end
