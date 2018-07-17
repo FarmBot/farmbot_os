8e5c0e2d934ee2a12b1d26d9eb489415ceebf4da
connor rigby <connor@farmbot.io>
2018-07-17 13:03:00 -0700

    Fix dump info

--- lib/farmbot/celery_script/ast/node/dump_info.ex
+++ lib/farmbot/celery_script/ast/node/dump_info.ex
@@ -4,13 +4,14 @@ defmodule Farmbot.CeleryScript.AST.Node.DumpInfo do
   allow_args []
   def execute(%{}, [], env) do
     data = %{
+      fbos_commit: Farmbot.Project.commit(),
       fbos_version: Farmbot.Project.version(),
       firmware_commit: Farmbot.Project.arduino_commit(),
       network_interface: Farmbot.System.ConfigStorage.get_all_network_configs |> Enum.at(0) |> Map.get(:name),
       fbos_dmesg_dump: System.cmd("dmesg", []) |> elem(0),
       firmware_state: Application.get_env(:farmbot, :behaviour)[:firmware_handler] |> to_string()
     }
-    json = Farmbot.JSON.encode(data)
+    json = Farmbot.JSON.encode!(data)
     case Farmbot.HTTP.post("/api/diagnostic_dumps", json) do
       {:ok, _} -> {:ok, env}
       {:error, _} -> {:error, "post failed", env}
