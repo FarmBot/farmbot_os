4edd6daa7ec746d8f66847eeab988f71d739b604
connor rigby <connor@farmbot.io>
2018-07-23 08:49:29 -0700

    Implement Box Leds

--- lib/farmbot/celery_script/ast/arg/pin_type.ex
+++ lib/farmbot/celery_script/ast/arg/pin_type.ex
@@ -5,6 +5,8 @@ defmodule Farmbot.CeleryScript.AST.Arg.PinType do
 
   def decode("Peripheral"), do: {:ok, Peripheral}
   def decode("Sensor"),     do: {:ok, Sensor}
+  def decode("BoxLed3"),    do: {:ok, "BoxLed3"}
+  def decode("BoxLed4"),    do: {:ok, "BoxLed4"}
 
   def encode(Peripheral),   do: {:ok, "Peripheral"}
   def encode(Sensor),       do: {:ok, "Sensor"}
