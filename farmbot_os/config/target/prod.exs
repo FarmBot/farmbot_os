456683178892d4ea1752650a84a843e292a8b53e
Connor Rigby <connor@farmbot.io>
2018-08-04 23:41:49 -0700

    Fix Sqlite bug and increase performance by 10 times!!

--- farmbot_os/config/target/prod.exs
+++ farmbot_os/config/target/prod.exs
@@ -14,20 +14,17 @@ config :farmbot_ext,
 config :farmbot_core, Farmbot.Config.Repo,
   adapter: Sqlite.Ecto2,
   loggers: [],
-  database: Path.join(data_path, "config-#{Mix.env()}.sqlite3"),
-  pool_size: 1
+  database: Path.join(data_path, "config-#{Mix.env()}.sqlite3")
 
 config :farmbot_core, Farmbot.Logger.Repo,
   adapter: Sqlite.Ecto2,
   loggers: [],
-  database: Path.join(data_path, "logs-#{Mix.env()}.sqlite3"),
-  pool_size: 1
+  database: Path.join(data_path, "logs-#{Mix.env()}.sqlite3")
 
 config :farmbot_core, Farmbot.Asset.Repo,
   adapter: Sqlite.Ecto2,
   loggers: [],
-  database: Path.join(data_path, "repo-#{Mix.env()}.sqlite3"),
-  pool_size: 1
+  database: Path.join(data_path, "repo-#{Mix.env()}.sqlite3")
 
 config :farmbot_os,
   ecto_repos: [Farmbot.Config.Repo, Farmbot.Logger.Repo, Farmbot.Asset.Repo],
