4f70aeff92244cc0416b7969fe9bee85b25a8493
connor rigby <connor@farmbot.io>
2018-08-15 05:59:02 -0700

    Work toward farmware/celeryscript stuff

--- farmbot_ext/lib/http/error.ex
+++ farmbot_ext/lib/http/error.ex
@@ -1,16 +1,21 @@
 defmodule Farmbot.HTTP.Error do
   @moduledoc "Some Farmbot HTTP Adapter error."
 
-  alias Farmbot.HTTP.{Response}
+  alias Farmbot.HTTP.{Response, Error}
 
   defexception [:message, :response]
 
   @doc false
   def exception(desc) when is_atom(desc) or is_binary(desc) do
-    %__MODULE__{message: String.trim(desc)}
+    %Error{message: String.trim(desc)}
   end
 
   def exception(%Response{status_code: code, body: body} = resp) do
-    %__MODULE__{message: "HTTP Request failed (#{code}) body: #{body}", response: resp}
+    case Farmbot.JSON.decode(body) do
+      {:ok, %{"error" => reason}} ->
+        %Error{message: "HTTP Request failed (#{code}) #{reason}", response: resp}
+      _ ->
+        %Error{message: "HTTP Request failed (#{code})", response: resp}
+    end
   end
 end
--- farmbot_ext/lib/http/helpers.ex
+++ farmbot_ext/lib/http/helpers.ex
@@ -1,14 +1,19 @@
 defmodule Farmbot.HTTP.Helpers do
-  @moduledoc """
-    Helpful stuff.
-  """
+  @moduledoc false
 
-  @doc """
-    Helper for checking status codes
-  """
+  @doc "Helper for checking status codes"
   defmacro is_2xx(number) do
     quote do
       unquote(number) > 199 and unquote(number) < 300
+    end
+  end
+
+  # Defines a function to fetch and decode an api resource.
+  @doc "Helper to define fetch and decode resource function in Farmbot.HTTP"
+  defmacro fadr(plural, kind) do
+    quote do
+      def unquote(plural)(),
+        do: fetch_and_decode("/api/#{unquote(plural)}.json", unquote(kind))
     end
   end
 end
