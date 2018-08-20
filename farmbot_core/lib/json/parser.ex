9b5cf5d636d45da82a93a2ac0f4ab903feeca166
Connor Rigby <connor@farmbot.io>
2018-08-20 10:22:26 -0700

    Replace httpoison

--- farmbot_core/lib/json/parser.ex
+++ farmbot_core/lib/json/parser.ex
@@ -2,6 +2,6 @@ defmodule Farmbot.JSON.Parser do
   @moduledoc """
   Callback module for wrapping a json dependency.
   """
-  @callback decode(iodata) :: {:ok, term} | {:error, term}
-  @callback encode(term) :: {:ok, iodata} | {:error, term}
+  @callback decode(iodata, term) :: {:ok, term} | {:error, term}
+  @callback encode(term, term) :: {:ok, iodata} | {:error, term}
 end
