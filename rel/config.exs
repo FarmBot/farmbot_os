140ba4d50e8e1a6b4e5188cdcc65ac6746e9bb6b
connor rigby <connor@farmbot.io>
2018-08-28 07:26:56 -0700

    Bump to 1.7.3. Everything builds now. Haven't tested if it boots yet.

--- rel/config.exs
+++ rel/config.exs
@@ -28,12 +28,7 @@ end
 # will be used by default
 
 release :farmbot do
-  set(version: current_version(:farmbot))
+  set version: current_version(:farmbot)
   plugin Shoehorn
-  if System.get_env("NERVES_SYSTEM") do
-    set(dev_mode: false)
-    set(include_src: false)
-    set(include_erts: System.get_env("ERL_LIB_DIR"))
-    set(include_system_libs: System.get_env("ERL_SYSTEM_LIB_DIR"))
-  end
+  plugin Nerves
 end
