54941ed1454cdc0de97a558339ebf80775423165
Connor Rigby <connor@farmbot.io>
2018-09-10 13:57:48 -0700

    Fix target specific config.

--- mix.exs
+++ mix.exs
@@ -158,7 +158,10 @@ defmodule Farmbot.Mixfile do
   defp system("rpi3"),
     do: [{:nerves_system_farmbot_rpi3, "1.4.1-farmbot.0", runtime: false}]
 
-  defp package do
+  defp system("rpi0"),
+    do: [{:nerves_system_farmbot_rpi0, "1.4.0-farmbot.0", runtime: false}]
+  
+    defp package do
     [
       name: "farmbot",
       maintainers: ["Farmbot.io"],
