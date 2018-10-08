af8dfbb847be7778d029c9eab509e00166fa8b51
Connor Rigby <connor@farmbot.io>
2018-10-08 16:25:54 -0700

    Update nerves_runtime code

--- platform/target/uevent.ex
+++ platform/target/uevent.ex
@@ -23,54 +23,45 @@ defmodule Farmbot.Target.Uevent do
   end
 
   def init([]) do
-    executable = :code.priv_dir(:nerves_runtime) ++ '/uevent'
-    port = Port.open({:spawn_executable, executable},
-    [{:args, []},
-      {:packet, 2},
-      :use_stdio,
-      :binary,
-      :exit_status])
-
-    {:ok, %{port: port}}
+    :ok = SystemRegistry.register()
+    {:ok, nil}
   end
 
-  def handle_info({_, {:data, <<?n, message::binary>>}}, s) do
-    msg = :erlang.binary_to_term(message)
-    handle_port(msg, s)
-  end
-
-  defp handle_port({:uevent, _uevent, kv}, s) do
-    event =
-      Enum.reduce(kv, %{}, fn (str, acc) ->
-        [k, v] = String.split(str, "=", parts: 2)
-        k = String.downcase(k)
-        Map.put(acc, k, v)
-      end)
-    case Map.get(event, "devpath", "") do
-      "/devices" <> _path -> handle_uevent(event)
-      _ -> :noop
+  def handle_info({:system_registry, :global, new_reg}, %{} = old_reg) do
+    require IEx; IEx.pry
+    new_ttys = get_in(new_reg, [:state, "subsystems", "tty"]) || []
+    old_ttys = get_in(old_reg, [:state, "subsystems", "tty"]) || []
+    case new_ttys -- old_ttys do
+      [new]   -> new |> List.last() |> maybe_new_tty()
+      [_ | _] -> Logger.warn(3, "Multiple new tty devices detected. Ignoring.")
+      [] -> :ok
     end
-    {:noreply, s}
+    {:noreply, new_reg}
   end
 
-  defp handle_uevent(%{"action" => "add", "subsystem" => "tty", "devname" => tty}) do
-    if String.contains?(tty, "USB") || String.contains?(tty, "ACM") do
-      Logger.busy 3, "Detected new UART Device: #{tty}"
-      Application.put_env(:farmbot, :uart_handler, tty: "/dev/" <> tty)
-      old_env = Application.get_env(:farmbot, :behaviour)
+  def handle_info({:system_registry, :global, reg}, _) do
+    {:noreply, reg}
+  end
 
-      if old_env[:firmware_handler] == Farmbot.Firmware.StubHandler do
-        new_env = Keyword.put(old_env, :firmware_handler, Farmbot.Firmware.UartHandler)
-        Application.put_env(:farmbot, :behaviour, new_env)
-        if Process.whereis(Farmbot.Firmware) do
-          GenServer.stop(Farmbot.Firmware, :shutdown)
-        end
+  def maybe_new_tty("ttyUSB" <> _ = tty), do: new_tty(tty)
+  def maybe_new_tty("ttyACM" <> _ = tty), do: new_tty(tty)
+  def maybe_new_tty("ttyS" <> _), do: :ok
+  def maybe_new_tty("tty" <> _), do: :ok
+  def maybe_new_tty(unknown) do
+    Logger.warn 1, "Unknown tty: #{inspect(unknown)}"
+  end
+
+  def new_tty(tty) do
+    Logger.busy 3, "Detected new UART Device: #{tty}"
+    Application.put_env(:farmbot, :uart_handler, tty: "/dev/" <> tty)
+    old_env = Application.get_env(:farmbot, :behaviour)
+
+    if old_env[:firmware_handler] == Farmbot.Firmware.StubHandler do
+      new_env = Keyword.put(old_env, :firmware_handler, Farmbot.Firmware.UartHandler)
+      Application.put_env(:farmbot, :behaviour, new_env)
+      if Process.whereis(Farmbot.Firmware) do
+        GenServer.stop(Farmbot.Firmware, :shutdown)
       end
-    else
-      Logger.debug 3, "Ignoring system tty: #{tty}"
-      :ok
     end
   end
-
-  defp handle_uevent(_action), do: :ok
 end
