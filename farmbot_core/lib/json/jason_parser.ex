b3f1369a77c9bb020cd8dff534dbd043564ffd3a
Connor Rigby <connor@farmbot.io>
2018-10-31 13:32:30 -0700

    Re-implement image uploading

--- farmbot_core/lib/json/jason_parser.ex
+++ farmbot_core/lib/json/jason_parser.ex
@@ -4,13 +4,4 @@ defmodule Farmbot.JSON.JasonParser do
 
   def decode(data, opts), do: Jason.decode(data, opts)
   def encode(data, opts), do: Jason.encode(data, opts)
-
-  require Protocol
-  # Bot State
-  Protocol.derive Jason.Encoder, Farmbot.BotState
-  Protocol.derive Jason.Encoder, Farmbot.BotState.Configuration
-  Protocol.derive Jason.Encoder, Farmbot.BotState.InformationalSettings
-  Protocol.derive Jason.Encoder, Farmbot.BotState.LocationData
-  Protocol.derive Jason.Encoder, Farmbot.BotState.McuParams
-  Protocol.derive Jason.Encoder, Farmbot.BotState.Pin
 end
