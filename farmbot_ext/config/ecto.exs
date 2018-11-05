3b8f8d591faefc5165833c5594d2647f12e54db5
Connor Rigby <connor@farmbot.io>
2018-11-05 08:18:26 -0800

    Config + dead code cleanup

---
+++ farmbot_ext/config/ecto.exs
@@ -0,0 +1,24 @@
+use Mix.Config
+
+config :farmbot_ext,
+  ecto_repos: [Farmbot.Config.Repo, Farmbot.Logger.Repo, Farmbot.Asset.Repo]
+
+config :ecto, json_library: Farmbot.JSON
+
+config :farmbot_core, Farmbot.Config.Repo,
+  adapter: Sqlite.Ecto2,
+  loggers: [],
+  database: "config.#{Mix.env()}.db",
+  priv: "../farmbot_core/priv/config"
+
+config :farmbot_core, Farmbot.Logger.Repo,
+  adapter: Sqlite.Ecto2,
+  loggers: [],
+  database: "logger.#{Mix.env()}.db",
+  priv: "../farmbot_core/priv/logger"
+
+config :farmbot_core, Farmbot.Asset.Repo,
+  adapter: Sqlite.Ecto2,
+  loggers: [],
+  database: "asset.#{Mix.env()}.db",
+  priv: "../farmbot_core/priv/asset"
