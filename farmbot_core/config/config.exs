8b258b6b613f76e9dd08e2c3c468b78abaf0e8da
Connor Rigby <connor@farmbot.io>
2018-11-05 08:18:26 -0800

    Config + dead code cleanup

--- farmbot_core/config/config.exs
+++ farmbot_core/config/config.exs
@@ -1,10 +1,22 @@
 use Mix.Config
 
-# config :logger,
-#   handle_otp_reports: true,
-#   handle_sasl_reports: true
+config :farmbot_core, Farmbot.AssetWorker.Farmbot.Asset.FarmEvent,
+  checkup_time_ms: 10_000
 
-config :ecto, json_library: Farmbot.JSON
+config :farmbot_core, Farmbot.AssetWorker.Farmbot.Asset.FarmwareInstallation,
+  error_retry_time_ms: 30_000,
+  install_dir: "/tmp/farmware"
+
+config :farmbot_core, Farmbot.AssetMonitor,
+  checkup_time_ms: 30_000
+
+config :farmbot_core,
+  expected_fw_versions: ["6.4.2.F", "6.4.2.R", "6.4.2.G"],
+  default_server: "https://my.farm.bot",
+  default_currently_on_beta:
+    String.contains?(to_string(:os.cmd('git rev-parse --abbrev-ref HEAD')), "beta"),
+  firmware_io_logs: false,
+  farm_event_debug_log: false
 
 # Configure Farmbot Behaviours.
 config :farmbot_core, :behaviour,
@@ -14,48 +26,6 @@ config :farmbot_core, :behaviour,
   celery_script_io_layer: Farmbot.Core.CeleryScript.StubIOLayer,
   json_parser: Farmbot.JSON.JasonParser
 
-config :farmbot_core, Farmbot.AssetWorker.Farmbot.Asset.FarmEvent, checkup_time_ms: 10_000
-
-config :farmbot_core, Farmbot.AssetWorker.Farmbot.Asset.FarmwareInstallation,
-  error_retry_time_ms: 30_000,
-  install_dir: "/tmp/farmware"
-
-config :farmbot_core, Farmbot.AssetMonitor, checkup_time_ms: 30_000
-
-if Mix.env() == :test do
-  config :farmbot_core, :behaviour,
-    celery_script_io_layer: Farmbot.TestSupport.CeleryScript.TestIOLayer
-
-  config :farmbot_core, Farmbot.AssetWorker.Farmbot.Asset.FarmEvent, checkup_time_ms: 1000
-
-  # must be lower than other timers
-  # To ensure other timers have time to timeout
-  config :farmbot_core, Farmbot.AssetMonitor, checkup_time_ms: 500
-end
-
-config :farmbot_core,
-  ecto_repos: [Farmbot.Config.Repo, Farmbot.Logger.Repo, Farmbot.Asset.Repo],
-  expected_fw_versions: ["6.4.2.F", "6.4.2.R", "6.4.2.G"],
-  default_server: "https://my.farm.bot",
-  default_currently_on_beta:
-    String.contains?(to_string(:os.cmd('git rev-parse --abbrev-ref HEAD')), "beta"),
-  firmware_io_logs: false,
-  farm_event_debug_log: false
-
-config :farmbot_core, Farmbot.Config.Repo,
-  adapter: Sqlite.Ecto2,
-  loggers: [],
-  database: ".#{Mix.env()}_configs.sqlite3",
-  priv: "priv/config"
-
-config :farmbot_core, Farmbot.Logger.Repo,
-  adapter: Sqlite.Ecto2,
-  loggers: [],
-  database: ".#{Mix.env()}_logs.sqlite3",
-  priv: "priv/logger"
-
-config :farmbot_core, Farmbot.Asset.Repo,
-  adapter: Sqlite.Ecto2,
-  loggers: [],
-  database: ".#{Mix.env()}_assets.sqlite3",
-  priv: "priv/asset"
+import_config "ecto.exs"
+import_config "logger.exs"
+import_config "#{Mix.env()}.exs"
--- farmbot_core/lib/ecto_types/json_type.ex
+++
@@ -1,7 +0,0 @@
-defmodule Farmbot.EctoTypes.JSONType do
-  @behaviour Ecto.Type
-  def type, do: :json
-  def cast(any), do: {:ok, any}
-  def load(value), do: Farmbot.JSON.decode(value)
-  def dump(value), do: Farmbot.JSON.encode(value)
-end
