ee290e6f6c8e766cf08491468e46513984b23c95
Connor Rigby <connor@farmbot.io>
2018-11-19 11:36:32 -0800

    Add formatting and Timezone to CI

--- test/support/test_support.ex
+++ test/support/test_support.ex
@@ -1,15 +1,12 @@
 defmodule Farmbot.TestSupport do
   alias Farmbot.AssetWorker.Farmbot.Asset.{FarmEvent, PersistentRegimen}
+
   def farm_event_timeout do
-    Application.get_env(:farmbot_core, FarmEvent)[
-      :checkup_time_ms
-    ]
+    Application.get_env(:farmbot_core, FarmEvent)[:checkup_time_ms]
   end
 
   def persistent_regimen_timeout do
-    Application.get_env(:farmbot_core, PersistentRegimen)[
-      :checkup_time_ms
-    ]
+    Application.get_env(:farmbot_core, PersistentRegimen)[:checkup_time_ms]
   end
 
   def asset_monitor_timeout do
