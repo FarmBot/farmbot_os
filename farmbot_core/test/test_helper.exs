de660c3d53b6617d50fd24fa7902b088476d40ee
Connor Rigby <connor@farmbot.io>
2018-11-19 12:10:20 -0800

    Update TZ from env?

--- farmbot_core/test/test_helper.exs
+++ farmbot_core/test/test_helper.exs
@@ -4,7 +4,7 @@
 # Ecto.Adapters.SQL.Sandbox.mode(Farmbot.Config.Repo, :auto)
 # Ecto.Adapters.SQL.Sandbox.mode(Farmbot.Logger.Repo, {:shared, self()})
 # Ecto.Adapters.SQL.Sandbox.mode(Farmbot.Asset.Repo,  :auto)
-tz = Timex.local().time_zone
+tz = System.get_env("TZ") || Timex.local().time_zone
 
 Farmbot.Asset.Device.changeset(Farmbot.Asset.device(), %{timezone: tz})
 |> Farmbot.Asset.Repo.insert_or_update!()
