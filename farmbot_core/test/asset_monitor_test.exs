01a2d44ea3a9d3497740ef2200041d6e74acccd2
Connor Rigby <connor@farmbot.io>
2018-11-19 14:00:32 -0800

    Refactor tests to stop juggling timeouts

--- farmbot_core/test/asset_monitor_test.exs
+++ farmbot_core/test/asset_monitor_test.exs
@@ -1,59 +1,62 @@
 defmodule Farmbot.AssetMonitorTest do
   use ExUnit.Case, async: false
   alias Farmbot.Asset
-  alias Farmbot.AssetSupervisor
+  alias Farmbot.{AssetMonitor, AssetSupervisor}
   import Farmbot.TestSupport.AssetFixtures
-  alias Farmbot.TestSupport.CeleryScript.TestIOLayer
 
   describe "persistent regimens" do
     test "adding a persistent regimen starts a process" do
-      seq = sequence()
-      reg = regimen(%{regimen_items: [%{time_offset: 100, sequence_id: seq.id}]})
-      event = regimen_event(reg)
-      pr = Asset.upsert_persistent_regimen!(reg, event)
-      pr = Asset.update_persistent_regimen!(pr, %{monitor: true})
+      farm_event_params = %{
+        start_time: DateTime.utc_now(),
+        end_time: DateTime.utc_now(),
+        repeat: 1,
+        time_unit: "never"
+      }
 
-      Farmbot.AssetMonitor.force_checkup()
+      pr = persistent_regimen(%{}, farm_event_params, %{monitor: true})
 
-      pid = AssetSupervisor.whereis_child(pr)
-      assert is_pid(pid)
-      assert Process.alive?(pid)
+      AssetMonitor.force_checkup()
+
+      assert {id, _, _, _} = AssetSupervisor.whereis_child(pr)
+      assert id == pr.local_id
 
       Asset.Repo.delete!(pr)
 
-      Farmbot.AssetMonitor.force_checkup()
-      refute Process.alive?(pid)
+      AssetMonitor.force_checkup()
+
+      assert {id, :undefined, _, _} = AssetSupervisor.whereis_child(pr)
+      assert id == pr.local_id
     end
   end
 
   describe "farm events" do
     test "adding a farm event starts a process" do
-      ast = TestIOLayer.debug_ast()
-      seq = sequence(%{body: [ast]})
+      seq = sequence()
       now = DateTime.utc_now()
-      start_time = Timex.shift(now, seconds: 2)
+      start_time = Timex.shift(now, minutes: -20)
       end_time = Timex.shift(now, minutes: 10)
 
       params = %{
         monitor: true,
         start_time: start_time,
         end_time: end_time,
-        repeat: 1,
-        time_unit: "minutely"
+        repeat: 5,
+        time_unit: "hourly"
       }
 
       event = sequence_event(seq, params)
 
-      Farmbot.AssetMonitor.force_checkup()
+      AssetMonitor.force_checkup()
 
-      pid = AssetSupervisor.whereis_child(event)
-      assert is_pid(pid)
-      assert Process.alive?(pid)
+      assert {id, _, _, _} = AssetSupervisor.whereis_child(event)
+      assert id == event.local_id
 
       Asset.Repo.delete!(event)
 
-      Farmbot.AssetMonitor.force_checkup()
-      refute Process.alive?(pid)
+      AssetMonitor.force_checkup()
+
+      assert {id, :undefined, _, _} = AssetSupervisor.whereis_child(event)
+      assert id == event.local_id
     end
   end
 end
