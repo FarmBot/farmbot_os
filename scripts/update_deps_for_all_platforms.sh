9c49a870d3125d71fac42e3e887783b41fa8e5cd
Connor Rigby <connor@farmbot.io>
2018-11-23 14:37:05 -0800

    Add nerves-hub support for rpi

--- scripts/update_deps_for_all_platforms.sh
+++ scripts/update_deps_for_all_platforms.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-TARGETS="rpi0 \
+TARGETS="rpi \
+rpi0
 rpi3 \
 host
 "
 for target in $TARGETS; do
-  MIX_TARGET=$target mix do deps.get, deps.compile
+  MIX_TARGET=$target mix do deps.get
 done
