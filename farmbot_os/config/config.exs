65436cd367b7c3ef6cfed45a476fc3a9c5243aff
Connor Rigby <connor@farmbot.io>
2018-11-29 08:57:27 -0800

    Update config files

--- farmbot_os/config/config.exs
+++ farmbot_os/config/config.exs
@@ -38,24 +38,6 @@ config :ecto, json_library: Farmbot.JSON
 config :farmbot_core,
   ecto_repos: [Farmbot.Config.Repo, Farmbot.Logger.Repo, Farmbot.Asset.Repo]
 
-config :farmbot_core, Farmbot.Config.Repo,
-  adapter: Sqlite.Ecto2,
-  loggers: [],
-  database: "config.#{Mix.env()}.db",
-  priv: "../farmbot_core/priv/config"
-
-config :farmbot_core, Farmbot.Logger.Repo,
-  adapter: Sqlite.Ecto2,
-  loggers: [],
-  database: "logger.#{Mix.env()}.db",
-  priv: "../farmbot_core/priv/logger"
-
-config :farmbot_core, Farmbot.Asset.Repo,
-  adapter: Sqlite.Ecto2,
-  loggers: [],
-  database: "asset.#{Mix.env()}.db",
-  priv: "../farmbot_core/priv/asset"
-
 config :farmbot, Farmbot.OS.FileSystem, data_path: "/tmp/farmbot"
 config :farmbot, Farmbot.System, system_tasks: Farmbot.Host.SystemTasks
 
