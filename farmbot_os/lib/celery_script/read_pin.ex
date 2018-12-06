bf1994df1aca8558f1fc84982636db5939e03bfa
Connor Rigby <connor@farmbot.io>
2018-12-06 13:44:33 -0800

    Update celery_script
    Support peripherals in read and write pin

--- farmbot_os/lib/celery_script/read_pin.ex
+++ farmbot_os/lib/celery_script/read_pin.ex
@@ -3,7 +3,21 @@ defmodule Farmbot.OS.IOLayer.ReadPin do
 
   alias Farmbot.Firmware
 
-  def execute(%{pin_num: p, pin_mode: m}, _body)
+  def execute(%{pin_number: %{args: %{pin_id: id, pin_type: "Peripheral"}}, pin_mode: m}, body)
+      when is_integer(m) do
+    case Farmbot.Asset.get_peripheral(id: id) do
+      %{pin: p} -> execute(%{pin_number: p, pin_mode: m}, body)
+      nil -> {:error, "Could not find peripheral"}
+    end
+  end
+
+  def execute(%{pin_num: p, pin_mode: m}, body)
+      when is_integer(p)
+      when is_integer(m) do
+    execute(%{pin_number: p, pin_mode: m}, body)
+  end
+
+  def execute(%{pin_number: p, pin_mode: m}, _body)
       when is_integer(p)
       when is_integer(m) do
     with {:ok, {_, {:report_pin_value, [p: ^p, v: _]}}} <-
