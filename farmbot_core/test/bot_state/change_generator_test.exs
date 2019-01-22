12f17e7f1286c5682e64f131c38b182b2b6bab91
Connor Rigby <konnorrigby@gmail.com>
2019-01-22 11:43:18 -0800

    Work towards refactoring Farmware
    * Add bot state serializer to filesystem
    * Add helper to turn ecto types into TSC types
    * Add module to extract ecto changes as a list of instructions
    * Add new transport for bot_state_ng
    * Update CSVM Runtime to poll until `await` can complete

---
+++ farmbot_core/test/bot_state/change_generator_test.exs
@@ -0,0 +1,70 @@
+defmodule Farmbot.BotStateNG.ChangeGeneratorTest do
+  use ExUnit.Case
+  alias Farmbot.BotStateNG.ChangeGenerator
+
+  describe "ecto integration" do
+    {:ok, bot_state} = Farmbot.BotState.start_link([], [])
+    _initial = Farmbot.BotState.subscribe(bot_state)
+    :ok = Farmbot.BotState.set_position(bot_state, 1, 2, 3)
+
+    changes =
+      receive do
+        {Farmbot.BotState, change} -> ChangeGenerator.changes(change)
+      after
+        100 -> raise(:timeout)
+      end
+
+    assert {[:location_data, :position, :x], 1.0} in changes
+    assert {[:location_data, :position, :y], 2.0} in changes
+    assert {[:location_data, :position, :z], 3.0} in changes
+  end
+
+  test "returns instructions for all data" do
+    initial = %{
+      a: "string",
+      b: false,
+      c: 1.0,
+      d: nil,
+      nested: %{
+        e: "gnirts",
+        f: true,
+        g: 2.0,
+        h: nil,
+        deeply: %{
+          i: "hello",
+          j: false,
+          k: 3.0,
+          l: nil
+        }
+      }
+    }
+
+    changes = ChangeGenerator.changes(initial)
+    assert {[:a], "string"} in changes
+    assert {[:b], false} in changes
+    assert {[:c], 1.0} in changes
+    assert {[:d], nil} in changes
+
+    assert {[:nested, :e], "gnirts"} in changes
+    assert {[:nested, :f], true} in changes
+    assert {[:nested, :g], 2.0} in changes
+    assert {[:nested, :h], nil} in changes
+
+    assert {[:nested, :deeply, :i], "hello"} in changes
+    assert {[:nested, :deeply, :j], false} in changes
+    assert {[:nested, :deeply, :k], 3.0} in changes
+    assert {[:nested, :deeply, :l], nil} in changes
+  end
+
+  test "raises on lists" do
+    assert_raise RuntimeError, "no lists", fn ->
+      ChangeGenerator.changes(%{some_list: ["a", "b", "c"]})
+    end
+  end
+
+  test "raises on atoms" do
+    assert_raise RuntimeError, "unknown data: :a", fn ->
+      ChangeGenerator.changes(%{some_atom: :a})
+    end
+  end
+end
