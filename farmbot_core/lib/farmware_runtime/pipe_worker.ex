6fef2d4b94c5ffcfe48716797f3c632258287602
connor rigby <connor@farmbot.io>
2019-01-24 13:02:33 -0800

    Use Ports for communicating with FIFO

--- farmbot_core/lib/farmware_runtime/pipe_worker.ex
+++ farmbot_core/lib/farmware_runtime/pipe_worker.ex
@@ -1,30 +1,30 @@
 defmodule Farmbot.FarmwareRuntime.PipeWorker do
   @moduledoc """
-  Proxy for IO operations.
+  Proxy for Pipe IO operations.
   """
   use GenServer
   require Logger
 
   def start_link(pipe_name) do
-    GenServer.start_link(__MODULE__, pipe_name)
+    GenServer.start_link(__MODULE__, [pipe_name])
   end
 
   def close(pipe) do
     GenServer.stop(pipe, :normal)
   end
 
-  def read(pipe, size) do
-    GenServer.call(pipe, {:read, size}, :infinity)
+  def read(pipe, amnt) do
+    GenServer.call(pipe, {:read, amnt})
   end
 
   def write(pipe, packet) do
     GenServer.call(pipe, {:write, packet}, :infinity)
   end
 
-  def init(pipe_name) do
+  def init([pipe_name]) do
     with {_, 0} <- System.cmd("mkfifo", [pipe_name]),
-         {:ok, pipe} <- :file.open(to_charlist(pipe_name), [:read, :write, :binary, :raw]) do
-      {:ok, %{pipe_name: pipe_name, pipe: pipe, ref: nil}}
+         pipe <- :erlang.open_port(to_charlist(pipe_name), [:eof, :binary]) do
+      {:ok, %{pipe_name: pipe_name, pipe: pipe, buffer: <<>>, caller: nil, size: nil}}
     else
       {:error, _} = error -> {:stop, error}
       {_, _num} -> {:stop, {:error, "mkfifo"}}
@@ -33,24 +33,32 @@ defmodule Farmbot.FarmwareRuntime.PipeWorker do
 
   def terminate(_, state) do
     Logger.warn("PipeWorker #{state.pipe_name} exit")
-    :file.close(state.pipe)
+    :erlang.port_close(state.pipe)
     File.rm!(state.pipe_name)
   end
 
-  def handle_call({cmd, args}, {pid, _} = _from, state) do
-    ref = make_ref()
-    {:reply, ref, %{state | ref: ref}, {:continue, {pid, {cmd, args}}}}
+  def handle_call({:write, packet}, _from, state) do
+    reply = :erlang.port_command(state.pipe, packet)
+    {:reply, reply, state}
   end
 
-  def handle_continue({pid, {:read, size}}, state) do
-    result = :file.read(state.pipe, size)
-    send(pid, {__MODULE__, state.ref, result})
-    {:noreply, %{state | ref: nil}}
+  def handle_call({:read, amnt}, {_pid, ref} = from, %{caller: nil, size: nil} = state) do
+    {:reply, ref, %{state | caller: from, size: amnt, buffer: nil}, {:continue, state.buffer}}
   end
 
-  def handle_continue({pid, {:write, packet}}, state) do
-    result = :file.write(state.pipe, packet)
-    send(pid, {__MODULE__, state.ref, result})
-    {:noreply, %{state | ref: nil}}
+  def handle_info({pipe, {:data, data}}, %{pipe: pipe, buffer: buffer} = state) do
+    buffer = buffer <> data
+    {:noreply, %{state | buffer: nil}, {:continue, buffer}}
+  end
+
+  def handle_continue(buffer, %{size: size} = state) when byte_size(buffer) >= size do
+    {pid, ref} = state.caller
+    {resp, buffer} = String.split_at(buffer, size)
+    send(pid, {__MODULE__, ref, {:ok, resp}})
+    {:noreply, %{state | caller: nil, size: nil, buffer: buffer}}
+  end
+
+  def handle_continue(buffer, state) do
+    {:noreply, %{state | buffer: buffer}}
   end
 end
