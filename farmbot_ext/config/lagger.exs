a0267c825c0b23bcdec558746fbf9b49c875722e
connor rigby <connor@farmbot.io>
2019-03-11 08:26:56 -0700

    Fix more namespaces

--- farmbot_ext/config/lagger.exs
+++ farmbot_ext/config/lagger.exs
@@ -1,5 +1,13 @@
 use Mix.Config
+
+config :logger,
+  handle_otp_reports: false,
+  handle_sasl_reports: false
+
 config :lager, :error_logger_redirect, false
 config :lager, :error_logger_whitelist, []
 config :lager, :crash_log, false
-config :lager, :handlers, []
+
+config :lager,
+  handlers: [],
+  extra_sinks: []
