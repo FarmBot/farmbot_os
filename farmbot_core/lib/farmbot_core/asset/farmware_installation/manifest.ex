252f933b155b81806b4090f16ce860e9bfb5f942
Connor Rigby <connor@farmbot.io>
2019-03-18 13:55:47 -0700

    Rename `farmware_manifest_version`

--- farmbot_core/lib/farmbot_core/asset/farmware_installation/manifest.ex
+++ farmbot_core/lib/farmbot_core/asset/farmware_installation/manifest.ex
@@ -43,7 +43,7 @@ defmodule FarmbotCore.Asset.FarmwareInstallation.Manifest do
     field(:args, :string)
     field(:config, :map, default: %{})
     field(:package_version, :string)
-    field(:farmware_manifest_version_requirement, :string)
+    field(:farmware_manifest_version, :string)
     field(:farmware_tools_version_requirement, :string, default: ">= 0.0.0")
     field(:farmbot_os_version_requirement, :string, default: "~> 8.0")
   end
@@ -61,7 +61,7 @@ defmodule FarmbotCore.Asset.FarmwareInstallation.Manifest do
       config: Map.new(manifest.config, fn({key, config_data}) -> {key, Config.view(config_data)} end),
       package_version: manifest.package_version,
       farmware_tools_version_requirement: manifest.farmware_tools_version_requirement,
-      farmware_manifest_version_requirement: manifest.farmware_manifest_version_requirement,
+      farmware_manifest_version: manifest.farmware_manifest_version,
       farmbot_os_version_requirement: manifest.farmbot_os_version_requirement,
     }
   end
@@ -80,7 +80,7 @@ defmodule FarmbotCore.Asset.FarmwareInstallation.Manifest do
       :config,
       :package_version,
       :farmware_tools_version_requirement,
-      :farmware_manifest_version_requirement,
+      :farmware_manifest_version,
       :farmbot_os_version_requirement,
     ])
     |> validate_required([
@@ -90,11 +90,11 @@ defmodule FarmbotCore.Asset.FarmwareInstallation.Manifest do
       :executable,
       :args,
       :package_version,
-      :farmware_manifest_version_requirement,
+      :farmware_manifest_version,
       :farmbot_os_version_requirement
     ])
     |> validate_farmbot_os_version_requirement()
-    |> validate_farmware_manifest_version_requirement()
+    |> validate_farmware_manifest_version()
     |> validate_farmware_tools_version_requirement()
     |> validate_package_version()
     |> validate_config()
@@ -123,9 +123,9 @@ defmodule FarmbotCore.Asset.FarmwareInstallation.Manifest do
     end
   end
 
-  defp validate_farmware_manifest_version_requirement(%{valid?: false} = change), do: change
-  defp validate_farmware_manifest_version_requirement(changeset) do
-    req = get_field(changeset, :farmware_manifest_version_requirement)
+  defp validate_farmware_manifest_version(%{valid?: false} = change), do: change
+  defp validate_farmware_manifest_version(changeset) do
+    req = get_field(changeset, :farmware_manifest_version)
     match =
       try do
         Version.match?(@acceptable_manifest_version_requirement, req)
@@ -138,7 +138,7 @@ defmodule FarmbotCore.Asset.FarmwareInstallation.Manifest do
         changeset
 
       _ ->
-        add_error(changeset, :farmware_manifest_version_requirement, "Version requirement not met")
+        add_error(changeset, :farmware_manifest_version, "Version requirement not met")
     end
   end
 
@@ -167,7 +167,7 @@ defmodule FarmbotCore.Asset.FarmwareInstallation.Manifest do
     version = get_field(changeset, :package_version)
     case Version.parse(version) do
       {:ok, _} -> changeset
-      :error -> 
+      :error ->
         add_error(changeset, :package_version, "not a valid semver string")
     end
   end
@@ -175,7 +175,7 @@ defmodule FarmbotCore.Asset.FarmwareInstallation.Manifest do
   defp validate_config(%{valid?: false} = change), do: change
   defp validate_config(changeset) do
     config = get_field(changeset, :config)
-    Enum.reduce(config, changeset, fn 
+    Enum.reduce(config, changeset, fn
       {name, %{} = params}, changeset when is_binary(name) ->
         case Config.changeset(%Config{}, params) do
           %{valid?: true} = config_changeset ->
@@ -185,7 +185,7 @@ defmodule FarmbotCore.Asset.FarmwareInstallation.Manifest do
           %{valid?: false} ->
             add_error(changeset, :config, "invalid config item")
         end
-      {_, _}, changeset -> 
+      {_, _}, changeset ->
         add_error(changeset, :config, "invalid config item")
     end)
   end
@@ -202,7 +202,7 @@ defmodule FarmbotCore.Asset.FarmwareInstallation.Manifest do
   end
 
   defp validate_zip(%{valid?: false} = change), do: change
-  defp validate_zip(changeset) do 
+  defp validate_zip(changeset) do
     zip = get_field(changeset, :zip)
     parse_uri(zip, changeset, :zip)
   end
