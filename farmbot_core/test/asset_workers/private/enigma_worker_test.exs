db8da310188a05be52061337054a0b55272ee17a
Rick Carlino <rick.carlino@gmail.com>
2019-03-19 12:08:06 -0500

    Tests for BotState and BotStateNG WRT Enigma resources.

---
+++ farmbot_core/test/asset_workers/private/enigma_worker_test.exs
@@ -0,0 +1,7 @@
+defmodule FarmbotCore.Private.EnigmaWorkerTest do
+  use ExUnit.Case, async: true
+  alias FarmbotCore.{Asset.Private.Enigma, AssetWorker}
+  import Farmbot.TestSupport.AssetFixtures
+
+  test "instantiation of a worker"
+end
