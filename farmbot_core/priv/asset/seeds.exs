4c5a093e2cad2c23b807dd22aa73b26544a671ce
Rick Carlino <rick.carlino@gmail.com>
2019-03-20 15:04:23 -0500

    Implement and test Enigmas for firmware_missing * Test for create_or_update_enigma!() and clear_enigma()

--- farmbot_core/priv/asset/seeds.exs
+++
@@ -1,3 +0,0 @@
-alias FarmbotCore.Asset.Private
-
-Private.new_enigma(%{priority: 100, problem_tag: "firmware.missing"})
--- farmbot_os/lib/farmbot_os/firmware_tty_detector.ex
+++ farmbot_os/lib/farmbot_os/firmware_tty_detector.ex
@@ -9,8 +9,6 @@ defmodule FarmbotOS.FirmwareTTYDetector do
   alias FarmbotCore.FirmwareSideEffects
   alias FarmbotCore.{Asset, BotState}
 
-  import FarmbotCore.Config, only: [get_config_value: 3, update_config_value: 4]
-
   @expected_names Application.get_env(:farmbot, __MODULE__)[:expected_names]
   @expected_names ||
     Mix.raise("""
@@ -20,135 +18,44 @@ defmodule FarmbotOS.FirmwareTTYDetector do
           expected_names: ["ttyS0", "ttyNotReal"]
     """)
 
-  @error_ms 5000
+  def tty(server \\ __MODULE__) do
+    GenServer.call(server, :tty)
+  end
 
-  def start_link(args) do
-    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  def tty!(server \\ __MODULE__) do
+    tty(server) || raise "No TTY detected"
+  end
+
+  def start_link(args, opts \\ [name: __MODULE__]) do
+    GenServer.start_link(__MODULE__, args, opts)
   end
 
   def init([]) do
-    {:ok, %{device: nil, open: false, version: nil}, 0}
+    {:ok, nil, 0}
   end
 
-  def terminate(_, _) do
-    System.cmd("killall", ["-9", "avrdude"], into: IO.stream(:stdio, :line))
+  def handle_call(:tty, _, detected_tty) do
+    {:reply, detected_tty, detected_tty}
   end
 
-  def handle_info(:timeout, %{device: nil} = state) do
-    case get_config_value(:string, "settings", "firmware_hardware") do
-      nil ->
-        {:noreply, state, @error_ms}
+  def handle_info(:timeout, state) do
+    enumerated = UART.enumerate() |> Map.to_list()
+    {:noreply, state, {:continue, enumerated}}
+  end
 
-      _hw ->
-        available = UART.enumerate() |> Map.to_list()
-        {:noreply, state, {:continue, available}}
+  def handle_continue([{name, _} | rest], state) do
+    if farmbot_tty?(name) do
+      {:noreply, name}
+    else
+      {:noreply, state, {:continue, rest}}
     end
   end
 
-  def handle_info(:timeout, %{device: _device, open: false} = state) do
-    case get_config_value(:bool, "settings", "firmware_needs_flash") do
-      true -> handle_flash(state)
-      false -> handle_open(state)
-    end
+  def handle_continue([], state) do
+    {:noreply, state}
   end
 
-  def handle_info(:timeout, %{device: _device, open: true, version: nil} = state) do
-    case FarmbotFirmware.request({:software_version_read, []}) do
-      {:ok, {_, {:report_software_version, [version]}}} ->
-        {:noreply, %{state | version: version}}
-
-      _ ->
-        {:noreply, state, 5000}
-    end
+  defp farmbot_tty?(file_path) do
+    file_path in @expected_names
   end
-
-  def handle_info({:DOWN, _ref, :process, _pid, reason}, state) do
-    {:stop, reason, state}
-  end
-
-  def handle_continue([{name, _} | _rest], %{device: nil} = state)
-      when name in @expected_names do
-    FarmbotCore.Logger.debug(3, "Found tty: #{name}")
-    {:noreply, %{state | device: name}, 0}
-  end
-
-  def handle_continue([{name, _} | rest], %{device: nil} = state) do
-    FarmbotCore.Logger.debug(3, "#{name} not a valid Farmbot tty")
-    {:noreply, state, {:continue, rest}}
-  end
-
-  def handle_continue([], %{device: nil} = state) do
-    {:noreply, state, @error_ms}
-  end
-
-  defp handle_flash(state) do
-    dir = Application.app_dir(:farmbot, ["priv"])
-
-    case get_config_value(:string, "settings", "firmware_hardware") do
-      "arduino" ->
-        flash_fw(Path.join(dir, "arduino_firmware.hex"), state)
-
-      "farmduino" ->
-        flash_fw(Path.join(dir, "farmduino.hex"), state)
-
-      "farmduino_k14" ->
-        flash_fw(Path.join(dir, "farmduino_k14.hex"), state)
-
-      nil ->
-        {:noreply, state, @error_ms}
-
-      other ->
-        Logger.error("Unknown arduino firmware #{other}")
-        {:stop, {:unknown_firmware, other}, state}
-    end
-  end
-
-  defp handle_open(state) do
-    opts = [
-      device: state.device,
-      transport: UARTTransport,
-      side_effects: FirmwareSideEffects
-    ]
-
-    child_spec = {Farmbot.Firmware, opts}
-
-    case Supervisor.start_child(FirmwareSupervisor, child_spec) do
-      {:ok, pid} ->
-        # This might cause some sort of race condition.
-        hw = get_config_value(:string, "settings", "firmware_hardware")
-
-        Asset.update_fbos_config!(%{
-          firmware_path: state.device,
-          firmware_hardware: hw
-        })
-
-        :ok = BotState.set_config_value(:firmware_hardware, hw)
-        Process.monitor(pid)
-        {:noreply, %{state | open: true, version: nil}, 5000}
-
-      error ->
-        {:stop, error, state}
-    end
-  end
-
-  defp flash_fw(fw_file, state) do
-    args =
-      ~w"-q -q -patmega2560 -cwiring -P#{dev(state.device)} -b115200 -D -V -Uflash:w:#{fw_file}:i"
-
-    opts = [stderr_to_stdout: true, into: IO.stream(:stdio, :line)]
-    res = System.cmd("avrdude", args, opts)
-
-    case res do
-      {_, 0} ->
-        update_config_value(:bool, "settings", "firmware_needs_flash", false)
-        {:noreply, state, 0}
-
-      _ ->
-        Logger.error("firmware flash failed")
-        {:noreply, state, @error_ms}
-    end
-  end
-
-  defp dev("/dev/" <> _ = device), do: IO.inspect(device, label: "DEVICE")
-  defp dev("tty" <> _ = dev), do: IO.inspect(Path.join("/dev", dev), label: "DEVICE")
 end
