eec2c09c0d06b00a9a9c1f2919cf5317675074f7
Rick Carlino <rick.carlino@gmail.com>
2019-03-20 15:04:23 -0500

    Implement and test Enigmas for firmware_missing * Test for create_or_update_enigma!() and clear_enigma()

--- farmbot_core/lib/farmbot_core/asset/private.ex
+++ farmbot_core/lib/farmbot_core/asset/private.ex
@@ -14,9 +14,18 @@ defmodule FarmbotCore.Asset.Private do
   import Ecto.Changeset, warn: false
 
   @doc "Creates a new Enigma."
-  def new_enigma(params) do
-    Enigma.changeset(%Enigma{}, params)
-    |> Repo.insert()
+  def create_or_update_enigma!(params) do
+    enigma = if problem_tag = params[:problem_tag] do
+      find_enigma_by_problem_tag(problem_tag) || %Enigma{}
+    else
+      %Enigma{}
+    end
+
+    Enigma.changeset(enigma, params) |> Repo.insert_or_update!()
+  end
+
+  def find_enigma_by_problem_tag(problem_tag) do
+    Repo.get_by(Enigma, problem_tag: problem_tag)
   end
 
   @doc """
@@ -24,13 +33,12 @@ defmodule FarmbotCore.Asset.Private do
   problem_tag.
   """
   def clear_enigma(problem_tag) do
-    Repo.get_by(problem_tag: problem_tag)
-      |> case do
-         nil -> :ok
-         %Enigma{} = enigma ->
-           Repo.delete!(enigma)
-           :ok
-      end
+    case find_enigma_by_problem_tag(problem_tag) do
+       nil -> :ok
+       %Enigma{} = enigma ->
+         Repo.delete!(enigma)
+         :ok
+    end
   end
 
   @doc "Lists `module` objects that still need to be POSTed to the API."
