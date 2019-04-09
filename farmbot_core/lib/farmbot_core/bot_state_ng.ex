5b2bdc8f0ead572d4ef9dccf5b2192536b0b6a53
Connor Rigby <connor@farmbot.io>
2019-04-09 14:32:40 -0700

    Temporarily quiet down compiler warning

--- farmbot_core/lib/farmbot_core/bot_state_ng.ex
+++ farmbot_core/lib/farmbot_core/bot_state_ng.ex
@@ -76,13 +76,15 @@ defmodule FarmbotCore.BotStateNG do
   end
 
   @doc "Add or update a farmware to state.farmwares"
-  def add_or_update_farmware(state, name, %{} = manifest) do
+  def add_or_update_farmware(state, _name, %{} = _manifest) do
+    # TODO(Connor) finish this.
     cs = changeset(state, %{})
 
     new_farmwares =
       cs
       |> get_field(:process_info)
       |> Map.get(:farmwares)
+      # |> Map.put(name, manifest)
     put_change(cs, :process_info, %{farmwares: new_farmwares})
   end
 
