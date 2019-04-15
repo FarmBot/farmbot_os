a9e4d9fe541c781f09260bf00143c03049d4fd62
Connor Rigby <connor@farmbot.io>
2019-04-15 14:03:09 -0700

    Update reload task

--- farmbot_os/lib/mix/tasks.farmbot/firmware/reload.ex
+++ farmbot_os/lib/mix/tasks.farmbot/firmware/reload.ex
@@ -1,33 +1,51 @@
 defmodule Mix.Tasks.Farmbot.Firmware.Reload do
   use Mix.Task
 
-  def run([farmbot_node | apps]) do
-    # module = Module.concat([Elixir, module])
-    # true = Code.ensure_loaded?(module)
+  @switches [
+    app: :keep,
+    module: :keep
+  ]
 
+  def run(args) do
+    {opts, args} = OptionParser.parse!(args, strict: @switches)
+    [farmbot_node | _] = args
     farmbot_node = String.to_atom(farmbot_node)
+
+    mods = get_mods(opts)
     {:ok, _} = Node.start(:console)
     Node.set_cookie(:democookie)
     true = Node.connect(farmbot_node)
 
-    Application.load(:farmbot_core)
-    Application.load(:farmbot_ext)
-    Application.load(:farmbot)
-
-    {:ok, farmbot_core_mods} = :application.get_key(:farmbot_core, :modules)
-    {:ok, farmbot_ext_mods} = :application.get_key(:farmbot_ext, :modules)
-    {:ok, farmbot_os_mods} = :application.get_key(:farmbot, :modules)
-
-    mods =
-      Enum.reduce(apps, [], fn app, acc ->
-        app = String.to_atom(app)
-        Application.load(app)
-        {:ok, more} = :application.get_key(app, :modules)
-        acc ++ more
-      end)
-
-    for module <- mods ++ farmbot_core_mods ++ farmbot_ext_mods ++ farmbot_os_mods do
+    for module <- mods do
       {:ok, [{^farmbot_node, :loaded, ^module}]} = IEx.Helpers.nl([farmbot_node], module)
     end
+  end
+
+  defp get_mods(opts) do
+    case Keyword.get_values(opts, :module) do
+      [_ | _] = mods ->
+        Enum.map(mods, fn mod ->
+          mod = Module.concat("Elixir", mod)
+          Code.ensure_loaded?(mod)
+          mod
+        end)
+
+      [] ->
+        get_apps(opts)
+    end
+  end
+
+  defp get_apps(opts) do
+    apps =
+      case Keyword.get_values(opts, :app) do
+        [] -> [:farmbot_core, :farmbot_ext, :farmbot]
+        [_ | _] = apps -> Enum.map(apps, &String.to_atom/1)
+      end
+
+    Enum.reduce(apps, [], fn app, mods ->
+      Application.load(app)
+      {:ok, more_mods} = :application.get_key(app, :modules)
+      mods ++ more_mods
+    end)
   end
 end
