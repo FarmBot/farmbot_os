a7fa3bb90e112b971f42d788fe222705b3a59df0
Connor Rigby <connor@farmbot.io>
2019-04-17 11:46:53 -0700

    Update job_progress to not use atoms

--- farmbot_core/lib/farmbot_core/bot_state/job_progress.ex
+++ farmbot_core/lib/farmbot_core/bot_state/job_progress.ex
@@ -1,16 +1,18 @@
 defmodule FarmbotCore.BotState.JobProgress do
   @moduledoc "Interface for job progress."
 
-  @typedoc "Unit of the job."
-  @type unit :: :percent | :bytes
-  @typedoc "Status of the job."
-  @type status :: :working | :complete | :error
+  @typedoc "Unit of the job. Will be `percent` | `bytes`"
+  @type unit :: String.t()
+
+
+  @typedoc "Status of the job. Will be `error` | `working` | `complete`"
+  @type status :: String.t()
 
   alias FarmbotCore.BotState.JobProgress
 
   defmodule Percent do
     @moduledoc "Percent job."
-    defstruct status: :working, percent: 0, unit: :percent, type: :ota, time: nil, file_type: nil
+    defstruct status: "working", percent: 0, unit: "percent", type: "ota", time: nil, file_type: nil
 
     defimpl Inspect, for: __MODULE__ do
       def inspect(%{percent: percent}, _) do
@@ -21,8 +23,8 @@ defmodule FarmbotCore.BotState.JobProgress do
     @type t :: %__MODULE__{
             status: JobProgress.status(),
             percent: integer,
-            unit: :percent,
-            type: :image | :ota,
+            unit: JobProgress.unit(),
+            type: String.t,
             file_type: binary(),
             time: DateTime.t()
           }
@@ -30,7 +32,7 @@ defmodule FarmbotCore.BotState.JobProgress do
 
   defmodule Bytes do
     @moduledoc "Bytes job."
-    defstruct status: :working, bytes: 0, unit: :bytes, type: :ota, time: nil, file_type: nil
+    defstruct status: "working", bytes: 0, unit: "bytes", type: "ota", time: nil, file_type: nil
 
     defimpl Inspect, for: __MODULE__ do
       def inspect(%{bytes: bytes}, _) do
@@ -41,8 +43,8 @@ defmodule FarmbotCore.BotState.JobProgress do
     @type t :: %__MODULE__{
             status: JobProgress.status(),
             bytes: integer,
-            unit: :bytes,
-            type: :image | :ota,
+            unit: JobProgress.unit(),
+            type: String.t(),
             file_type: binary(),
             time: DateTime.t()
           }
