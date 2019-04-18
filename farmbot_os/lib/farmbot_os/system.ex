7ecc8e918369f3549f3455826414f71f6098f52a
Connor Rigby <connor@farmbot.io>
2019-04-18 13:41:14 -0700

    Implement factory_reset sys_call

--- farmbot_os/lib/farmbot_os/system.ex
+++ farmbot_os/lib/farmbot_os/system.ex
@@ -3,7 +3,8 @@ defmodule FarmbotOS.System do
   Common functionality that should be implemented by a system
   """
   require FarmbotCore.Logger
-  alias FarmbotCore.{Asset, Project}
+  require Logger
+  alias FarmbotCore.Asset
 
   error_msg = """
   Please configure `:system_tasks`!
@@ -20,6 +21,26 @@ defmodule FarmbotOS.System do
   @doc "Shuts down the machine."
   @callback shutdown() :: any
 
+  def soft_restart do
+    Logger.info("Stopping app: :farmbot")
+    _ = Application.stop(:farmbot)
+
+    Logger.info("Stopping app: :farmbot_ext")
+    _ = Application.stop(:farmbot_ext)
+
+    Logger.info("Stopping app: :farmbot_core")
+    _ = Application.stop(:farmbot_core)
+
+    Logger.info("Starting ap: :farmbot_core")
+    _ = Application.ensure_all_started(:farmbot_core)
+
+    Logger.info("Starting ap: :farmbot_ext")
+    _ = Application.ensure_all_started(:farmbot_ext)
+
+    Logger.info("Starting ap: :farmbot")
+    _ = Application.ensure_all_started(:farmbot)
+  end
+
   @doc "Reads the last shutdown is there was one."
   def last_shutdown_reason do
     file = Path.join(@data_path, "last_shutdown_reason")
@@ -30,6 +51,43 @@ defmodule FarmbotOS.System do
     end
   end
 
+  @doc "Remove all configuration data, and reboot."
+  @spec factory_reset(any) :: no_return
+  def factory_reset(reason, force \\ false) do
+    if force || should_factory_reset?() do
+      try_lock_fw()
+      write_file(reason)
+      _ = FarmbotCore.EctoMigrator.drop()
+      _ = Supervisor.start_child(:elixir_sup, {Task, &FarmbotOS.System.soft_restart/0})
+      :ok
+    else
+      FarmbotCore.Logger.error(1, "Factory Reset disabled.")
+      :ok
+    end
+  end
+
+  @doc "Reboot."
+  @spec reboot(any) :: no_return
+  def reboot(reason) do
+    try_lock_fw()
+    write_file(reason)
+    @system_tasks.reboot()
+  end
+
+  @doc "Shutdown."
+  @spec shutdown(any) :: no_return
+  def shutdown(reason) do
+    try_lock_fw()
+    write_file(reason)
+    @system_tasks.shutdown()
+  end
+
+  defp write_file(reason) do
+    file = Path.join(@data_path, "last_shutdown_reason")
+    if reason, do: File.write!(file, inspect(reason)), else: File.rm_rf(file)
+  end
+
+  # Check if the FarmbotFirmware process is alive
   defp try_lock_fw do
     if Process.whereis(FarmbotFirmware) do
       FarmbotCore.Logger.warn(1, "Trying to emergency lock firmware before powerdown")
@@ -39,61 +97,14 @@ defmodule FarmbotOS.System do
     end
   end
 
-  @doc "Remove all configuration data, and reboot."
-  @spec factory_reset(any) :: no_return
-  def factory_reset(reason) do
-    if Project.env() == :dev do
-      # credo:disable-for-next-line
-      require IEx
-      IEx.pry()
+  # This is wrapped in a try/catch because it's possible that
+  # the `Asset.Repo` server is not running. We want to ensure if it isn't
+  # Running, we are still able to reset.
+  defp should_factory_reset? do
+    try do
+      if Asset.fbos_config(:disable_factory_reset), do: false, else: true
+    catch
+      _, _ -> true
     end
-
-    if Process.whereis(FarmbotCore) do
-      if Asset.fbos_config().disable_factory_reset do
-        reboot(reason)
-      else
-        do_reset(reason)
-      end
-    else
-      do_reset(reason)
-    end
-  end
-
-  defp do_reset(reason) do
-    Application.stop(:farmbot_ext)
-    Application.stop(:farmbot_core)
-
-    for p <- Path.wildcard(Path.join(@data_path, "*")) do
-      File.rm_rf!(p)
-    end
-
-    reboot(reason)
-  end
-
-  @doc "Reboot."
-  @spec reboot(any) :: no_return
-  def reboot(reason) do
-    write_file(reason)
-    try_lock_fw()
-    @system_tasks.reboot()
-  end
-
-  @doc "Shutdown."
-  @spec shutdown(any) :: no_return
-  def shutdown(reason) do
-    write_file(reason)
-    try_lock_fw()
-    @system_tasks.shutdown()
-  end
-
-  defp write_file(nil) do
-    file = Path.join(@data_path, "last_shutdown_reason")
-    File.rm_rf(file)
-  end
-
-  defp write_file(reason) do
-    IO.puts("Farmbot powering down: #{reason}")
-    file = Path.join(@data_path, "last_shutdown_reason")
-    File.write!(file, inspect(reason))
   end
 end
