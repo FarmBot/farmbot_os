e5e0aa6d96b190c311db8e0d5bb9ce31422d3ac1
Connor Rigby <connor@farmbot.io>
2019-04-25 08:18:09 -0700

    Update SQLITE repos to only have one pool process.

--- farmbot_os/config/host/dev.exs
+++ farmbot_os/config/host/dev.exs
@@ -9,16 +9,19 @@ config :farmbot_ext,
 config :farmbot_core, FarmbotCore.Config.Repo,
   adapter: Sqlite.Ecto2,
   loggers: [],
+  pool_size: 1,
   database: Path.join(data_path, "config-#{Mix.env()}.sqlite3")
 
 config :farmbot_core, FarmbotCore.Logger.Repo,
   adapter: Sqlite.Ecto2,
   loggers: [],
+  pool_size: 1,
   database: Path.join(data_path, "logs-#{Mix.env()}.sqlite3")
 
 config :farmbot_core, FarmbotCore.Asset.Repo,
   adapter: Sqlite.Ecto2,
   loggers: [],
+  pool_size: 1,
   database: Path.join(data_path, "asset-#{Mix.env()}.sqlite3")
 
 config :farmbot, FarmbotOS.Init.Supervisor,
