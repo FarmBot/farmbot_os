b968827d4b1af0e0cbc79f75008da4f4122723ae
connor rigby <connor@farmbot.io>
2019-05-07 08:12:11 -0700

    Add compiler warning for preloader impl

--- farmbot_ext/lib/farmbot_ext/api/preloader.ex
+++ farmbot_ext/lib/farmbot_ext/api/preloader.ex
@@ -5,6 +5,15 @@ defmodule FarmbotExt.API.Preloader do
     * FarmbotCore.Asset.FbosConfig
     * FarmbotCore.Asset.FirmwareConfig
   """
+  Application.get_env(:farmbot_ext, __MODULE__)[:preloader_impl] ||
+    Mix.raise("""
+    No default preloader implementation was provided. 
+
+      config :farmbot_ext, FarmbotExt.API.Preloader, [
+        preloader_impl: FarmbotExt.API.Preloader.HTTP
+      ]
+    """)
+
   @callback preload_all :: :ok | :error
 
   def preload_all do
