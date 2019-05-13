30a4275998b7640051cba2ad8e044c9f851dab95
Connor Rigby <connor@farmbot.io>
2019-05-13 13:11:09 -0700

    Refactor config system for mox

--- farmbot_celery_script/config/config.exs
+++ farmbot_celery_script/config/config.exs
@@ -7,10 +7,3 @@ else
   config :farmbot_celery_script, FarmbotCeleryScript.SysCalls,
     sys_calls: FarmbotCeleryScript.SysCalls.Stubs
 end
-
-if Mix.env() == :test do
-  # Assign values used by the test suite to mock things out.
-  # Leaving these values unassign results in default (production/dev) module use
-  config :farmbot_ext, FarmbotExt.API.Preloader, preloader_impl: MockPreloader
-  config :farmbot_ext, FarmbotExt.AMQP.ConnectionWorker, network_impl: MockConnectionWorker
-end
