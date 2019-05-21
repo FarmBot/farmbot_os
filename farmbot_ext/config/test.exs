b8e2dbfbbae98eafd77178347bfa1eb3f7022059
Rick Carlino <RickCarlino@users.noreply.github.com>
2019-05-21 13:59:54 -0500

    AutoSyncChannel Testing: Device, `auto_sync = false` (#714)
    * Test auto_sync ignore case
    
    * Test case: auto_sync is disabled.
    
    * Test for auto_sync of Device assets

--- farmbot_ext/config/test.exs
+++ farmbot_ext/config/test.exs
@@ -1,5 +1,6 @@
 use Mix.Config
 
 config :farmbot_ext, FarmbotExt.API.Preloader, preloader_impl: MockPreloader
-
 config :farmbot_ext, FarmbotExt.AMQP.ConnectionWorker, network_impl: MockConnectionWorker
+config :farmbot_ext, FarmbotExt.AMQP.AutoSyncChannel, query_impl: MockQuery
+config :farmbot_ext, FarmbotExt.AMQP.AutoSyncChannel, command_impl: MockCommand
