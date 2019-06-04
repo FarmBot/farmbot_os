77ade6a4a887f6338e477490327dff1c1e045af7
Connor Rigby <connor@farmbot.io>
2019-06-04 13:36:05 -0700

    Cleanup farmbot_ext auto_sync_channel
    Also add some documentation for stubable
    modules Query and Command

--- farmbot_core/lib/farmbot_core/asset/query.ex
+++ farmbot_core/lib/farmbot_core/asset/query.ex
@@ -1,7 +1,13 @@
 defmodule FarmbotCore.Asset.Query do
+  @moduledoc """
+  Get data from the database
+  """
+  
   alias FarmbotCore.Asset
 
   @callback auto_sync?() :: boolean()
+
+  @doc "Returns the configuration value for auto_sync"
   def auto_sync?() do
     Asset.fbos_config().auto_sync
   end
