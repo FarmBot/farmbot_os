2ef66e0b29f7fd06d4e6c8341e2e84a926efa78c
Connor Rigby <connor@farmbot.io>
2019-06-05 09:21:56 -0700

    Update `mox` to be used correctly.
    I previously misunderstood how the black magic of mox actually works.
    This updates `farmbot_ext` to not require setting excess data in every
    config.exs entry. Also removes calls to `Application.get_env/2`

--- farmbot_ext/config/test.exs
+++
@@ -1,6 +0,0 @@
-use Mix.Config
-
-config :farmbot_ext, FarmbotExt.API.Preloader, preloader_impl: MockPreloader
-config :farmbot_ext, FarmbotExt.AMQP.ConnectionWorker, network_impl: MockConnectionWorker
-config :farmbot_ext, FarmbotExt.AMQP.AutoSyncChannel, query_impl: MockQuery
-config :farmbot_ext, FarmbotExt.AMQP.AutoSyncChannel, command_impl: MockCommand
