b9442833fd2c059191b14b8ed018aa9faa54943b
Connor Rigby <connor@farmbot.io>
2019-06-07 10:20:44 -0700

    Refactor Preloader test system
    * Gets rid of the Preloader behaviour
      * This prevents the Preloader mock from overriding the _actual_
        implementation.
    * Update auto_sync_channel_test to mock the API, not the Preloader
    * remove old preloader_test as it didn't do anything

--- farmbot_ext/test/test_helper.exs
+++ farmbot_ext/test/test_helper.exs
@@ -3,7 +3,7 @@ Mox.defmock(FarmbotCore.Asset.Query, for: FarmbotCore.Asset.Query)
 Mox.defmock(FarmbotCore.Asset.Command, for: FarmbotCore.Asset.Command)
 
 # Mocking for FarmbotExt
-Mox.defmock(FarmbotExt.API.Preloader, for: FarmbotExt.API.Preloader)
+Mox.defmock(FarmbotExt.API, for: FarmbotExt.API)
 Mox.defmock(FarmbotExt.AMQP.ConnectionWorker, for: FarmbotExt.AMQP.ConnectionWorker)
 
 ExUnit.start()
