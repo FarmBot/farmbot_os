014c03dbc7d3e5374b099df33ee94ca4fb11d62e
Rick Carlino <rick.carlino@gmail.com>
2019-06-12 08:44:14 -0500

    Configurable EXUNIT_TIMEOUT

--- farmbot_ext/test/test_helper.exs
+++ farmbot_ext/test/test_helper.exs
@@ -5,5 +5,10 @@ Mox.defmock(FarmbotCore.Asset.Command, for: FarmbotCore.Asset.Command)
 # Mocking for FarmbotExt
 Mox.defmock(FarmbotExt.API, for: FarmbotExt.API)
 Mox.defmock(FarmbotExt.AMQP.ConnectionWorker, for: FarmbotExt.AMQP.ConnectionWorker)
+timeout = System.get_env("EXUNIT_TIMEOUT")
 
-ExUnit.start()
+if timeout do
+  ExUnit.start(assert_receive_timeout: String.to_integer(timeout))
+else
+  ExUnit.start()
+end
