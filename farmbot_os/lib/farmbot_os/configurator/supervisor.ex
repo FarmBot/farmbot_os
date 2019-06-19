69a990309fecca8a6f566ecad3006ae4bcd9fe74
Connor Rigby <connor@farmbot.io>
2019-06-19 14:16:39 -0700

    Move configurator back to first class of `farmbot_os`
    * Configurator HTTP endpoing is now always started
    * data access is now a behaviour for configurator
       * Will allow for independent testing with mox
    * network access is now a behaviour for configurator
       * Will allow for independent testing with mox

---
+++ farmbot_os/lib/farmbot_os/configurator/supervisor.ex
@@ -0,0 +1,31 @@
+defmodule FarmbotOS.Configurator.Supervisor do
+  use Supervisor
+  alias FarmbotOS.Configurator.Router
+
+  def start_link(args) do
+    Supervisor.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(_args) do
+    :ets.new(:configurator_session, [:named_table, :public, read_concurrency: true])
+
+    transport_opts = [
+      num_acceptors: 1
+    ]
+
+    opts = [port: default_port(), transport_options: transport_opts]
+
+    children = [
+      {Plug.Adapters.Cowboy, scheme: :http, plug: Router, options: opts}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  defp default_port do
+    case System.get_env("CONFIGURATOR_PORT") do
+      nil -> 80
+      str -> String.to_integer(str)
+    end
+  end
+end
