a5dbe3c6671f9a9bf1cba74f26346b687bc27eea
connor rigby <connor@farmbot.io>
2019-07-05 13:51:47 -0700

    Implement task for opening the firmware
    Adds a new flag for `firmware_needs_open` and a task that polls that
    value with a bit of state to open the firmware tty

---
+++ farmbot_core/priv/config/migrations/20190705203806_add_firmware_needs_open_flag.exs
@@ -0,0 +1,11 @@
+defmodule FarmbotCore.Config.Repo.Migrations.AddFirmwareNeedsOpenFlag do
+  use Ecto.Migration
+  import FarmbotCore.Config, only: [update_config_value: 4]
+  import FarmbotCore.Config.MigrationHelpers
+
+  def change do
+    update_config_value(:string, "settings", "firmware_hardware", nil)
+    update_config_value(:bool, "settings", "firmware_needs_flash", true)
+    create_settings_config("firmware_needs_open", :bool, false)
+  end
+end
