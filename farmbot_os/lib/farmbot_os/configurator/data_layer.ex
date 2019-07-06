4c5dded5b35a4108da90eb85f30bec4290055a87
connor rigby <connor@farmbot.io>
2019-07-05 18:02:08 -0700

    Some configurator tweaks

--- farmbot_os/lib/farmbot_os/configurator/data_layer.ex
+++ farmbot_os/lib/farmbot_os/configurator/data_layer.ex
@@ -26,7 +26,5 @@ defmodule FarmbotOS.Configurator.DataLayer do
   @callback load_email() :: nil | String.t()
   @callback load_password() :: nil | String.t()
   @callback load_server() :: nil | String.t()
-  @callback dump_logs() :: [map()]
-  @callback dump_log_db() :: {:error, File.posix()} | binary()
   @callback save_config(conf) :: any()
 end
