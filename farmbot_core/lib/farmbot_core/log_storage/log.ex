bf9bf437d0497fa6196c772028b0e793c05882ba
Connor Rigby <connor@farmbot.io>
2019-07-09 10:21:38 -0700

    Remove Farmbot Logger colors
    * Fixes espeak trying to speak ansi colors
    * allows espeak to be specified as the sole channel

--- farmbot_core/lib/farmbot_core/log_storage/log.ex
+++ farmbot_core/lib/farmbot_core/log_storage/log.ex
@@ -84,20 +84,7 @@ defmodule FarmbotCore.Log do
 
   defimpl String.Chars, for: Log do
     def to_string(log) do
-      if log.meta[:color] && function_exported?(IO.ANSI, log.meta[:color], 0) do
-        "#{apply(IO.ANSI, log.meta[:color], [])}#{log.message}#{color(:normal)}\n"
-      else
-        "#{color(log.level)}#{log.message}#{color(:normal)}\n"
-      end
+      IO.iodata_to_binary(log.message)
     end
-
-    defp color(:debug), do: IO.ANSI.light_blue()
-    defp color(:info), do: IO.ANSI.cyan()
-    defp color(:busy), do: IO.ANSI.blue()
-    defp color(:success), do: IO.ANSI.green()
-    defp color(:warn), do: IO.ANSI.yellow()
-    defp color(:error), do: IO.ANSI.red()
-    defp color(:normal), do: IO.ANSI.normal()
-    defp color(_), do: IO.ANSI.normal()
   end
 end
