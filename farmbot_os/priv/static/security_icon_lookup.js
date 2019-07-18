5de367e78663357068547a8adaf9ad3ed8f900a4
Connor Rigby <connor@farmbot.io>
2019-07-18 09:14:46 -0700

    Fix network panel of configurator having one interface selected

---
+++ farmbot_os/priv/static/security_icon_lookup.js
@@ -0,0 +1,4 @@
+var security_icon_lookup = {};
+security_icon_lookup["NONE"] = "icon_none.svg";
+security_icon_lookup["WPA-PSK"] = "icon_lock.svg";
+security_icon_lookup["WPA-EAP"] = "icon_key.svg";
