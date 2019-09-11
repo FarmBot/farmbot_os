9b927f03f7ba9ff7f56d4ae3d71abecd08430369
Connor Rigby <connor@farmbot.io>
2019-09-11 09:46:03 -0700

    Update farmbot_system_rpi3

--- farmbot_core/mix.exs
+++ farmbot_core/mix.exs
@@ -47,7 +47,7 @@ defmodule FarmbotCore.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :ssl, :inets],
       mod: {FarmbotCore, []}
     ]
   end
