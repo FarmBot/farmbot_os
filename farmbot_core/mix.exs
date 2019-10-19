25582777729ca3a73cf3a38f99a52e7142bb214f
Connor Rigby <connor@farmbot.io>
2019-10-19 08:06:34 -0700

    Add new farmbot_telemetry application

--- farmbot_core/mix.exs
+++ farmbot_core/mix.exs
@@ -57,6 +57,7 @@ defmodule FarmbotCore.MixProject do
     [
       {:farmbot_celery_script, path: "../farmbot_celery_script", env: Mix.env()},
       {:farmbot_firmware, path: "../farmbot_firmware", env: Mix.env()},
+      {:farmbot_telemetry, path: "../farmbot_telemetry", env: Mix.env()},
       {:elixir_make, "~> 0.6", runtime: false},
       {:sqlite_ecto2, "~> 2.3"},
       {:timex, "~> 3.4"},
---
+++ farmbot_telemetry/lib/farmbot_telemetry/application.ex
@@ -0,0 +1,19 @@
+defmodule FarmbotTelemetry.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: FarmbotTelemetry.Worker.start_link(arg)
+      # {FarmbotTelemetry.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: FarmbotTelemetry.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
