6e306714884cf89f9dbe4fa2847e942ae54fa833
Connor Rigby <connor@farmbot.io>
2019-10-20 13:36:11 -0700

    Initial pass at telelemtry event system

--- farmbot_telemetry/lib/farmbot_telemetry.ex
+++ farmbot_telemetry/lib/farmbot_telemetry.ex
@@ -1,18 +1,91 @@
 defmodule FarmbotTelemetry do
   @moduledoc """
-  Documentation for FarmbotTelemetry.
+  Interface for farmbot system introspection
   """
+  alias FarmbotTelemetry.Class
 
-  @doc """
-  Hello world.
+  @typedoc "Module that implements the FarmbotTelemetry.Class behaviour"
+  @type class() :: module()
 
-  ## Examples
+  @typedoc "First argument to the handler"
+  @type event() :: nonempty_maybe_improper_list(class(), Class.type())
 
-      iex> FarmbotTelemetry.hello()
-      :world
+  @typedoc "Second argument to the handler"
+  @type data() :: %{required(:action) => Class.action(), required(:timestamp) => DateTime.t()}
 
-  """
-  def hello do
-    :world
+  @type meta() :: map()
+
+  @typedoc "Term that will be passed to the handler."
+  @type config() :: any()
+
+  @typedoc "Module that implements the "
+  @type handler() :: (event(), data(), meta(), config() -> any())
+
+  @typedoc "required options for FarmbotTelemetry"
+  @type opt() ::
+          {:class, class()}
+          | {:handler, handler()}
+          | {:config, config()}
+
+  @typedoc false
+  @type opts :: [opts]
+
+  @doc false
+  @spec child_spec(opts) :: Supervisor.child_spec()
+  def child_spec(opts) do
+    %{
+      id: opts[:class],
+      start: {__MODULE__, :attach, [opts]}
+    }
+  end
+
+  @doc false
+  @spec attach(opts) :: GenServer.on_start()
+  def attach(opts) do
+    class = opts[:class]
+    handler_id = opts[:handler_id]
+    handler = opts[:handler]
+    config = opts[:config]
+
+    for {type, _actions} <- class.matrix() do
+      _ = :telemetry.attach(handler_id, [class, type], handler, config)
+    end
+
+    :ignore
+  end
+
+  @doc false
+  defmacro __using__(_opts) do
+    quote do
+      alias FarmbotTelemetry.{
+              AMQPClass,
+              DNSClass,
+              HTTPClass,
+              NetworkClass
+            },
+            warn: false
+
+      require FarmbotTelemetry
+    end
+  end
+
+  @doc "Execute a telemetry event. `type` and `action` will be validated"
+  def execute(class, type, action, meta \\ %{}) do
+    :telemetry.execute([class, type], %{action: action, timestamp: DateTime.utc_now()}, meta)
+    # quote location: :keep,
+    #       bind_quoted: [class: class, type: type, action: action, meta: Macro.escape(meta)] do
+    #   # unless {type, action} in class.matrix() do
+    #   #   raise """
+    #   #   #{type}.#{action} is unknown for #{class}
+    #   #   """
+    #   # end
+
+    #   _ = FarmbotTelemetry.unsafe_execute(class, type, action, meta)
+    # end
+  end
+
+  @doc false
+  def unsafe_execute(class, type, action, meta \\ %{}) do
+    :telemetry.execute([class, type], %{action: action, timestamp: DateTime.utc_now()}, meta)
   end
 end
