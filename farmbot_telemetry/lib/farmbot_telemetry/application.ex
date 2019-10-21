13f081e2c98d60288604c0e9d57c9525de3e392e
Connor Rigby <connor@farmbot.io>
2019-10-21 13:46:07 -0700

    Simplify telemetry systems

--- farmbot_telemetry/lib/farmbot_telemetry/application.ex
+++ farmbot_telemetry/lib/farmbot_telemetry/application.ex
@@ -2,28 +2,11 @@ defmodule FarmbotTelemetry.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
-  alias FarmbotTelemetry.LogHandler
-
-  alias FarmbotTelemetry.{
-    AMQPEventClass,
-    DNSEventClass,
-    HTTPEventClass,
-    NetworkEventClass
-  }
 
   use Application
 
   def start(_type, _args) do
-    children =
-      for class <- [AMQPEventClass, DNSEventClass, HTTPEventClass, NetworkEventClass] do
-        {FarmbotTelemetry,
-         [
-           class: class,
-           handler_id: "#{class}-LogHandler",
-           handler: &LogHandler.handle_event/4,
-           config: [level: :info]
-         ]}
-      end
+    children = []
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
