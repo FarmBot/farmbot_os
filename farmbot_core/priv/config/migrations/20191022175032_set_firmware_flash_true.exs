921128817094f882f274b996d0b0cbcb6ed4a461
Connor Rigby <connor@farmbot.io>
2019-10-22 10:52:27 -0700

    Force firmware to be flashed

---
+++ farmbot_core/priv/config/migrations/20191022175032_set_firmware_flash_true.exs
@@ -0,0 +1,11 @@
+defmodule FarmbotCore.Config.Repo.Migrations.SetFirmwareFlashTrue do
+  use Ecto.Migration
+
+  def up do
+    FarmbotCore.Config.update_config_value(:bool, "settings", "firmware_needs_flash", true)
+  end
+
+  def down do
+    FarmbotCore.Config.update_config_value(:bool, "settings", "firmware_needs_flash", true)
+  end
+end
