299cda8d672a7aef36f932247c1f5e4ce07a0a79
Connor Rigby <connor@farmbot.io>
2019-10-24 08:54:16 -0700

    Allow events without a measurement

--- farmbot_telemetry/test/farmbot_telemetry_test.exs
+++ farmbot_telemetry/test/farmbot_telemetry_test.exs
@@ -8,7 +8,7 @@ defmodule FarmbotTelemetryTest do
     :ok = FarmbotTelemetry.attach_recv(:event, :test_subsystem, self())
     :ok = FarmbotTelemetry.event(:test_subsystem, :measurement, 1.0)
 
-    assert_receive {[:farmbot_telemetry, :event, :test_subsystem], %{:measurement => 1.0}, _meta,
-                    _config}
+    assert_receive {[:farmbot_telemetry, :event, :test_subsystem],
+                    %{measurement: :measurement, value: 1.0}, _meta, _config}
   end
 end
