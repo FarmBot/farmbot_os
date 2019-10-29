a33fbfa42267c5b76a6db649f4101f38dbb556df
Connor Rigby <connor@farmbot.io>
2019-10-29 09:06:28 -0700

    update release candidate script

--- scripts/release_candidate.exs
+++ scripts/release_candidate.exs
@@ -2,7 +2,7 @@
 {_, 0} = System.cmd("git", ["checkout", "staging"], into: IO.stream(:stdio, :line))
 {_, 0} = System.cmd("git", ["fetch", "--all"], into: IO.stream(:stdio, :line))
 {_, 0} = System.cmd("git", ["reset", "--hard", "origin/staging"], into: IO.stream(:stdio, :line))
-version = File.read!("VERSION") |> Version.parse!()
+version = File.read!("VERSION") |> String.trim() |> Version.parse!()
 [<<"rc", rc :: binary>>] = version.pre
 version = %{version | pre: ["rc#{String.to_integer(rc) + 1}"]}
 :ok = File.write!("VERSION", to_string(version))
