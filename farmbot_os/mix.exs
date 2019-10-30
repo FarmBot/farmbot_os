a9debcc2e587241fbb4b85c9eb7ba390125809b7
Connor Rigby <connor@farmbot.io>
2019-10-30 11:34:32 -0700

    Update vintage_net and farmbot_system_rpi3

--- farmbot_os/mix.exs
+++ farmbot_os/mix.exs
@@ -101,10 +101,12 @@ defmodule FarmbotOS.MixProject do
       {:nerves_firmware_ssh, "~> 0.4", targets: @all_targets},
       {:circuits_gpio, "~> 0.4", targets: @all_targets},
       {:toolshed, "~> 0.2", targets: @all_targets},
-      {:vintage_net, "~> 0.6", targets: @all_targets},
+      {:vintage_net,
+       github: "nerves-networking/vintage_net", tag: "eap-status", targets: @all_targets},
       {:mdns_lite, "~> 0.4", targets: @all_targets},
       {:busybox, "~> 0.1", targets: @all_targets},
-      {:farmbot_system_rpi3, "1.8.0-farmbot.2", runtime: false, targets: :rpi3},
+      {:nerves_system_br, "~> 1.9", override: true, runtime: false, targets: @all_targets},
+      {:farmbot_system_rpi3, "1.9.0-farmbot.1", runtime: false, targets: :rpi3},
       {:farmbot_system_rpi0, "1.8.0-farmbot.0", runtime: false, targets: :rpi0},
       {:farmbot_system_rpi, "1.8.0-farmbot.0", runtime: false, targets: :rpi}
     ]
