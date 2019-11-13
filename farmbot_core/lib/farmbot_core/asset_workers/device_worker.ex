b49dd522012ee9e3059573b5b1479da8613ca959
Connor Rigby <connor@farmbot.io>
2019-11-13 14:41:29 -0800

    Add a log for when ota-hour is updated

--- farmbot_core/lib/farmbot_core/asset_workers/device_worker.ex
+++ farmbot_core/lib/farmbot_core/asset_workers/device_worker.ex
@@ -1,8 +1,9 @@
 defimpl FarmbotCore.AssetWorker, for: FarmbotCore.Asset.Device do
   alias FarmbotCore.Asset.Device
   use GenServer
+  require FarmbotCore.Logger
 
-  def tracks_changes?(%Device{}), do: false
+  def tracks_changes?(%Device{}), do: true
 
   def preload(%Device{}), do: []
 
@@ -16,5 +17,28 @@ defimpl FarmbotCore.AssetWorker, for: FarmbotCore.Asset.Device do
 
   def handle_info(:timeout, %Device{} = device) do
     {:noreply, device}
+  end
+
+  def handle_cast({:new_data, new_device}, old_device) do
+    _ = log_changes(new_device, old_device)
+    {:noreply, new_device}
+  end
+
+  def log_changes(new_device, old_device) do
+    interesting_params = [
+      :ota_hour
+    ]
+    new_interesting_device = Map.take(new_device, interesting_params) |> MapSet.new()
+    old_interesting_device = Map.take(old_device, interesting_params) |> MapSet.new()
+    difference = MapSet.difference(new_interesting_device, old_interesting_device)
+    Enum.each(difference, fn
+      {:ota_hour, nil} ->
+        FarmbotCore.Logger.success 1, "Farmbot will apply updates as soon as possible"
+      {:ota_hour, hour} ->
+        FarmbotCore.Logger.success 1, "Farmbot will apply updates at #{hour}"
+
+      {_key, _value} ->
+        :noop
+    end)
   end
 end
