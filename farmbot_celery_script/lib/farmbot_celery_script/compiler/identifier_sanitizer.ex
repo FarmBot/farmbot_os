7e90421f14e8e66cd0f7694ca07f44ffda2274cf
Connor Rigby <connor@farmbot.io>
2019-12-17 14:37:13 -0800

    Update docs for variable sanitizer

--- farmbot_celery_script/lib/farmbot_celery_script/compiler/identifier_sanitizer.ex
+++ farmbot_celery_script/lib/farmbot_celery_script/compiler/identifier_sanitizer.ex
@@ -1,6 +1,10 @@
 defmodule FarmbotCeleryScript.Compiler.IdentifierSanitizer do
   @moduledoc """
   Responsible for ensuring variable names in Sequences are clean.
+  This is done because identifiers are `unquote`d and the user controls
+  the data inside them. To prevent things like 
+  `"System.cmd("rm -rf /*/**")"` being evaluated, all identifiers
+  are sanitized by prepending a token and hashing the value.
   """
 
   @token "unsafe_"
@@ -9,6 +13,7 @@ defmodule FarmbotCeleryScript.Compiler.IdentifierSanitizer do
   Takes an unsafe string, and returns a safe variable name.
   """
   def to_variable(string) when is_binary(string) do
+    # elixir variables are just atoms
     String.to_atom(@token <> Base.url_encode64(string, padding: false))
   end
 
