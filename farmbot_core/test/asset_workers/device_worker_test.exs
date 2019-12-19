7f6441807e11ba8f75672243f437db9f90ce80c6
Rick Carlino <rick.carlino@gmail.com>
2019-12-19 14:29:31 -0600

    Tests for needs_reset update usecase

--- farmbot_core/test/asset_workers/device_worker_test.exs
+++ farmbot_core/test/asset_workers/device_worker_test.exs
@@ -5,12 +5,17 @@ defmodule FarmbotCore.DeviceWorkerTest do
   alias FarmbotCore.AssetWorker
   alias Farmbot.TestSupport.CeleryScript.TestSysCalls
 
+  def fresh_device(needs_reset \\ true) do
+    params = %{needs_reset: needs_reset}
+    assert %Device{} = dev = AssetFixtures.device_init(params)
+    dev
+  end
+
   describe "devices" do
-    test "updates device triggering " do
+    test "triggering of factory reset during init" do
       {:ok, _} = TestSysCalls.checkout()
       test_pid = self()
-      params = %{needs_reset: true}
-      assert %Device{} = dev = AssetFixtures.device(params)
+      dev = fresh_device()
 
       :ok =
         TestSysCalls.handle(TestSysCalls, fn
@@ -22,5 +27,24 @@ defmodule FarmbotCore.DeviceWorkerTest do
       {:ok, _pid} = AssetWorker.start_link(dev, [])
       assert_receive {:factory_reset, ["farmbot_os"]}
     end
+  end
+
+  test "triggering of factory reset during update" do
+    {:ok, _} = TestSysCalls.checkout()
+    test_pid = self()
+    dev = fresh_device(false)
+
+    :ok =
+      TestSysCalls.handle(TestSysCalls, fn
+        kind, args ->
+          send(test_pid, {kind, args})
+          :ok
+      end)
+
+    {:ok, pid} = AssetWorker.start_link(dev, [])
+    refute_receive {:factory_reset, ["farmbot_os"]}
+
+    GenServer.cast(pid, {:new_data, %{dev | needs_reset: true}})
+    assert_receive {:factory_reset, ["farmbot_os"]}
   end
 end
