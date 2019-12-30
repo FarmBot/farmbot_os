f2e5ce9723a4dcdf60884e92cbac294c815427ac
connor rigby <connor@farmbot.io>
2019-12-30 09:21:34 -0800

    Fix rpi3 configuration error

--- farmbot_os/config/target/rpi3.exs
+++ farmbot_os/config/target/rpi3.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :farmbot_core, FarmbotCore.FirmwareTTYDetector, expected_names: ["ttyUSB0", "ttyACM0"]
 
-config :farmbot_firmware, FarmbotFirmware, reset: FarmbotOS.Platform.Target.FirmwareReset.NULL
+config :farmbot_firmware, FarmbotFirmware, reset: FarmbotFirmware.NullReset
 
 config :farmbot, FarmbotOS.Init.Supervisor,
   init_children: [
