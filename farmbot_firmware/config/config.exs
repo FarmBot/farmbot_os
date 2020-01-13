4b923865347c1a59bf7eb3b8189efc486362194f
Rick Carlino <rick.carlino@gmail.com>
2020-01-13 14:25:16 -0600

    Remove useless guard clause

--- farmbot_firmware/config/config.exs
+++ farmbot_firmware/config/config.exs
@@ -2,6 +2,4 @@ use Mix.Config
 
 if Mix.env() == :test do
   config :farmbot_firmware, :uart_adapter, FarmbotFirmware.UartTestAdapter
-else
-  IO.puts("!!!!!!!!! On CircleCI, Mix.env() == " <> Mix.env())
 end
