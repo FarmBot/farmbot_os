aae4751568f10dbd8ea6497338f6180380b3cfd5
Rick Carlino <rick.carlino@gmail.com>
2020-01-28 12:54:05 -0600

    Request.request_timeout tests

--- farmbot_firmware/test/farmbot_firmware/request_test.exs
+++ farmbot_firmware/test/farmbot_firmware/request_test.exs
@@ -11,4 +11,11 @@ defmodule FarmbotFirmware.RequestTest do
 
     assert_raise ArgumentError, boom
   end
+
+  test "timeout handler" do
+    ok = Request.request_timeout(:tag, :code, :result)
+    error = Request.request_timeout(:tag, :code)
+    assert {:ok, {:tag, :result}} == ok
+    assert {:error, "timeout waiting for request to complete: :code"} == error
+  end
 end
