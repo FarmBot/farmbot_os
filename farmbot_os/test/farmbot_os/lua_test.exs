1dee7fbfbea1b494914062bdb3f8d5e6ba2b18f7
Rick Carlino <rick.carlino@gmail.com>
2020-02-10 12:48:04 -0600

    Tests for Lua related functionality

---
+++ farmbot_os/test/farmbot_os/lua_test.exs
@@ -0,0 +1,28 @@
+defmodule FarmbotOS.LuaTest do
+  use ExUnit.Case
+  use Mimic
+  setup :verify_on_exit!
+  alias FarmbotOS.Lua
+
+  test "evaluates Lua" do
+    assert Lua.eval_assertion("Returns 'true'", "return true")
+    {:error, message1} = Lua.eval_assertion("Returns 'true'", "-1")
+
+    assert message1 ==
+             "failed to parse expression (line:1): syntax error before: '-'"
+
+    {:error, error} = Lua.eval_assertion("random error", "return (1/0)")
+    assert error == :badarith
+  end
+
+  test "assertion logs" do
+    # Hmmm
+    expect(FarmbotCore.LogExecutor, :execute, 1, fn log ->
+      assert log.level == "assertion"
+      assert log.message == "this is an assertion"
+      assert log.meta == %{assertion_passed: true, assertion_type: :assertion}
+    end)
+
+    Lua.log_assertion(true, :assertion, "this is an assertion")
+  end
+end
