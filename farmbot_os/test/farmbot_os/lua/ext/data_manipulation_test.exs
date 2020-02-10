8e2626ce71a1910c2e10457b58779e8cff5a5a7b
Rick Carlino <rick.carlino@gmail.com>
2020-02-10 16:13:55 -0600

    Begin Lua data manipulaltion tests

--- farmbot_os/test/farmbot_os/lua/ext/data_manipulation_test.exs
+++ farmbot_os/test/farmbot_os/lua/ext/data_manipulation_test.exs
@@ -2,9 +2,21 @@ defmodule FarmbotOS.FarmbotOS.Lua.Ext.DataManipulationTest do
   use ExUnit.Case
   use Mimic
   setup :verify_on_exit!
-  alias FarmbotOS.FarmbotOS.Lua.Ext.DataManipulation
 
-  test "work in progress" do
-    raise "WIP"
+  def lua(test_name, lua_code) do
+    FarmbotOS.Lua.eval_assertion(test_name, lua_code)
+  end
+
+  test "update_device()" do
+    expect(FarmbotCore.Asset, :update_device!, 1, fn params ->
+      assert %{"name" => "Test Farmbot"} == params
+    end)
+
+    lua_code = """
+    update_device({name = "Test Farmbot"})
+    return true
+    """
+
+    assert true == lua("update device test", lua_code)
   end
 end
