8429aa2331ab88cb0cd6bfaa1c795a0f33c5f091
Rick Carlino <rick.carlino@gmail.com>
2020-02-10 14:18:33 -0600

    WIP

---
+++ farmbot_os/test/farmbot_os/lua/ext/data_manipulation_test.exs
@@ -0,0 +1,10 @@
+defmodule FarmbotOS.FarmbotOS.Lua.Ext.DataManipulationTest do
+  use ExUnit.Case
+  use Mimic
+  setup :verify_on_exit!
+  alias FarmbotOS.FarmbotOS.Lua.Ext.DataManipulation
+
+  test "work in progress" do
+    raise "WIP"
+  end
+end
