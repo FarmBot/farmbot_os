796ef547efbf1da4654295f306454bda07d9a1e7
Rick Carlino <rick.carlino@gmail.com>
2020-02-13 16:15:21 -0600

    (36.2%%) Continue farmbot_os testing (movement.ex)

--- farmbot_os/test/farmbot_os/syscalls/movement_test.exs
+++ farmbot_os/test/farmbot_os/syscalls/movement_test.exs
@@ -4,6 +4,47 @@ defmodule FarmbotOS.SysCalls.MovementTest do
   setup :verify_on_exit!
   alias FarmbotOS.SysCalls.Movement
 
+  test "move_absolute/4" do
+    FarmbotFirmware
+    |> expect(:request, 3, fn
+      {:parameter_read, [:movement_max_spd_x]} ->
+        {:ok, {:tag, {:report_parameter_value, [{:movement_max_spd_x, 1}]}}}
+
+      {:parameter_read, [:movement_max_spd_y]} ->
+        {:ok, {:tag, {:report_parameter_value, [{:movement_max_spd_y, 2}]}}}
+
+      {:parameter_read, [:movement_max_spd_z]} ->
+        {:ok, {:tag, {:report_parameter_value, [{:movement_max_spd_z, 3}]}}}
+    end)
+    |> expect(:command, 1, fn {nil, {:command_movement, params}} ->
+      expected = [x: 1.0, y: 2.0, z: 3.0, a: 0.04, b: 0.08, c: 0.12]
+      assert expected == params
+      :ok
+    end)
+
+    result = Movement.move_absolute(1, 2, 3, 4)
+    assert :ok == result
+  end
+
+  test "move_absolute/4 - error (in tuple)" do
+    expect(FarmbotFirmware, :request, 1, fn {:parameter_read, [_]} ->
+      {:error, "boom"}
+    end)
+
+    message = "Movement failed. \"boom\""
+
+    assert message == Movement.move_absolute(1, 2, 3, 4).message
+  end
+
+  test "get_position/1 - error" do
+    expect(FarmbotFirmware, :request, 1, fn {nil, {:position_read, []}} ->
+      {:error, "boom"}
+    end)
+
+    message = "Firmware error @ \"get_position\": \"boom\""
+    assert {:error, ^message} = Movement.get_position(:x)
+  end
+
   test "zero()" do
     mock = fn
       {:position_write_zero, [:x]} ->
