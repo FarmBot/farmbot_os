b59ece24fbce8503591323f8d71b3f905d560506
Rick Carlino <rick.carlino@gmail.com>
2020-02-17 12:13:44 -0600

    (40.7%) PointLookup failure case tests

--- farmbot_os/test/farmbot_os/syscalls/point_lookup_test.exs
+++ farmbot_os/test/farmbot_os/syscalls/point_lookup_test.exs
@@ -2,12 +2,17 @@ defmodule FarmbotOS.SysCalls.PointLookupTest do
   use ExUnit.Case
   use Mimic
   setup :verify_on_exit!
-  # alias FarmbotOS.SysCalls.PointLookup
-  # alias FarmbotOS.SysCalls.ResourceUpdate
+  alias FarmbotOS.SysCalls.PointLookup
+  alias FarmbotOS.SysCalls.ResourceUpdate
 
-  # test "" do
-  #   first_point = ResourceUpdate.resource_update("Plant", 24, %{})
-  #   same_point = PointLookup.point("GenericPointer", 24)
-  #   assert first_point == same_point
-  # end
+  test "failure cases" do
+    err1 = PointLookup.point("GenericPointer", 24)
+    assert {:error, "GenericPointer not found"} == err1
+
+    err2 = PointLookup.get_toolslot_for_tool(24)
+    assert {:error, "Could not find point for tool by id: 24"} == err2
+
+    err3 = PointLookup.get_point_group(24)
+    assert {:error, "Could not find PointGroup.24"} == err3
+  end
 end
