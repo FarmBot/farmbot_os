510575b59604b2551605e31362cc45a0663d11c5
Rick Carlino <rick.carlino@gmail.com>
2020-02-17 12:45:31 -0600

    (40.8%) PointLookup.point/2

--- farmbot_os/test/farmbot_os/syscalls/point_lookup_test.exs
+++ farmbot_os/test/farmbot_os/syscalls/point_lookup_test.exs
@@ -4,6 +4,8 @@ defmodule FarmbotOS.SysCalls.PointLookupTest do
   setup :verify_on_exit!
   alias FarmbotOS.SysCalls.PointLookup
   alias FarmbotOS.SysCalls.ResourceUpdate
+  alias FarmbotCore.Asset.Point
+  alias FarmbotCore.Asset.Repo
 
   test "failure cases" do
     err1 = PointLookup.point("GenericPointer", 24)
@@ -14,5 +16,21 @@ defmodule FarmbotOS.SysCalls.PointLookupTest do
 
     err3 = PointLookup.get_point_group(24)
     assert {:error, "Could not find PointGroup.24"} == err3
+  end
+
+  test "PointLookup.point/2" do
+    expected = %{
+      name: "test suite III",
+      x: 1.2,
+      y: 3.4,
+      z: 5.6
+    }
+   point = %Point{id: 555, pointer_type: "GenericPointer"}
+
+    Map.merge(point, expected)
+      |> Point.changeset()
+      |> Repo.insert!()
+
+    assert expected == PointLookup.point("GenericPointer", point.id)
   end
 end
