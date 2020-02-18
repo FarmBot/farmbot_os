0fed24eadb269a6c7905ebceedba2d65979e8d04
Rick Carlino <rick.carlino@gmail.com>
2020-02-18 12:55:56 -0600

    Asset related tests

--- farmbot_core/test/asset/command_test.exs
+++ farmbot_core/test/asset/command_test.exs
@@ -1,6 +1,46 @@
 defmodule FarmbotCore.Asset.CommandTest do
   use ExUnit.Case, async: false
-  alias FarmbotCore.{Asset, Asset.Command}
+
+  alias FarmbotCore.{
+    Asset,
+    Asset.Device,
+    Asset.FbosConfig,
+    Asset.FirmwareConfig,
+    Asset.Command
+  }
+
+  test "update / destroy firmware config" do
+    params = %{id: 23, movement_invert_motor_x: 12.34}
+    :ok = Command.update(FirmwareConfig, 23, params)
+    config = Enum.at(Asset.Repo.all(FirmwareConfig), 0)
+    assert config.movement_invert_motor_x == params[:movement_invert_motor_x]
+
+    :ok = Command.update(FirmwareConfig, 23, nil)
+    next_config = Enum.at(Asset.Repo.all(FirmwareConfig), 0)
+    refute next_config
+  end
+
+  test "update / destroy fbos config" do
+    params = %{id: 23, update_channel: "whatever"}
+    :ok = Command.update(FbosConfig, 23, params)
+    config = Enum.at(Asset.Repo.all(FbosConfig), 0)
+    assert config.update_channel == params[:update_channel]
+
+    :ok = Command.update(FbosConfig, 23, nil)
+    next_config = Enum.at(Asset.Repo.all(FbosConfig), 0)
+    refute next_config
+  end
+
+  test "update / destroy device" do
+    params = %{id: 23, name: "Old Device"}
+    :ok = Command.update(Device, 23, params)
+    device = Enum.at(Asset.Repo.all(Device), 0)
+    assert device.name == params[:name]
+
+    :ok = Command.update(Device, 23, nil)
+    next_device = Enum.at(Asset.Repo.all(Device), 0)
+    refute next_device
+  end
 
   test "insert new regimen" do
     id = id()
