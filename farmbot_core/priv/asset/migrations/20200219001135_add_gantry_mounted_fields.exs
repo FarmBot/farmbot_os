7e3c252eebc7b23969da0b7f952b7107482d44bd
Rick Carlino <rick.carlino@gmail.com>
2020-02-19 12:42:25 -0600

    Tests for gantry mounted tools

--- farmbot_core/priv/asset/migrations/20200219001135_add_gantry_mounted_fields.exs
+++ farmbot_core/priv/asset/migrations/20200219001135_add_gantry_mounted_fields.exs
@@ -10,6 +10,3 @@ defmodule FarmbotCore.Asset.Repo.Migrations.AddGantryMountedFields do
     execute("UPDATE points SET updated_at = \"1970-11-07 16:52:31.618000\"")
   end
 end
-
-# farmbot_core/priv/asset/migrations/20190926161914_add_point_discarded_at.exs
-# farmbot_core/priv/repo/migrations/20200219001135_add_gantry_mounted_fields.exs
