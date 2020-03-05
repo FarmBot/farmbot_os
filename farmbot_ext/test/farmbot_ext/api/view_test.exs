0058d1b01e702eac8616145957de45a0cd505c62
Rick Carlino <rick.carlino@gmail.com>
2020-03-05 15:41:54 -0600

    Update coveralls.json, tests for View.render/1

---
+++ farmbot_ext/test/farmbot_ext/api/view_test.exs
@@ -0,0 +1,13 @@
+defmodule FarmbotExt.API.ViewTest do
+    use ExUnit.Case
+
+    def render(%{ok: :ok}) do
+      :yep
+    end
+
+    test "render/2" do
+      result = FarmbotExt.API.View.render(__MODULE__, %{ok: :ok})
+      assert :yep == result
+    end
+  end
+  
