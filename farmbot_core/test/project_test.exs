0f24ed894c2b250f2f9d301f2c62ec9b94b5a878
Rick Carlino <rick.carlino@gmail.com>
2020-04-02 14:55:11 -0500

    Revert easy win that was not so easy

--- farmbot_core/test/project_test.exs
+++ farmbot_core/test/project_test.exs
@@ -1,10 +1,15 @@
 defmodule FarmbotCore.ProjectTest do
   use ExUnit.Case
+  @opts [cd: Path.join("c_src", "farmbot-arduino-firmware")]
 
   test "arduino_commit" do
     actual = FarmbotCore.Project.arduino_commit()
-    expected = "0c4b14eb1bec8d466fbb815bfd7ee13b0b2d8c91"
-    # Prevents me from accidentally releasing wrong firmware.
+
+    expected =
+      System.cmd("git", ~w"rev-parse --verify HEAD", @opts)
+      |> elem(0)
+      |> String.trim()
+
     assert expected == actual
   end
 end
