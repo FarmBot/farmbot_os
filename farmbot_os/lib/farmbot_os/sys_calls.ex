8d70e8c4547869d73da91639f5df917aebfe7eb2
Rick Carlino <rick.carlino@gmail.com>
2020-04-13 15:49:53 -0500

    Elock / unlock verbiage

--- farmbot_os/lib/farmbot_os/sys_calls.ex
+++ farmbot_os/lib/farmbot_os/sys_calls.ex
@@ -205,12 +205,14 @@ defmodule FarmbotOS.SysCalls do
   @impl true
   def emergency_lock do
     _ = FarmbotFirmware.command({:command_emergency_lock, []})
+    FarmbotCore.Logger.busy(1, "E-stopped")
     :ok
   end
 
   @impl true
   def emergency_unlock do
     _ = FarmbotFirmware.command({:command_emergency_unlock, []})
+    FarmbotCore.Logger.busy(1, "Unlocked.")
     :ok
   end
 
