18b5f3779b210cd1b394d56cbc928ebf2ed61e10
Rick Carlino <rick.carlino@gmail.com>
2020-05-03 11:00:06 -0500

    Ready for QA

---
+++ farmbot_core/lib/farmbot_core/firmware_resetter.ex
@@ -0,0 +1,54 @@
+defmodule FarmbotCore.FirmwareResetter do
+  defmodule Stub do
+    require FarmbotCore.Logger
+
+    def fail do
+      m = "Reset function NOT FOUND. Please notify FarmBot support."
+      FarmbotCore.Logger.error(3, m)
+      {:error, m}
+    end
+
+    def open(_, _), do: fail()
+    def write(_, _), do: fail()
+  end
+
+  @gpio Application.get_env(:farmbot, __MODULE__, [])[:gpio] || Stub
+  alias FarmbotCore.Asset
+  require FarmbotCore.Logger
+
+  def reset(package \\ nil) do
+    pkg = package || Asset.fbos_config(:firmware_hardware)
+    FarmbotCore.Logger.debug(3, "Attempting to retrieve #{pkg} reset function.")
+    {:ok, fun} = find_reset_fun(pkg)
+    fun.()
+  end
+
+  def find_reset_fun("express_k10") do
+    FarmbotCore.Logger.debug(3, "Using special express reset function")
+    {:ok, fn -> express_reset_fun() end}
+  end
+
+  def find_reset_fun(_) do
+    FarmbotCore.Logger.debug(3, "Using default reset function")
+    {:ok, fn -> :ok end}
+  end
+
+  def express_reset_fun() do
+    try do
+      FarmbotCore.Logger.debug(3, "Begin MCU reset")
+      {:ok, gpio} = @gpio.open(19, :output)
+      :ok = @gpio.write(gpio, 0)
+      :ok = @gpio.write(gpio, 1)
+      Process.sleep(1000)
+      :ok = @gpio.write(gpio, 0)
+      FarmbotCore.Logger.debug(3, "Finish MCU Reset")
+      :ok
+    rescue
+      ex ->
+        message = Exception.message(ex)
+        msg = "Could not reset express firmware: #{message}"
+        FarmbotCore.Logger.error(3, msg)
+        :express_reset_error
+    end
+  end
+end
--- farmbot_os/lib/farmbot_os/sys_calls/flash_firmware.ex
+++ farmbot_os/lib/farmbot_os/sys_calls/flash_firmware.ex
@@ -1,26 +1,9 @@
 defmodule FarmbotOS.SysCalls.FlashFirmware do
   @moduledoc false
 
-  alias FarmbotCore.{Asset, Asset.Private}
+  alias FarmbotCore.{Asset, Asset.Private, FirmwareResetter}
   alias FarmbotFirmware
   alias FarmbotCore.FirmwareTTYDetector
-
-  defmodule Stub do
-    require FarmbotCore.Logger
-
-    def fail do
-      m = "No express function found. Please notify support."
-      FarmbotCore.Logger.error(3, m)
-      {:error, m}
-    end
-
-    def open(_, _), do: fail()
-    def write(_, _), do: fail()
-  end
-
-  # This only matter for express.
-  # When it's an express, use Circuits.GPIO.
-  @gpio Application.get_env(:farmbot, __MODULE__, [])[:gpio] || Stub
 
   import FarmbotFirmware.PackageUtils,
     only: [find_hex_file: 1, package_to_string: 1]
@@ -38,7 +21,7 @@ defmodule FarmbotOS.SysCalls.FlashFirmware do
          {:ok, tty} <- find_tty(),
          _ <-
            FarmbotCore.Logger.debug(3, "found tty: #{tty} for firmware flash"),
-         {:ok, fun} <- find_reset_fun(package),
+         {:ok, fun} <- FirmwareResetter.find_reset_fun(package),
          _ <-
            FarmbotCore.Logger.debug(
              3,
@@ -82,34 +65,6 @@ defmodule FarmbotOS.SysCalls.FlashFirmware do
 
       tty ->
         {:ok, tty}
-    end
-  end
-
-  def find_reset_fun("express_k10") do
-    FarmbotCore.Logger.debug(3, "Using special express reset function")
-    {:ok, fn -> express_reset_fun() end}
-  end
-
-  def find_reset_fun(_) do
-    FarmbotCore.Logger.debug(3, "Using default reset function")
-    {:ok, &FarmbotFirmware.NullReset.reset/0}
-  end
-
-  def express_reset_fun() do
-    try do
-      FarmbotCore.Logger.debug(3, "Begin MCU reset")
-      {:ok, gpio} = @gpio.open(19, :output)
-      :ok = @gpio.write(gpio, 0)
-      :ok = @gpio.write(gpio, 1)
-      Process.sleep(1000)
-      :ok = @gpio.write(gpio, 0)
-      FarmbotCore.Logger.debug(3, "Finish MCU Reset")
-      :ok
-    rescue
-      ex ->
-        message = Exception.message(ex)
-        Logger.error("Could not flash express firmware: #{message}")
-        :express_reset_error
     end
   end
 end
