bc6ad08ae74710d5b96d4be016f2e89253561afe
Rick Carlino <rick.carlino@gmail.com>
2020-05-28 10:07:26 -0500

    Implement fix suggested by DIY forum users, remove unused var warning

--- farmbot_core/lib/farmbot_core/firmware_side_effects.ex
+++ farmbot_core/lib/farmbot_core/firmware_side_effects.ex
@@ -11,7 +11,7 @@ defmodule FarmbotCore.FirmwareSideEffects do
   end
 
   @impl FarmbotFirmware.SideEffects
-  def handle_load([u, x, v, y, w, z]) do
+  def handle_load([_u, x, _v, y, _w, z]) do
     :ok = BotState.set_load(x, y, z)
   end
 
--- farmbot_core/lib/farmbot_core/firmware_tty_detector.ex
+++ farmbot_core/lib/farmbot_core/firmware_tty_detector.ex
@@ -42,7 +42,19 @@ defmodule FarmbotCore.FirmwareTTYDetector do
   end
 
   def handle_info(:timeout, state) do
-    enumerated = UART.enumerate() |> Map.to_list()
+    # This was a feature request by a DIY user that experienced
+    # problems while using third party boards.
+    # The comparator function below always puts ttyAMA0 at the
+    # end of the list to prevent problems with these baords.
+    ama0_always_last = fn
+      {"ttyAMA0", _} -> "z"
+      _ -> "a"
+    end
+
+    enumerated = UART.enumerate()
+    |> Map.to_list()
+    |> Enum.sort_by(ama0_always_last)
+
     {:noreply, state, {:continue, enumerated}}
   end
 
