dfbc021e4c1433b64a8ce482bb8d88fdb77da04c
Rick Carlino <rick.carlino@gmail.com>
2020-05-29 15:33:39 -0500

    10.1.0-rc14

--- farmbot_firmware/lib/farmbot_firmware/transports/uart_default_adapter.ex
+++ farmbot_firmware/lib/farmbot_firmware/transports/uart_default_adapter.ex
@@ -2,10 +2,20 @@ defmodule FarmbotFirmware.UartDefaultAdapter do
   @moduledoc """
   A thin wrapper of Circuits.UART to simplify testing.
   """
-  defdelegate start_link, to: Circuits.UART, as: :start_link
-  defdelegate open(uart_pid, device_path, opts), to: Circuits.UART, as: :open
-  defdelegate stop(uart_pid), to: Circuits.UART, as: :stop
-  defdelegate write(uart_pid, str), to: Circuits.UART, as: :write
+  # defdelegate start_link, to: Circuits.UART, as: :start_link
+  # defdelegate open(uart_pid, device_path, opts), to: Circuits.UART, as: :open
+  # defdelegate stop(uart_pid), to: Circuits.UART, as: :stop
+  # defdelegate write(uart_pid, str), to: Circuits.UART, as: :write
+
+  # ========= STUB!!!
+  def open(_, _, _) do
+    :ok
+  end
+
+  # ========= STUB!!!
+  def write(_, _) do
+    :ok
+  end
 
   def generate_opts do
     [
