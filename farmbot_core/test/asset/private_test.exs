6dcf99de17e13af298f60647deda5400b4efb2c1
Rick Carlino <rick.carlino@gmail.com>
2020-06-24 14:12:46 -0500

    TEST: FarmbotCore.Asset.Private.list_local/1

---
+++ farmbot_core/test/asset/private_test.exs
@@ -0,0 +1,37 @@
+defmodule FarmbotCore.Asset.PrivateTest do
+  use ExUnit.Case, async: true
+
+  alias FarmbotCore.Asset.{
+    FbosConfig,
+    FirmwareConfig,
+    Point,
+    Private,
+    Repo
+  }
+
+  @assets [Point, FbosConfig, FirmwareConfig]
+
+  def destroy_assets() do
+    Enum.map(@assets, &Repo.delete_all/1)
+  end
+
+  def create_assets() do
+    %{
+      point: Repo.insert!(%Point{gantry_mounted: false, pullout_direction: 0}),
+      fbos_config: Repo.insert!(%FbosConfig{}),
+      firmware_config: Repo.insert!(%FirmwareConfig{})
+    }
+  end
+
+  test "list_local" do
+    destroy_assets()
+    old_results = Private.list_local(Point)
+    assert Enum.count(old_results) == 0
+
+    %{point: created_point} = create_assets()
+    new_results = Private.list_local(Point)
+    assert Enum.count(new_results) == 1
+    local_point = Enum.at(new_results, 0)
+    assert local_point == created_point
+  end
+end
