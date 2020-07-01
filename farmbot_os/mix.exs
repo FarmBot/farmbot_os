e8df96c0f08f8c5f0237e3ba8331cbc667fa0e76
Rick Carlino <rick.carlino@gmail.com>
2020-07-01 18:17:38 -0500

    rc20: Upgrade Nerves system

--- farmbot_os/mix.exs
+++ farmbot_os/mix.exs
@@ -126,7 +126,7 @@ defmodule FarmbotOS.MixProject do
       {:busybox, "~> 0.1.4", targets: @all_targets},
       {:farmbot_system_rpi3,
        git: "https://github.com/FarmBot/farmbot_system_rpi3.git",
-       tag: "v1.11.4-farmbot.2",
+       tag: "v1.11.4-farmbot.3",
        runtime: false,
        targets: :rpi3},
       {:farmbot_system_rpi,
