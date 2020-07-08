d67cc7108d8dac7bab73797e77843651865c9611
Rick Carlino <rick.carlino@gmail.com>
2020-07-08 17:44:48 -0500

    Remove ref to unused col in migration.

--- farmbot_core/priv/config/migrations/20190705203806_add_firmware_needs_open_flag.exs
+++ farmbot_core/priv/config/migrations/20190705203806_add_firmware_needs_open_flag.exs
@@ -1,10 +1,8 @@
 defmodule FarmbotCore.Config.Repo.Migrations.AddFirmwareNeedsOpenFlag do
   use Ecto.Migration
-  import FarmbotCore.Config, only: [update_config_value: 4]
   import FarmbotCore.Config.MigrationHelpers
 
   def change do
-    update_config_value(:bool, "settings", "firmware_needs_flash", true)
     create_settings_config("firmware_needs_open", :bool, false)
   end
 end
