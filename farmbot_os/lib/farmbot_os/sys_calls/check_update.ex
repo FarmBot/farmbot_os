f1f84efeedc59c16edf3eb87cae1d1eedfcc7a83
Rick Carlino <rick.carlino@gmail.com>
2020-09-16 13:48:28 -0500

    v12-rc6

--- farmbot_os/lib/farmbot_os/sys_calls/check_update.ex
+++ farmbot_os/lib/farmbot_os/sys_calls/check_update.ex
@@ -4,6 +4,18 @@ defmodule FarmbotOS.SysCalls.CheckUpdate do
   alias FarmbotOS.UpdateSupport
 
   def check_update() do
+    if UpdateSupport.in_progress?() do
+      dont_check_update()
+    else
+      do_check_update()
+    end
+  end
+
+  def dont_check_update() do
+    {:error, "Installation already started. Please wait or reboot."}
+  end
+
+  def do_check_update() do
     # Try to find the upgrade image URL (might be `nil`)
     url_or_nil =
       UpdateSupport.get_target()
