c14e458bbe37b17834cb41563d0d53347cd35c2b
Rick Carlino <rick.carlino@gmail.com>
2020-09-23 10:18:05 -0500

    Tests for TerminalChannel base cases

--- farmbot_ext/test/farmbot_ext/amqp/terminal_channel_test.exs
+++ farmbot_ext/test/farmbot_ext/amqp/terminal_channel_test.exs
@@ -3,8 +3,26 @@ defmodule FarmbotExt.AMQP.TerminalChannelTest do
   use Mimic
   setup :verify_on_exit!
   setup :set_mimic_global
+  alias FarmbotExt.AMQP.TerminalChannel
+  alias FarmbotExt.AMQP.TerminalChannelSupport, as: Support
 
-  test "terminal channel" do
-    raise "TODO"
+  test "terminal channel startup" do
+    jwt = %FarmbotExt.JWT{bot: "device_#{Enum.random(1000..100_000)}"}
+    chan = %{fake_chan: true}
+
+    expect(Support, :get_channel, 1, fn bot ->
+      assert bot == jwt.bot
+      {:ok, chan}
+    end)
+
+    expect(FarmbotCore.LogExecutor, :execute, 1, fn log ->
+      assert log.message == "Connected to terminal channel"
+    end)
+
+    {:ok, pid} = TerminalChannel.start_link([jwt: jwt], [])
+    actual = :sys.get_state(pid)
+    expected = %TerminalChannel{chan: chan, iex_pid: nil, jwt: jwt}
+    assert actual == expected
+    GenServer.stop(pid, :normal)
   end
 end
