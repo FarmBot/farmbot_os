aff4cc0d23a2cf823475fd4d0e903189d6d9db70
Rick Carlino <rick.carlino@gmail.com>
2020-10-13 15:50:27 -0500

    [30.1%] Tests for FarmbotExt.Bootstrap.Authorization

--- farmbot_ext/test/farmbot_ext/bootstrap/authorization_test.exs
+++ farmbot_ext/test/farmbot_ext/bootstrap/authorization_test.exs
@@ -1,9 +1,12 @@
 defmodule FarmbotExt.Bootstrap.AuthorizationTest do
+  require Helpers
+
   use ExUnit.Case
   use Mimic
 
   alias FarmbotCore.JSON
   alias FarmbotExt.Bootstrap.Authorization
+  alias FarmbotCore.JSON
 
   test "build_payload/1" do
     fake_secret =
@@ -14,5 +17,23 @@ defmodule FarmbotExt.Bootstrap.AuthorizationTest do
     {:ok, result} = Authorization.build_payload(fake_secret)
     expected = "{\"user\":{\"credentials\":\"ZXlKcWRYTjBJam9pWVNCMFpYTjBJbjA9\"}}"
     assert result == expected
+  end
+
+  test "build_secret/3" do
+    email = "test@test.com"
+    password = "password123"
+    pub_key = RSA.decode_key(Helpers.pub_key())
+    cyphertext = Authorization.build_secret(email, password, pub_key)
+    priv_key = RSA.decode_key(Helpers.priv_key())
+
+    %{
+      "email" => email_result,
+      "password" => password_result,
+      "version" => version
+    } = JSON.decode!(RSA.decrypt(cyphertext, {:private, priv_key}))
+
+    assert email_result == email
+    assert password_result == password
+    assert version == 1
   end
 end
--- farmbot_ext/vendor/rsa.ex
+++ farmbot_ext/vendor/rsa.ex
@@ -9,4 +9,10 @@ defmodule RSA do
     [entry] = :public_key.pem_decode(text)
     :public_key.pem_entry_decode(entry)
   end
+
+    # ONLY NEEDED FOR TESTS AND VERIFICATION.
+    # Decrypt using the private key
+    def decrypt(cyphertext, {:private, key}) do
+      cyphertext |> :public_key.decrypt_private(key)
+    end
 end
