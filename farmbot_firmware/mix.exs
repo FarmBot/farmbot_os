5c4bfa91ee4b4d9f54e68d899eb7b5c6ff957446
Rick Carlino <rick.carlino@gmail.com>
2020-10-22 15:38:06 -0500

    Dep updates - farmbot_firware

--- farmbot_firmware/mix.exs
+++ farmbot_firmware/mix.exs
@@ -54,7 +54,7 @@ defmodule FarmbotFirmware.MixProject do
   defp deps do
     [
       {:farmbot_telemetry, path: "../farmbot_telemetry", env: Mix.env()},
-      {:circuits_uart, "~> 1.4.0"},
+      {:circuits_uart, "~> 1.4.2"},
       {:excoveralls, "~> 0.13.3", only: [:test], targets: [:host]},
       {:mimic, "~> 1.3.1", only: :test},
       {:ex_doc, "~> 0.23.0", only: [:dev], targets: [:host], runtime: false}
