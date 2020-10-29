b2bea39053966ebf16e45788d74b65279fc3debc
Rick Carlino <rick.carlino@gmail.com>
2020-10-29 13:36:16 -0500

    Updates to run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -8,37 +8,43 @@ echo "######### farmbot_telemetry"
 cd ../farmbot_telemetry
 mix format
 mix deps.get --all
-mix test
+mix coveralls.html
+rm -f *.coverdata
 
 echo "######### farmbot_celery_script"
 cd ../farmbot_celery_script
 mix format
 mix deps.get --all
-mix test
+mix coveralls.html
+rm -f *.coverdata
 
 echo "######### farmbot_firmware"
 cd ../farmbot_firmware
 mix format
 mix deps.get --all
-mix test
+mix coveralls.html
+rm -f *.coverdata
 
 echo "######### farmbot_core"
 cd ../farmbot_core
 mix format
 mix deps.get --all
-mix test
+mix coveralls.html
+rm -f *.coverdata
 
 echo "######### farmbot_ext"
 cd ../farmbot_ext
 mix format
 mix deps.get --all
-mix test
+mix coveralls.html
+rm -f *.coverdata
 
 echo "######### farmbot_os"
 cd ../farmbot_os
 mix format
 mix deps.get --all
-mix test
+mix coveralls.html
+rm -f *.coverdata
 
 cd ..
 cd farmbot_os
