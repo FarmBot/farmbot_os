8b42b1de748e9d557781ea39b176fc32f91ac8ce
Rick Carlino <rick.carlino@gmail.com>
2021-07-13 15:01:09 -0500

    Only use latest soil height values when interpolating

--- farmbot_core/lib/farmbot_celery_script/special_value.ex
+++ farmbot_core/lib/farmbot_celery_script/special_value.ex
@@ -1,9 +1,9 @@
 defmodule FarmbotCeleryScript.SpecialValue do
   alias FarmbotCore.Asset.{ Repo, Point }
+  import Ecto.Query
   alias FarmbotCeleryScript.Interpolation
   require Logger
   require FarmbotCore.Logger
-  import Ecto.Query
   @msg "Need at least 3 soil height samples to guess soil height. "
     <> "Using fallback value instead: "
   def safe_height() do
@@ -22,11 +22,40 @@ defmodule FarmbotCeleryScript.SpecialValue do
     end
   end
 
-  defp soil_samples do
-    Repo.all(from(p in Point, where: like(p.meta, "%at_soil_level%")))
+  def soil_samples do
+    from(p in Point, where: like(p.meta, "%at_soil_level%"), order_by: p.updated_at)
+    |> Repo.all()
     |> Enum.filter(&is_soil_sample?/1)
+    |> index_by_location()
   end
 
   defp is_soil_sample?(%{meta: %{"at_soil_level" => "true"}}), do: true
   defp is_soil_sample?(_), do: false
+
+  # PROBLEM:
+  # * We have many soil readings.
+  # * Some readings may be duplicates.
+  # * If a soil sample has multiple readings, we only care
+  #   about latest version.
+  #
+  # RULES:
+  # * `list` is an Array of `Point` resources ORDERED BY `updated_at`
+  #   field, least recently updated points come first. The function
+  #   does NOT WORK ON UNSORTED LISTS
+  # * Round soil readings to the nearest 10mm.
+  # * When two soil readings measure the same spot, always use
+  #   the latest value.
+  def index_by_location(list) do
+    list
+    |> Enum.reduce(%{}, fn value, acc ->
+      %{x: x, y: y} = value
+      # If two values have the same X/Y coords, last write
+      # wins.
+      key = {round_to_10(x), round_to_10(y)}
+      Map.put(acc, key, value)
+    end)
+    |> Map.values()
+  end
+
+  defp round_to_10(number), do: round(number / 10) * 10
 end
--- farmbot_core/test/farmbot_celery_script/special_value_test.exs
+++ farmbot_core/test/farmbot_celery_script/special_value_test.exs
@@ -7,7 +7,24 @@ defmodule FarmbotCeleryScript.SpecialValueTest do
   setup :verify_on_exit!
 
   test "soil_height" do
-    IO.puts("==== TODO: Re-write this test!")
-    assert true
+    most_recent = %{x: 1, y: 1, updated_at: ~U[2021-07-12 12:34:56.789000Z]}
+    outlier = %{x: 100, y: 200, updated_at: ~U[2021-07-10 10:34:56.789000Z]}
+
+    # SCENARIO: One point in the garden has been measure 7 times over 7 days,
+    #           but we only want the most recent reading.
+    list = [
+      %{x: 1, y: 1, updated_at: ~U[2021-07-06 06:34:56.789000Z]},
+      %{x: 2, y: 1, updated_at: ~U[2021-07-07 07:34:56.789000Z]},
+      %{x: 3, y: 1, updated_at: ~U[2021-07-08 08:34:56.789000Z]},
+      %{x: 1, y: 1, updated_at: ~U[2021-07-09 09:34:56.789000Z]},
+      %{x: 1, y: 2, updated_at: ~U[2021-07-10 10:34:56.789000Z]},
+      outlier,
+      %{x: 1, y: 3, updated_at: ~U[2021-07-11 11:34:56.789000Z]},
+      most_recent
+    ]
+
+    expected = [most_recent, outlier]
+
+    assert SpecialValue.index_by_location(list) == expected
   end
 end
