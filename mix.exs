617872f009f0e1b62a9d1143cf42bdfbaa7c95d6
Rick Carlino <rick.carlino@gmail.com>
2021-12-21 17:41:07 -0600

    UNSTABLE / EXPERIMENTAL FBOS BUILD

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule FarmbotOS.MixProject do
   use Mix.Project
 
-  @all_targets [:rpi3, :rpi]
+  @all_targets [:rpi4, :rpi3, :rpi]
   @version Path.join([__DIR__, "VERSION"])
            |> File.read!()
            |> String.trim()
@@ -107,16 +107,9 @@ defmodule FarmbotOS.MixProject do
       {:ex_doc, "~> 0.25", only: [:dev], targets: [:host], runtime: false},
       {:excoveralls, "~> 0.14", only: [:test], targets: [:host]},
       {:extty, "~> 0.2.1"},
-      {:farmbot_system_rpi,
-       git: "https://github.com/FarmBot/farmbot_system_rpi.git",
-       ref: "v1.17.3-farmbot.1",
-       runtime: false,
-       targets: :rpi},
-      {:farmbot_system_rpi3,
-       git: "https://github.com/FarmBot/farmbot_system_rpi3.git",
-       tag: "v1.17.4-farmbot.1",
-       runtime: false,
-       targets: :rpi3},
+      {:nerves_system_rpi, "~> 1.17", targets: :rpi},
+      {:nerves_system_rpi3, "~> 1.17", targets: :rpi3},
+      {:nerves_system_rpi4, "~> 1.17", targets: :rpi4},
       {:hackney, "~> 1.18"},
       {:jason, "~> 1.2"},
       {:luerl, github: "rvirding/luerl", tag: "1.0"},
