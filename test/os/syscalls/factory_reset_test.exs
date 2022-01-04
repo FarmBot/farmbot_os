135412f9630d46319d28796c5068e413b93b52f8
Rick Carlino <rick.carlino@gmail.com>
2022-01-04 11:25:04 -0600

    Tests

--- test/os/syscalls/factory_reset_test.exs
+++ test/os/syscalls/factory_reset_test.exs
@@ -1,11 +1,15 @@
 defmodule FarmbotOS.SysCalls.FactoryResetTest do
   use ExUnit.Case
   use Mimic
+
+  require Helpers
+
   alias FarmbotOS.SysCalls.FactoryReset
+  alias FarmbotOS.{Asset, BotState, APIFetcher}
 
   setup :verify_on_exit!
 
-  test "factory reset" do
+  test "factory reset - FarmBot OS" do
     expect(FarmbotOS.System, :factory_reset, 1, fn reason, flag ->
       assert reason == "Soft resetting..."
       assert flag
@@ -13,5 +17,24 @@ defmodule FarmbotOS.SysCalls.FactoryResetTest do
     end)
 
     assert :ok == FactoryReset.factory_reset("farmbot_os")
+  end
+
+  test "factory reset - Arduino" do
+    expect(APIFetcher, :delete!, 1, fn _, "/api/firmware_config" ->
+      :unit_test
+    end)
+    expect(APIFetcher, :get!, 1, fn _, "/api/firmware_config" ->
+      :unit_test
+    end)
+    expect(BotState, :set_sync_status, 1, fn "maintenance" ->
+      :unit_test
+    end)
+    expect(FarmbotOS.System, :reboot, 1, fn "Arduino factory reset" ->
+      :unit_test
+    end)
+    Helpers.expect_log("Arduino Firmware going down for factory reset!")
+    expect(Asset, :firmware_config, 1, fn :id -> 123 end)
+    expect(Asset, :delete_firmware_config!, 1, fn 123 -> :ok end)
+    assert :ok == FactoryReset.factory_reset("arduino_firmware")
   end
 end
