b912cfece017a3a7fa3f94eb7a5712770aa9fbc8
Rick Carlino <rick.carlino@gmail.com>
2022-01-20 08:35:15 -0600

    Dead code removal and test updates

--- test/asset_workers/device_worker_test.exs
+++ test/asset_workers/device_worker_test.exs
@@ -3,11 +3,8 @@ defmodule FarmbotOS.DeviceWorkerTest do
   use Mimic
 
   alias Farmbot.TestSupport.AssetFixtures
-  alias FarmbotOS.Celery.SysCallGlue.Stubs
   alias FarmbotOS.Asset.Device
   alias FarmbotOS.AssetWorker
-
-  @im_so_sorry 300
 
   setup :set_mimic_global
   setup :verify_on_exit!
@@ -18,36 +15,12 @@ defmodule FarmbotOS.DeviceWorkerTest do
     dev
   end
 
-  test "triggering of factory reset during init" do
-    expect(Stubs, :factory_reset, fn _ ->
-      :ok
-    end)
-
-    dev = fresh_device()
-    {:ok, _pid} = AssetWorker.start_link(dev, [])
-    Process.sleep(@im_so_sorry)
-  end
-
-  test "DO trigger factory reset during update" do
-    dev = fresh_device(false)
-    {:ok, pid} = AssetWorker.start_link(dev, [])
-
-    expect(Stubs, :factory_reset, 1, fn _pkg ->
-      :ok
-    end)
-
-    GenServer.cast(pid, {:new_data, %{dev | needs_reset: true}})
-    Process.sleep(@im_so_sorry)
-  end
-
-  test "DO NOT trigger factory reset during update" do
+  test "initializes and runs noops" do
     dev = fresh_device(false)
     {:ok, _} = AssetWorker.start_link(dev, [])
-
-    stub(Stubs, :factory_reset, fn _pkg ->
-      nooo = "SHOULD NOT HAPPEN!"
-      flunk(nooo)
-      raise nooo
-    end)
+    # Test noop functions:
+    worker = FarmbotOS.AssetWorker.FarmbotOS.Asset.Device
+    {:noreply, %{}} = worker.handle_info({:csvm_done, make_ref(), :ok}, %{})
+    {:noreply, %{}} = worker.handle_cast({:new_data, %{}}, %{})
   end
 end
