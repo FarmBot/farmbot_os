637a0c9577e6264d2e2da605a2f54e3135712d81
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2023-03-31 14:35:22 -0700

    change dispense helper messages

--- priv/lua/dispense.lua
+++ priv/lua/dispense.lua
@@ -24,7 +24,7 @@ return function(ml, params)
         toast('You must have a tool named "' .. tool_name .. '" to use this sequence.', 'error')
         return
     elseif flow_rate == 0 then
-        toast("**FLOW RATE (mL/s)** must be greater than 0 for the " .. tool_name .. " tool. Refer to the sequence description for setup instructions.", "error")
+        toast("**FLOW RATE (mL/s)** must be greater than 0 for the " .. tool_name .. " tool.", "error")
         return
     elseif ml <= 0 then
         toast("Liquid volume was 0mL. Skipping.", "warn")
@@ -36,23 +36,33 @@ return function(ml, params)
 
     local seconds = math.floor(ml / flow_rate * 10) / 10
     local start_time = os.time() * 1000
-    local message = "Dispensing " .. ml .. "mL over " .. seconds .. " seconds"
     local status = "Dispensing"
-    function wait_with_progress(milliseconds)
-        for i = 1, 10 do
-            set_job_progress(message, {
-                percent = (i - 1) * 10,
+    local job_message = status .. " " .. ml .. "mL"
+    local log_message = job_message .. " over " .. seconds .. " seconds"
+    function wait_with_progress(seconds)
+        if seconds < 1 then
+            set_job_progress(job_message, { percent = 0, status = status, time = start_time })
+            wait(seconds * 1000)
+            return
+        end
+        for i = 1, math.floor(seconds) do
+            set_job_progress(job_message, {
+                percent = math.floor((i - 1) / seconds * 100),
                 status = status,
                 time = start_time,
             })
-            wait(milliseconds / 10)
+            wait(1000)
+        end
+        local remainder = seconds - math.floor(seconds)
+        if remainder > 0 then
+            wait(math.ceil(remainder * 1000))
         end
     end
 
     -- Action
-    send_message("info", message)
+    send_message("info", log_message)
     on(pin_number)
-    wait_with_progress(seconds * 1000)
+    wait_with_progress(seconds)
     off(pin_number)
-    set_job_progress(message, { percent = 100, status = "Complete", time = start_time })
+    set_job_progress(job_message, { percent = 100, status = "Complete", time = start_time })
 end
--- priv/lua/spec/dispense_spec.lua
+++ priv/lua/spec/dispense_spec.lua
@@ -38,10 +38,20 @@ describe("water()", function()
     assert.spy(on).was_not_called()
   end)
 
+  it("accepts tool name", function()
+    _G.api = spy.new(function() return {} end)
+    dispense(100, { tool_name = "nozzle" })
+
+    assert.spy(api).was.called(1)
+    assert.spy(toast).was.called_with("You must have a tool named \"nozzle\" to use this sequence.", "error")
+    assert.spy(set_job_progress).was_not_called()
+    assert.spy(on).was_not_called()
+  end)
+
   it("handles missing flow rate", function()
     _G.api = spy.new(function()
       return {
-        [0] = {
+        {
           name = "Watering Nozzle",
           flow_rate_ml_per_s = 0,
         }
@@ -50,7 +60,7 @@ describe("water()", function()
     dispense(100)
 
     assert.spy(api).was.called(1)
-    assert.spy(toast).was.called_with("**FLOW RATE (mL/s)** must be greater than 0 for the Watering Nozzle tool. Refer to the sequence description for setup instructions.", "error")
+    assert.spy(toast).was.called_with("**FLOW RATE (mL/s)** must be greater than 0 for the Watering Nozzle tool.", "error")
     assert.spy(set_job_progress).was_not_called()
     assert.spy(on).was_not_called()
   end)
@@ -58,7 +68,7 @@ describe("water()", function()
   it("handles missing amount", function()
     _G.api = spy.new(function()
       return {
-        [0] = {
+        {
           name = "Watering Nozzle",
           flow_rate_ml_per_s = 10,
         }
@@ -75,7 +85,7 @@ describe("water()", function()
   it("handles excessive amount", function()
     _G.api = spy.new(function()
       return {
-        [0] = {
+        {
           name = "Watering Nozzle",
           flow_rate_ml_per_s = 10,
         }
@@ -92,7 +102,7 @@ describe("water()", function()
   it("dispenses amount", function()
     _G.api = spy.new(function()
       return {
-        [0] = {
+        {
           name = "Watering Nozzle",
           flow_rate_ml_per_s = 10,
         }
@@ -102,12 +112,48 @@ describe("water()", function()
 
     assert.spy(api).was.called(1)
     assert.spy(toast).was_not_called()
-    assert.spy(set_job_progress).was.called_with("Dispensing 100mL over 10 seconds", { percent = 0, status = "Dispensing", time = 1000000 })
-    assert.spy(on).was.called()
+    assert.spy(send_message).was.called_with("info", "Dispensing 100mL over 10 seconds")
+    assert.spy(set_job_progress).was.called_with("Dispensing 100mL", { percent = 0, status = "Dispensing", time = 1000000 })
+    assert.spy(on).was.called_with(8)
     assert.spy(wait).was.called(10)
     assert.spy(wait).was.called_with(1000)
-    assert.spy(off).was.called()
-    assert.spy(set_job_progress).was.called_with("Dispensing 100mL over 10 seconds", { percent = 100, status = "Complete", time = 1000000 })
+    assert.spy(off).was.called_with(8)
+    assert.spy(set_job_progress).was.called_with("Dispensing 100mL", { percent = 100, status = "Complete", time = 1000000 })
     assert.spy(set_job_progress).was.called(11)
   end)
+
+  for _, test in pairs({
+    { tool = "nozzle", rate = 1000, pin = 10, ml = 100, seconds = 0.1, wait_count = 1, job_count = 2, ms = { 100 } },
+    { tool = "hose", rate = 10, pin = 9, ml = 1000, seconds = 100, wait_count = 100, job_count = 101, ms = { 1000 } },
+    { tool = "pipe", rate = 10, pin = 9, ml = 1111, seconds = 111.1, wait_count = 112, job_count = 112, ms = { 1000, 100 } },
+  }) do
+    it("dispenses " .. test.ml .. "mL through " .. test.tool .. " at "
+       .. test.rate .. "mL/s over " .. test.seconds .. " seconds", function()
+      _G.api = spy.new(function()
+        return {
+          {
+            name = test.tool,
+            flow_rate_ml_per_s = test.rate,
+          }
+        }
+      end)
+      dispense(test.ml, { tool_name = test.tool, pin = test.pin })
+
+      assert.spy(api).was.called(1)
+      assert.spy(toast).was_not_called()
+      assert.spy(send_message).was.called_with("info",
+        "Dispensing " .. test.ml .. "mL over " .. test.seconds .. " seconds")
+      assert.spy(set_job_progress).was.called_with("Dispensing " .. test.ml .. "mL",
+        { percent = 0, status = "Dispensing", time = 1000000 })
+      assert.spy(on).was.called_with(test.pin)
+      assert.spy(wait).was.called(test.wait_count)
+      for _, i in pairs(test.ms) do
+        assert.spy(wait).was.called_with(i)
+      end
+      assert.spy(off).was.called_with(test.pin)
+      assert.spy(set_job_progress).was.called_with("Dispensing " .. test.ml .. "mL",
+        { percent = 100, status = "Complete", time = 1000000 })
+      assert.spy(set_job_progress).was.called(test.job_count)
+    end)
+  end
 end)
