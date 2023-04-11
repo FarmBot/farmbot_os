44b27b4bf7082e6e11df9ce941d799659966d8ef
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2023-04-11 10:17:19 -0700

    add movement to `water` lua helper

--- priv/lua/spec/water_spec.lua
+++ priv/lua/spec/water_spec.lua
@@ -6,12 +6,20 @@ _G.get_curve = spy.new(function()
 end)
 _G.toast = spy.new(function() end)
 _G.send_message = spy.new(function() end)
+_G.move = spy.new(function() end)
+_G.safe_z = spy.new(function() return 0 end)
+_G.set_job = spy.new(function() end)
+_G.complete_job = spy.new(function() end)
 
 describe("water()", function()
   before_each(function()
     _G.toast:clear()
+    _G.get_curve:clear()
     _G.send_message:clear()
     _G.dispense:clear()
+    _G.move:clear()
+    _G.set_job:clear()
+    _G.complete_job:clear()
   end)
 
   it("gets water amount and calls dispense()", function()
@@ -26,9 +34,13 @@ describe("water()", function()
     water(plant)
 
     assert.spy(get_curve).was.called_with(1)
-    assert.spy(dispense).was.called_with(100, nil)
-    assert.spy(send_message).was.called_with("info", "Watering 10 day old Plant at (1, 2, 3) 100mL")
     assert.spy(toast).was_not_called()
+    assert.spy(set_job).was.called_with("Watering Plant at (1, 2)", { status = "Moving" })
+    assert.spy(move).was.called_with({x = 1, y = 2, z = 0})
+    assert.spy(set_job).was.called_with("Watering Plant at (1, 2)", { status = "Watering", percent = 50 })
+    assert.spy(send_message).was.called_with("info", "Watering 10 day old Plant at (1, 2) 100mL")
+    assert.spy(dispense).was.called_with(100, nil)
+    assert.spy(complete_job).was.called_with("Watering Plant at (1, 2)")
   end)
 
   it("passes params", function()
@@ -43,7 +55,27 @@ describe("water()", function()
     water(plant, { tool_name = "tool", pin = 10 })
 
     assert.spy(get_curve).was.called_with(1)
-    assert.spy(dispense).was.called_with(100, { tool_name = "tool", pin = 10})
+    assert.spy(dispense).was.called_with(100, { tool_name = "tool", pin = 10 })
+  end)
+
+  it("handles missing plant age", function()
+    local plant = {
+      name = "Plant",
+      age = nil,
+      water_curve_id = nil,
+      x = 1,
+      y = 2,
+      z = 3,
+    }
+    water(plant)
+
+    assert.spy(get_curve).was_not_called()
+    assert.spy(toast).was.called_with("Plant at (1, 2) has not been planted yet. Skipping.", "warn")
+    assert.spy(set_job).was_not_called()
+    assert.spy(move).was_not_called()
+    assert.spy(send_message).was_not_called()
+    assert.spy(dispense).was_not_called()
+    assert.spy(complete_job).was_not_called()
   end)
 
   it("handles missing watering curve", function()
@@ -57,9 +89,12 @@ describe("water()", function()
     }
     water(plant)
 
-    assert.spy(get_curve).was.called_with(1)
-    assert.spy(dispense).was_not_called()
-    assert.spy(toast).was.called_with("Plant at (1, 2, 3) has no assigned water curve. Skipping.", "warn")
+    assert.spy(get_curve).was_not_called()
+    assert.spy(toast).was.called_with("Plant at (1, 2) has no assigned water curve. Skipping.", "warn")
+    assert.spy(set_job).was_not_called()
+    assert.spy(move).was_not_called()
     assert.spy(send_message).was_not_called()
+    assert.spy(dispense).was_not_called()
+    assert.spy(complete_job).was_not_called()
   end)
 end)
--- priv/lua/water.lua
+++ priv/lua/water.lua
@@ -1,5 +1,11 @@
 return function(plant, params)
-    local plant_xyz = "(" .. plant.x .. ", " .. plant.y .. ", " .. plant.z .. ")"
+    local plant_name_xy = plant.name .. " at (" .. plant.x .. ", " .. plant.y .. ")"
+    local job_name = "Watering " .. plant_name_xy
+
+    if not plant.age then
+        toast(plant_name_xy .. " has not been planted yet. Skipping.", "warn")
+        return
+    end
 
     -- Get water curve and water amount in mL
     local water_curve, water_ml
@@ -7,10 +13,17 @@ return function(plant, params)
         water_curve = get_curve(plant.water_curve_id)
         water_ml = water_curve.day(plant.age)
     else
-        toast(plant.name .. " at " .. plant_xyz .. " has no assigned water curve. Skipping.", "warn")
+        toast(plant_name_xy .. " has no assigned water curve. Skipping.", "warn")
         return
     end
 
-    send_message("info", "Watering " .. plant.age .. " day old " .. plant.name .. " at " .. plant_xyz .. " " .. water_ml .. "mL")
+    -- Move to the plant
+    set_job(job_name, { status = "Moving" })
+    move{ x = plant.x, y = plant.y, z = safe_z() }
+
+    -- Water the plant
+    set_job(job_name, { status = "Watering", percent = 50 })
+    send_message("info", "Watering " .. plant.age .. " day old " .. plant_name_xy .. " " .. water_ml .. "mL")
     dispense(water_ml, params)
+    complete_job(job_name)
 end
